593680e5c6afa9c6505fbaa95afb95d4a95b5f14
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-04-02 10:59:38 +0900

    feat(init)

---
+++ src/test/java/wooteco/chess/ChessApplicationTests.java
@@ -0,0 +1,13 @@
+package wooteco.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

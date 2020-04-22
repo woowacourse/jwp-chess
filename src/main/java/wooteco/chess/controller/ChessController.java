65eb0f7e7d881aa709306f52f20bbfb1154bac0a
juyeongjung <juyeong.dev@gmail.com>
2020-04-22 16:03:32 +0900

    refactor: 점수를 항상 표시하도록 수정

--- src/main/java/wooteco/chess/controller/ChessController.java
+++ src/main/java/wooteco/chess/controller/ChessController.java
@@ -23,6 +23,8 @@ public class ChessController {
 		gameManagerService.resetGame();
 		model.addAttribute("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
 		model.addAttribute("turn", gameManagerService.getCurrentTurn().name());
+		model.addAttribute("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore()));
+
 		return "board";
 	}
 
@@ -30,6 +32,7 @@ public class ChessController {
 	public String resume(Model model) {
 		model.addAttribute("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
 		model.addAttribute("turn", gameManagerService.getCurrentTurn().name());
+		model.addAttribute("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore()));
 
 		return "board";
 	}
@@ -38,27 +41,19 @@ public class ChessController {
 	public String move(Model model, @RequestParam(defaultValue = "") String target,
 		@RequestParam(defaultValue = "") String destination) {
 		try {
-			gameManagerService.move(Position.of(target),
-				Position.of(destination));
+			gameManagerService.move(Position.of(target), Position.of(destination));
 		} catch (RuntimeException e) {
 			model.addAttribute("error", e.getMessage());
 		}
 		model.addAttribute("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
 		model.addAttribute("turn", gameManagerService.getCurrentTurn().name());
+		model.addAttribute("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore()));
 
 		if (!gameManagerService.isKingAlive()) {
 			model.addAttribute("winner", gameManagerService.getCurrentTurn().reverse());
 			gameManagerService.resetGame();
 			return "end";
 		}
-		return "board";
-	}
-
-	@GetMapping("/status")
-	public String status(Model model) {
-		model.addAttribute("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
-		model.addAttribute("turn", gameManagerService.getCurrentTurn().name());
-		model.addAttribute("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore()));
 		return "board";
 	}
 

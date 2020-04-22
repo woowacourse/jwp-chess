db5d477e570f96cf536d6ad889b6211722898673
toneyparky <qkrtmddhks95@gmail.com>
2020-04-22 11:53:46 +0900

    feat: 기존 체스게임 프로그램 적용을 위한 커밋

---
+++ src/main/java/wooteco/chess/controller/ChessConsoleController.java
@@ -0,0 +1,59 @@
+package wooteco.chess.controller;
+
+import chess.domain.game.ChessGame;
+import chess.domain.game.OperationType;
+import chess.domain.game.Operations;
+import chess.domain.position.MovingPosition;
+import chess.view.OutputView;
+
+import static chess.view.InputView.inputOperation;
+
+public class ChessConsoleController {
+	public void run() {
+		OutputView.printOperationsFormat();
+
+		if (!beforeStart()) {
+			return;
+		}
+
+		execute();
+
+		OutputView.printFinish();
+	}
+
+	private boolean beforeStart() {
+		OperationType operationType = inputOperation().getOperationType();
+
+		operationType.checkFirstOperations();
+
+		if (operationType.isEnd()) {
+			OutputView.printFinish();
+			return false;
+		}
+
+		return true;
+	}
+
+	private void execute() {
+		ChessGame chessGame = new ChessGame();
+		OutputView.printBoard(chessGame.createBoard());
+
+		while (executeOperation(chessGame)) {
+			OutputView.printBoard(chessGame.createBoard());
+			OutputView.printScore(chessGame.calculateScore());
+		}
+		if (chessGame.isKingDead()) {
+			OutputView.printFinishByKingDead(chessGame.getAliveKingColor());
+		}
+	}
+
+	private boolean executeOperation(ChessGame chessGame) {
+		Operations operations = inputOperation();
+		OperationType operationType = operations.getOperationType();
+		if (operationType.isMove()) {
+			MovingPosition movingPosition = new MovingPosition(operations.getFirstArgument(), operations.getSecondArgument());
+			chessGame.move(movingPosition);
+		}
+		return (operationType.canExecuteMore() && !chessGame.isKingDead());
+	}
+}
---
+++ src/main/java/wooteco/chess/domain/piece/pieces/PiecesInitializer.java
@@ -0,0 +1,44 @@
+package wooteco.chess.domain.piece.pieces;
+
+import chess.domain.piece.Color;
+import chess.domain.piece.Piece;
+import chess.domain.position.Column;
+import chess.domain.position.Position;
+import chess.domain.position.PositionFactory;
+import chess.domain.position.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PiecesInitializer {
+	public static Pieces operate() {
+		return makeInitialPieces();
+	}
+
+	private static Pieces makeInitialPieces() {
+		List<Piece> pieces = new ArrayList<>();
+
+		for (Column column : Column.getInitialColumns()) {
+			addPieceBy(column, pieces);
+		}
+
+		return new Pieces(pieces);
+	}
+
+	private static void addPieceBy(Column column, List<Piece> pieces) {
+		for (Row row : Row.values()) {
+			pieces.add(create(row, column));
+		}
+	}
+
+	private static Piece create(Row row, Column column) {
+		Position position = PositionFactory.of(row, column);
+		Color color = column.getColor();
+
+		if (column.isPawnInitial()) {
+			return new Piece(position, column.getPawnType(), color);
+		}
+
+		return new Piece(position, row.getPieceType(), color);
+	}
+}

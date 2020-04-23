67505868d44d389797880995e1451eabbc53821b
kuenhwi <kueniyam@naver.com>
2020-04-23 15:16:56 +0900

    refactor: predicate를 정규표현식으로 대체하여 코드 단순화

--- src/main/java/domain/command/CommandType.java
+++ src/main/java/domain/command/CommandType.java
@@ -3,23 +3,22 @@ package domain.command;
 import domain.command.exceptions.CommandTypeException;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
 
 public enum CommandType {
-	START("start"::equals),
-	END("end"::equals),
-	STATUS("status"::equals),
-	MOVE(string -> "move".equals(string.split(" ")[0]));
+	START("start"),
+	END("end"),
+	STATUS("status"),
+	MOVE("move [a-h][1-8] [a-h][1-8]");
 
-	private final Predicate<String> predicate;
+	private final String regex;
 
-	CommandType(Predicate<String> predicate) {
-		this.predicate = predicate;
+	CommandType(final String regex) {
+		this.regex = regex;
 	}
 
 	public static CommandType getInstance(String command) {
 		return Arrays.stream(values())
-				.filter(value -> value.predicate.test(command))
+				.filter(value -> command.matches(value.regex))
 				.findFirst()
 				.orElseThrow(() -> new CommandTypeException("올바르지 않은 명령어입니다."));
 	}

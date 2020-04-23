65928b137355123710164a44fa225a752f8db85b
juyeongjung <juyeong.dev@gmail.com>
2020-04-23 14:54:41 +0900

    refactor: Spark Application 에서도 작동 하도록 수정

--- src/main/java/wooteco/chess/SparkChessApplication.java
+++ src/main/java/wooteco/chess/SparkChessApplication.java
@@ -1,76 +1,78 @@
-// package wooteco.chess;
-//
-// import static spark.Spark.*;
-//
-// import java.util.HashMap;
-// import java.util.Map;
-//
-// import spark.ModelAndView;
-// import spark.Spark;
-// import spark.template.handlebars.HandlebarsTemplateEngine;
-// import wooteco.chess.domain.position.Position;
-// import wooteco.chess.service.GameManagerService;
-// import wooteco.chess.util.WebOutputRenderer;
-//
-// public class SparkChessApplication {
-// 	public static void main(String[] args) {
-// 		Spark.staticFileLocation("static");
-// 		final GameManagerService gameManagerService = new GameManagerService();
-//
-// 		get("/", (request, response) -> "index.html");
-//
-// 		get("/start", (request, response) -> {
-// 			gameManagerService.resetGame();
-// 			Map<String, Object> model = new HashMap<>();
-// 			model.put("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
-// 			model.put("turn", gameManagerService.getCurrentTurn().name());
-//
-// 			return render(model, "board.hbs");
-// 		});
-//
-// 		get("/resume", (request, response) -> {
-// 			Map<String, Object> model = new HashMap<>();
-// 			model.put("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
-// 			model.put("turn", gameManagerService.getCurrentTurn().name());
-//
-// 			return render(model, "board.hbs");
-// 		});
-//
-// 		post("/move", (request, response) -> {
-// 			Map<String, Object> model = new HashMap<>();
-// 			try {
-// 				gameManagerService.move(Position.of(request.queryParams("target")),
-// 					Position.of(request.queryParams("destination")));
-// 			} catch (RuntimeException e) {
-// 				model.put("error", e.getMessage());
-// 			}
-// 			model.put("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
-// 			model.put("turn", gameManagerService.getCurrentTurn().name());
-//
-// 			if (!gameManagerService.isKingAlive()) {
-// 				model.put("winner", gameManagerService.getCurrentTurn().reverse());
-// 				gameManagerService.resetGame();
-// 				return render(model, "end.hbs");
-// 			}
-// 			return render(model, "board.hbs");
-// 		});
-//
-// 		get("/status", (request, response) -> {
-// 			Map<String, Object> model = new HashMap<>();
-// 			model.put("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard()));
-// 			model.put("turn", gameManagerService.getCurrentTurn().name());
-// 			model.put("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore()));
-//
-// 			return render(model, "board.hbs");
-// 		});
-//
-// 		get("/end", (request, response) -> {
-// 			gameManagerService.resetGame();
-// 			return render(new HashMap<>(), "end.hbs");
-// 		});
-// 	}
-//
-// 	public static String render(Map<String, Object> model, String templatePath) {
-// 		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-// 	}
-// }
+package wooteco.chess;
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import wooteco.chess.domain.position.Position;
+import wooteco.chess.service.GameManagerService;
+import wooteco.chess.util.WebOutputRenderer;
+
+public class SparkChessApplication {
+	public static void main(String[] args) {
+		Spark.staticFileLocation("static");
+		final GameManagerService gameManagerService = new GameManagerService();
+
+		get("/", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			model.put("roomNumbers", gameManagerService.getAllRoomNo());
+
+			return render(model, "index.hbs");
+		});
+
+		get("/start", (request, response) -> {
+			int roomNo = gameManagerService.newGame();
+			response.redirect("/board/" + roomNo);
+
+			return null;
+		});
+
+		get("/board/:roomNo", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			int roomNo = Integer.parseInt(request.params(":roomNo"));
+
+			model.put("roomNo", roomNo);
+			model.put("piecesDto", WebOutputRenderer.toPiecesDto(gameManagerService.getBoard(roomNo)));
+			model.put("turn", gameManagerService.getCurrentTurn(roomNo).name());
+			model.put("scores", WebOutputRenderer.scoreToModel(gameManagerService.calculateEachScore(roomNo)));
+
+			return render(model, "board.hbs");
+		});
+
+		post("/move", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			int roomNo = Integer.parseInt(request.queryParams("roomNo"));
+			try {
+				gameManagerService.move(Position.of(request.queryParams("target")),
+					Position.of(request.queryParams("destination")), roomNo);
+			} catch (RuntimeException e) {
+				model.put("error", e.getMessage());
+				model.put("roomNo", roomNo);
+				return render(model, "error.hbs");
+			}
+
+			if (!gameManagerService.isKingAlive(roomNo)) {
+				model.put("winner", gameManagerService.getCurrentTurn(roomNo).reverse());
+				gameManagerService.deleteGame(roomNo);
+				return render(model, "end.hbs");
+			}
+			response.redirect("/board/" + roomNo);
+			return null;
+		});
+
+		get("/end", (request, response) -> {
+			int roomNo = Integer.parseInt(request.queryParams("roomNo"));
+			gameManagerService.deleteGame(roomNo);
+
+			return render(new HashMap<>(), "end.hbs");
+		});
+	}
+
+	public static String render(Map<String, Object> model, String templatePath) {
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+	}
+}

3eb3db0478cca1f455280e4bd965343ebb654a2c
Sony <sonypark0204@gmail.com>
2020-04-23 15:12:33 +0900

    chore: 불필요한 파일 삭제

--- src/main/java/wooteco/chess/service/exception/InvalidGameException.java
+++
@@ -1,9 +0,0 @@
-package wooteco.chess.service.exception;
-
-public class InvalidGameException extends RuntimeException {
-    public static final String INVALID_GAME_ID_MESSAGE = "존재하지 않는 게임 입니다.";
-
-    public InvalidGameException() {
-        super(INVALID_GAME_ID_MESSAGE);
-    }
-}

4bf592feaa45ab32cfecb6e1334032ce75f29a8f
kouz <lastpuzly@gmail.com>
2020-04-24 13:47:40 +0900

    refactor: position -> ordinal() 변경

--- src/main/java/wooteco/chess/domain/position/Column.java
+++ src/main/java/wooteco/chess/domain/position/Column.java
@@ -3,35 +3,33 @@ package wooteco.chess.domain.position;
 import java.util.Arrays;
 
 public enum Column {
-	A("a", 1),
-	B("b", 2),
-	C("c", 3),
-	D("d", 4),
-	E("e", 5),
-	F("f", 6),
-	G("g", 7),
-	H("h", 8);
+	A("a"),
+	B("b"),
+	C("c"),
+	D("d"),
+	E("e"),
+	F("f"),
+	G("g"),
+	H("h");
 
 	private static final int STEP = 1;
 
 	private final String name;
-	private final int position;
 
-	Column(String name, int position) {
+	Column(String name) {
 		this.name = name;
-		this.position = position;
 	}
 
 	public Column plus() {
 		return Arrays.stream(Column.values())
-			.filter(value -> value.position == this.position + STEP)
+			.filter(value -> value.ordinal() == this.ordinal() + STEP)
 			.findFirst()
 			.orElseThrow(() -> new IllegalArgumentException("9 이상의 column 값은 가질수 없습니다."));
 	}
 
 	public Column minus() {
 		return Arrays.stream(Column.values())
-			.filter(value -> value.position == this.position - STEP)
+			.filter(value -> value.ordinal() == this.ordinal() - STEP)
 			.findFirst()
 			.orElseThrow(() -> new IllegalArgumentException("0 이하의 column 값은 가질수 없습니다."));
 	}
--- src/main/java/wooteco/chess/domain/position/Row.java
+++ src/main/java/wooteco/chess/domain/position/Row.java
@@ -3,35 +3,33 @@ package wooteco.chess.domain.position;
 import java.util.Arrays;
 
 public enum Row {
-	ONE("1", 1),
-	TWO("2", 2),
-	THREE("3", 3),
-	FOUR("4", 4),
-	FIVE("5", 5),
-	SIX("6", 6),
-	SEVEN("7", 7),
-	EIGHT("8", 8);
+	ONE("1"),
+	TWO("2"),
+	THREE("3"),
+	FOUR("4"),
+	FIVE("5"),
+	SIX("6"),
+	SEVEN("7"),
+	EIGHT("8");
 
 	private static final int STEP = 1;
 
 	private final String name;
-	private final int position;
 
-	Row(String name, int position) {
+	Row(String name) {
 		this.name = name;
-		this.position = position;
 	}
 
 	public Row plus() {
 		return Arrays.stream(Row.values())
-			.filter(value -> value.position == this.position + STEP)
+			.filter(value -> value.ordinal() == this.ordinal() + STEP)
 			.findFirst()
 			.orElseThrow(() -> new IllegalArgumentException("9 이상의 row 값은 가질수 없습니다."));
 	}
 
 	public Row minus() {
 		return Arrays.stream(Row.values())
-			.filter(value -> value.position == this.position - STEP)
+			.filter(value -> value.ordinal() == this.ordinal() - STEP)
 			.findFirst()
 			.orElseThrow(() -> new IllegalArgumentException("0 이하의 row 값은 가질수 없습니다."));
 	}

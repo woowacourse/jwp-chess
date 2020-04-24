a62481ed6917f9d9986511935dcc3d045b48cd51
dd <dqrd123@gmail.com>
2020-04-24 16:03:52 +0900

    feat : move 예외 처리 작업중

--- src/main/java/wooteco/chess/dto/GameManagerDTO.java
+++ src/main/java/wooteco/chess/dto/GameManagerDTO.java
@@ -10,6 +10,7 @@ public class GameManagerDTO {
     private final List<PieceResponseDTO> pieces;
     private final Color currentColor;
     private final boolean kingDead;
+    private String errorMessage;
 
     public GameManagerDTO(final List<PieceResponseDTO> pieces, final Color currentColor, final boolean kingDead) {
         this.pieces = pieces;
@@ -27,5 +28,13 @@ public class GameManagerDTO {
 
     public boolean isKingDead() {
         return kingDead;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(final String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }

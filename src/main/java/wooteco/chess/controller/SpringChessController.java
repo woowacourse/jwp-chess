63f5d680ed38be41673c07db8e6300131651ed8d
kouz <lastpuzly@gmail.com>
2020-04-24 15:42:20 +0900

    refactor: SpringChessController.renderStart() 구현

---
+++ src/main/java/wooteco/chess/controller/SpringChessController.java
@@ -0,0 +1,12 @@
+package wooteco.chess.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpringChessController {
+	@GetMapping("/")
+	public String renderStart() {
+		return "index";
+	}
+}

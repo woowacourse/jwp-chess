e7d29d071917f4274a8d7fd3992b65a149c39715
박찬인 <pci2676@gmail.com>
2020-04-25 07:11:36 +0900

    [비밥] 웹 기반 체스 게임 1단계 구현 (#51)
    * feat : 코드 마이그레이션
    
    * docs : README 작성
    
    * refactor : 테이블 구조 변경 
    
    - 단일 게임만 가능하던 구조에서 다중 게임이 가능하도록 수정
    
    * refactor : 예외를 try-catch에서 exception handler로 관리
    
    * refactor : unicode로 변환하는 기능을 service에서 분리
    
    * fix : 적을 잡으면 DB에서 삭제하도록 수정
    
    * refactor : 페이지 렌더링 방식 변경
    
    * refactor : 게임 종료 시 alert를 통해 알림
    
    * fix : í폰이 대각선으로 움직이는 기능 수정
    
    * refactor : autoBoxing 비용 절감
    
    * refactor : SQLException을 SQLAccessException으로 처리
    
    * refactor : SQLException 발생 시 SQLAccessException에 해당 에러 정보 전달
    
    * refactor : DBConnector에서 에러 발생시 프로그램 종료
    
    * refactor : PieceDao의 save() 반환타입 변경 및 사용하지 않는 메서드 제거
    
    * refactor : 추상적인 메서드 이름을 구체적으로 변경
    
    Co-authored-by: soring <jnsorn@gmail.com>

---
+++ src/main/java/wooteco/chess/domain/piece/position/Direction.java
@@ -0,0 +1,105 @@
+package wooteco.chess.domain.piece.position;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+import wooteco.chess.domain.board.Board;
+
+public enum Direction {
+	N(1, 0, (rowGap, columnGap) -> rowGap > 0 && columnGap == 0),
+	S(-1, 0, (rowGap, columnGap) -> rowGap < 0 && columnGap == 0),
+	E(0, 1, (rowGap, columnGap) -> rowGap == 0 && columnGap > 0),
+	W(0, -1, (rowGap, columnGap) -> rowGap == 0 && columnGap < 0),
+
+	NE(1, 1, (rowGap, columnGap) -> isSameAbs(rowGap, columnGap) && rowGap > 0 && columnGap > 0),
+	NW(1, -1, (rowGap, columnGap) -> isSameAbs(rowGap, columnGap) && rowGap > 0 && columnGap < 0),
+	SE(-1, 1, (rowGap, columnGap) -> isSameAbs(rowGap, columnGap) && rowGap < 0 && columnGap > 0),
+	SW(-1, -1, (rowGap, columnGap) -> isSameAbs(rowGap, columnGap) && rowGap < 0 && columnGap < 0),
+
+	NNE(2, 1, (rowGap, columnGap) -> rowGap == 2 && columnGap == 1),
+	NNW(2, -1, (rowGap, columnGap) -> rowGap == 2 && columnGap == -1),
+	SSE(-2, 1, (rowGap, columnGap) -> rowGap == -2 && columnGap == 1),
+	SSW(-2, -1, (rowGap, columnGap) -> rowGap == -2 && columnGap == -1),
+	NEE(1, 2, (rowGap, columnGap) -> rowGap == 1 && columnGap == 2),
+	NWW(1, -2, (rowGap, columnGap) -> rowGap == 1 && columnGap == -2),
+	SEE(-1, 2, (rowGap, columnGap) -> rowGap == -1 && columnGap == 2),
+	SWW(-1, -2, (rowGap, columnGap) -> rowGap == -1 && columnGap == -2);
+
+	private int rowGap;
+	private int columnGap;
+	private BiFunction<Integer, Integer, Boolean> find;
+
+	Direction(int rowGap, int columnGap, BiFunction<Integer, Integer, Boolean> find) {
+		this.rowGap = rowGap;
+		this.columnGap = columnGap;
+		this.find = find;
+	}
+
+	private static boolean isSameAbs(int rowGap, int columnGap) {
+		return Math.abs(rowGap) == Math.abs(columnGap);
+	}
+
+	public static Direction findDirection(Position position, Position targetPosition) {
+		int rowGap = position.calculateRowGap(targetPosition);
+		int columnGap = position.calculateColumnGap(targetPosition);
+
+		return Arrays.stream(values())
+			.filter(direction -> direction.find.apply(rowGap, columnGap))
+			.findFirst()
+			.orElseThrow(() -> new InvalidPositionException(InvalidPositionException.INVALID_DIRECTION));
+
+	}
+
+	public boolean hasPieceInRoute(Position position, Position targetPosition, Board board) {
+		List<Position> route = findRoutes(position, targetPosition);
+		return route.stream()
+			.map(board::findPiece)
+			.anyMatch(Optional::isPresent);
+	}
+
+	private List<Position> findRoutes(Position position, Position targetPosition) {
+		int loopCount = countOfLoop(position, targetPosition) - 1;
+		int routeRow = position.getRowNumber();
+		int routeColumn = position.getColumnNumber();
+		List<Position> route = new ArrayList<>();
+		for (int i = 0; i < loopCount; i++) {
+			routeRow += this.rowGap;
+			routeColumn += this.columnGap;
+			route.add(Position.of(routeColumn, routeRow));
+		}
+		return route;
+	}
+
+	private int countOfLoop(Position position, Position targetPosition) {
+		int columnGap = Math.abs(position.calculateColumnGap(targetPosition));
+		int rowGap = Math.abs(position.calculateRowGap(targetPosition));
+		return Math.max(columnGap, rowGap);
+	}
+
+	public static boolean isEveryDirection(Direction direction) {
+		return Arrays.asList(N, S, E, W, NE, NW, SE, SW).contains(direction);
+	}
+
+	public static boolean isLinearDirection(Direction direction) {
+		return Arrays.asList(N, S, E, W).contains(direction);
+	}
+
+	public static boolean isDiagonalDirection(Direction direction) {
+		return Arrays.asList(NE, NW, SE, SW).contains(direction);
+	}
+
+	public static boolean isKnightDirection(Direction direction) {
+		return Arrays.asList(NNE, NNW, SSE, SSW, NEE, NWW, SEE, SWW).contains(direction);
+	}
+
+	public static boolean isWhitePawnDirection(Direction direction) {
+		return Arrays.asList(N, NE, NW).contains(direction);
+	}
+
+	public static boolean isBlackPawnDirection(Direction direction) {
+		return Arrays.asList(S, SE, SW).contains(direction);
+	}
+}
---
+++ src/test/java/wooteco/chess/domain/board/RankTest.java
@@ -0,0 +1,44 @@
+package wooteco.chess.domain.board;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import wooteco.chess.domain.piece.Piece;
+import wooteco.chess.domain.piece.position.Column;
+import wooteco.chess.domain.piece.position.Position;
+import wooteco.chess.domain.piece.position.Row;
+
+public class RankTest {
+	List<Rank> ranks = BoardFactory.create().getRanks();
+
+	@DisplayName("Rank 생성")
+	@Test
+	void constructor_GivenPieces_CreateRank() {
+		assertThat(new Rank(new ArrayList<>())).isInstanceOf(Rank.class);
+	}
+
+	@DisplayName("해당 포지션에 Piece가 존재하는지 검사")
+	@ParameterizedTest
+	@CsvSource({"A,ONE,true", "H,EIGHT,true", "D,FIVE,false", "A,THREE,false"})
+	void findPiece_GivenPosition_ReturnOptional(Column column, Row row, boolean expected) {
+		Position position = Position.of(column, row);
+		Rank rank = ranks.get(row.getRankIndex());
+		Optional<Piece> piece = rank.findPiece(position);
+		assertThat(piece.isPresent()).isEqualTo(expected);
+	}
+
+	@DisplayName("각 라인마다 왕이 몇 개 존재하는지 검사")
+	@ParameterizedTest
+	@CsvSource({"0,1","1,0","2,0","3,0","4,0","5,0","6,0","7,1"})
+	void countOfKing_GivenRank_ReturnCountOfKing(int rankIndex, int expected) {
+		assertThat(ranks.get(rankIndex).countOfKing()).isEqualTo(expected);
+	}
+}

1d7bb25598df370169015d1a57b2455ccc585c4b
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-26 12:23:14 +0900

    refactor : 컨트롤러 분리

--- src/main/java/wooteco/chess/dao/util/ConnectionLoader.java
+++ src/main/java/wooteco/chess/dao/util/ConnectionLoader.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionLoader {
-	private static final String SERVER = "localhost:13306";
+	private static final String SERVER = "127.0.0.1:13306";
 	private static final String DB_NAME = "db_name";
 	private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
 	private static final String NAME = "root";
 	private static final String PASSWORD = "root";
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
 
 	public static Connection load() {
 		Connection con = null;
--- src/main/resources/static/js/player.js
+++ src/main/resources/static/js/player.js
@@ -4,10 +4,13 @@ let player2Name = prompt("당신은 플레이어 2입니다. 이름을 설정해
 let player2Password = prompt("당신은 플레이어 2입니다. 비밀번호를 설정해주세요.");
 
 fetch("http://localhost:4567/createNewGame", {
-    method: 'POST', body: JSON.stringify({
-        player1Name, player1Password, player2Name, player2Password
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        player1Name, player1Password, player2Name, player2Password,
     })
-}).then(res => res.json())
-    .then(data => {
-        location.replace("/startGame/" + data.id);
-    });
+}).then(res => res.json()).then(data => {
+    location.replace("/startGame/" + data.id);
+});

1a6d0cff1ef383a8b431bd8ee7b0649f4b817493
KS-KIM <nullable@kakao.com>
2020-04-26 17:47:35 +0900

    refactor: 불필요한 bean 생성 메서드 제거

--- src/main/java/wooteco/chess/WebConfiguration.java
+++ src/main/java/wooteco/chess/WebConfiguration.java
@@ -6,23 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import wooteco.chess.dao.DataSource;
 import wooteco.chess.dao.JdbcConfiguration;
-import wooteco.chess.dao.JdbcTemplate;
 
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @Configuration
 public class WebConfiguration {
-	@Bean
-	public JdbcTemplate provideJdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
-
-	@Bean
-	public DataSource provideDataSource(JdbcConfiguration jdbcConfiguration) {
-		return new DataSource(jdbcConfiguration);
-	}
-
 	@ConfigurationProperties(prefix = "spring.datasource")
 	@Bean
 	public JdbcConfiguration provideJdbcConfiguration() {

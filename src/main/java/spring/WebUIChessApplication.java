b538aca7289a1ff568a0e5937d028f4ee6959046
Tak Hyungmin <milkho11@naver.com>
2020-04-27 14:15:36 +0900

    [feat] Spring 컨트롤러 매핑 구현

--- src/main/java/spring/WebUIChessApplication.java
+++ src/main/java/spring/WebUIChessApplication.java
@@ -1,9 +1,11 @@
-//import controller.ChessWebController;
-//import service.ChessService;
-//
-//public class WebUIChessApplication {
-//    public static void main(String[] args) {
-//        ChessService chessService = new ChessService();
-//        // ChessWebController chessWebController = new ChessWebController(chessService);
-//    }
-//}
+package spring;
+
+import spring.controller.ChessWebController;
+import spring.service.ChessService;
+
+public class WebUIChessApplication {
+    public static void main(String[] args) {
+        ChessService chessService = new ChessService();
+        ChessWebController chessWebController = new ChessWebController(chessService);
+    }
+}
--- src/main/java/spring/controller/SpringChessController.java
+++ src/main/java/spring/controller/SpringChessController.java
@@ -1,11 +1,13 @@
 package spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spring.chess.game.ChessGame;
+import spring.dto.LocationDto;
 import spring.service.ChessService;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +27,54 @@ public class SpringChessController {
     String main() {
         Map<String, Object> model = new HashMap<>();
         return render(model, "start.html");
+    }
+
+    @GetMapping("/start")
+    String start() {
+        Map<String, Object> model = new HashMap<>();
+        return render(model, "index.html");
+    }
+
+    @GetMapping("/start/boards")
+    String findBoards() throws SQLException {
+        return chessService.findAllBoards();
+    }
+
+    @GetMapping("/start/board")
+    String findBoard(@RequestParam(name = "id") String id) throws SQLException {
+        int boardId = Integer.parseInt(id);
+        ChessGame chessGame = chessService.makeGameByDB(boardId);
+        return chessService.findGame(chessGame);
+    }
+
+    @PostMapping("/start/move")
+    String move(@RequestParam(name="now") String now, @RequestParam(name="des") String des, @RequestParam(name="game_id") String gameIdData) throws SQLException {
+        LocationDto nowDto = new LocationDto(now);
+        LocationDto destinationDto = new LocationDto(des);
+        int gameId = Integer.parseInt(gameIdData);
+
+        return chessService.move(nowDto, destinationDto, gameId);
+    }
+
+    @GetMapping("/start/winner")
+    String findWinner(@RequestParam(name="game_id") String gameIdData) throws SQLException {
+        int gameId = Integer.parseInt(gameIdData);
+        ChessGame chessGame = chessService.makeGameByDB(gameId);
+        return chessService.findWinner(chessGame);
+    }
+
+    @PostMapping("/end")
+    String end() {
+        Map<String, Object> model = new HashMap<>();
+        return render(model, "start.html");
+    }
+
+    @PostMapping("/start/new/game")
+    String startNewGame(@RequestParam(name="game_id") String gameIdData) throws SQLException {
+        int gameId = Integer.parseInt(gameIdData);
+        ChessGame chessGame = new ChessGame();
+        chessService.resetChessGame(chessGame, gameId);
+        return chessService.findBoard(gameId);
     }
 
     private static String render(Map<String, Object> model, String templatePath) {

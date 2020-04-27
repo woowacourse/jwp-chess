85dc7df7fe733889305137d71e4552be1c1d6d36
Tak Hyungmin <milkho11@naver.com>
2020-04-27 15:08:30 +0900

    [refactor] MoveCommand 리팩토링
    유효성 검증로직 추가

--- src/main/java/spring/chess/command/MoveCommand.java
+++ src/main/java/spring/chess/command/MoveCommand.java
@@ -7,10 +7,13 @@ import spring.chess.location.LocationSubStringUtil;
 import spring.chess.progress.Progress;
 import spring.chess.team.Team;
 
+import java.util.Objects;
+
 public class MoveCommand implements Command {
     private static final String SPACE = " ";
     private static final int NOW_INDEX_IN_MOVE_COMMAND = 1;
     private static final int DESTINATION_INDEX_IN_MOVE_COMMAND = 2;
+    private static final String INVALID_ARGUMENTS_ERROR_MESSAGE = "null 혹은 빈 문자열의 값이 존재합니다.";
 
     private final Location now;
     private final Location destination;
@@ -43,6 +46,8 @@ public class MoveCommand implements Command {
     }
 
     public static MoveCommand of(String rawCommand, ChessGame chessGame) {
+        valid(rawCommand, chessGame);
+
         String now = rawCommand.split(SPACE)[NOW_INDEX_IN_MOVE_COMMAND];
         String destination = rawCommand.split(SPACE)[DESTINATION_INDEX_IN_MOVE_COMMAND];
 
@@ -50,6 +55,12 @@ public class MoveCommand implements Command {
         Location destinationLocation = LocationSubStringUtil.substring(destination);
 
         return new MoveCommand(nowLocation, destinationLocation, chessGame);
+    }
+
+    private static void valid(String rawCommand, ChessGame chessGame) {
+        if (Objects.isNull(rawCommand) || rawCommand.isEmpty() || Objects.isNull(chessGame)) {
+            throw new IllegalArgumentException(INVALID_ARGUMENTS_ERROR_MESSAGE);
+        }
     }
 
     public static MoveCommand of(Location now, Location destination, ChessGame chessGame) {

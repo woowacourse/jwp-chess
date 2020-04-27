2008c22d6a62f366e2e532e7f09ae4e924ac9c1f
hj <a301dks@naver.com>
2020-04-27 17:04:15 +0900

    feat: BoardEntity 추가

---
+++ src/main/java/wooteco/chess/entity/BoardEntity.java
@@ -0,0 +1,34 @@
+package wooteco.chess.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ *    board entity class입니다.
+ *
+ *    @author HyungJu An, JunSeong Hong
+ */
+@Table("board")
+public class BoardEntity {
+	private final @Id
+	Long id;
+
+	private final Long roomId;
+	private final String position;
+	private final String name;
+
+	BoardEntity(final Long id, final Long roomId, final String position, final String name) {
+		this.id = id;
+		this.roomId = roomId;
+		this.position = position;
+		this.name = name;
+	}
+
+	public static BoardEntity of(final Long roomId, final String position, final String name) {
+		return new BoardEntity(null, roomId, position, name);
+	}
+
+	public BoardEntity withId(final Long id) {
+		return new BoardEntity(id, this.roomId, this.position, this.name);
+	}
+}

02a3dd2cdc3de3b3377544485da2aafa7c666f9c
kuenhwi <kueniyam@naver.com>
2020-04-27 16:29:22 +0900

    refactor: @RequestMapping 도입

--- src/main/java/chess/controller/SpringRoomController.java
+++ src/main/java/chess/controller/SpringRoomController.java
@@ -5,30 +5,20 @@ import chess.service.ChessRoomService;
 import chess.view.Announcement;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
 @Controller
+@RequestMapping("/chess/rooms")
 public class SpringRoomController {
-	private static final String PATH = "/chess/rooms";
-	private static final String PATH_VARIABLE = "{room_id}";
-	private static final String STATIC_PATH = "/chess";
-	private static final String SLASH = "/";
-	private static final String COMMAND_QUERY_PARAM = "command";
-	private static final String TABLE_KEY = "table";
-	private static final String ANNOUNCEMENT_KEY = "announcement";
-
 	private final ChessRoomService chessRoomService;
 
 	public SpringRoomController(final ChessRoomService chessRoomService) {
 		this.chessRoomService = chessRoomService;
 	}
 
-	@GetMapping(PATH + SLASH + PATH_VARIABLE)
+	@GetMapping("/{room_id}")
 	private String load(@PathVariable("room_id") final int roomId, final Model model) throws SQLException {
 		try {
 			loadData(roomId, model);
@@ -36,14 +26,14 @@ public class SpringRoomController {
 			initRoom(roomId);
 			loadData(roomId, model);
 		}
-		return STATIC_PATH;
+		return "chess";
 	}
 
 	private void loadData(final int roomId, final Model model) throws SQLException {
 		final String boardHtml = chessRoomService.loadBoardHtml(roomId);
 		final String announcementMessage = chessRoomService.loadAnnouncementMessage(roomId);
-		model.addAttribute(TABLE_KEY, boardHtml);
-		model.addAttribute(ANNOUNCEMENT_KEY, announcementMessage);
+		model.addAttribute("table", boardHtml);
+		model.addAttribute("announcement", announcementMessage);
 	}
 
 	private void initRoom(final int roomId) throws SQLException {
@@ -52,16 +42,16 @@ public class SpringRoomController {
 		chessRoomService.saveNewAnnouncementMessage(roomId);
 	}
 
-	@PostMapping(PATH + SLASH + PATH_VARIABLE)
+	@PostMapping("/{room_id}")
 	private String command(
 			@PathVariable("room_id") final int roomId,
-			@RequestParam(COMMAND_QUERY_PARAM) final String command) throws SQLException {
+			@RequestParam("command") final String command) throws SQLException {
 
 		try {
 			chessRoomService.updateRoom(roomId, command);
 		} catch (RuntimeException e) {
 			chessRoomService.saveAnnouncementMessage(roomId, Announcement.of(e.getMessage()).getString());
 		}
-		return Constants.REDIRECT + PATH + SLASH + roomId;
+		return Constants.REDIRECT + "/chess/rooms/" + roomId;
 	}
 }

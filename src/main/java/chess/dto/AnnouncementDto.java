450f31bca620f92638d6776444c16b4bfa2ec79f
kuenhwi <kueniyam@naver.com>
2020-04-27 17:38:04 +0900

    refactor: AnnouncementRepository를 Service에 적용

--- src/main/java/chess/dto/AnnouncementDto.java
+++ src/main/java/chess/dto/AnnouncementDto.java
@@ -1,13 +1,16 @@
 package chess.dto;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("announcement")
 public class AnnouncementDto {
 	@Id
 	private final int id;
+	@Column("message")
 	private final String announcement;
+	@Column("room_id")
 	private final int roomId;
 
 	public AnnouncementDto(final int id, final String announcement, final int roomId) {

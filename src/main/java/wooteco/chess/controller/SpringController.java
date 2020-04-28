eb49f7ec7d6d3bdbc9a9c014ef261d54ef41b6a4
begaonnuri <turtlenam0928@gmail.com>
2020-04-28 17:47:20 +0900

    feat: 체스 방들을 불러오는 기능 추가

--- src/main/java/wooteco/chess/controller/SpringController.java
+++ src/main/java/wooteco/chess/controller/SpringController.java
@@ -1,13 +1,32 @@
 package wooteco.chess.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import wooteco.chess.dto.RoomDto;
+import wooteco.chess.service.ChessGameService;
 
 @Controller
 public class SpringController {
+	private ChessGameService chessGameService;
+
+	public SpringController(ChessGameService chessGameService) {
+		this.chessGameService = chessGameService;
+	}
+
 	@GetMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index");
+	}
+
+	@GetMapping("/api/rooms")
+	@ResponseBody
+	public List<RoomDto> getRooms() throws SQLException {
+		return chessGameService.findAllRooms();
 	}
 }

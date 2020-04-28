d1b61603846db108cff314f54e9e11bbb91ad696
kuenhwi <kueniyam@naver.com>
2020-04-28 16:44:54 +0900

    refactor: StateDao를 StateRepository로 대체

--- src/main/java/chess/dto/StateDto.java
+++ src/main/java/chess/dto/StateDto.java
@@ -1,8 +1,16 @@
 package chess.dto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("state")
 public class StateDto {
+	@Id
 	final int id;
+	@Column("state")
 	final String state;
+	@Column("room_id")
 	final int roomId;
 
 	public StateDto(final int id, final String state, final int roomId) {
---
+++ src/main/java/chess/repository/StateRepository.java
@@ -0,0 +1,21 @@
+package chess.repository;
+
+import chess.dto.StateDto;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface StateRepository extends CrudRepository<StateDto, Integer> {
+
+	@Modifying
+	@Query("INSERT INTO state (state, room_id) VALUES (:state, :room_id)")
+	void save(@Param("state") final String state, @Param("room_id") final int roomId);
+
+	@Query("SELECT * FROM state WHERE room_id=:room_id")
+	StateDto findByRoomId(@Param("room_id") final int roomId);
+
+	@Modifying
+	@Query("UPDATE state SET state=:state WHERE room_id=:room_id")
+	void setByRoomId(@Param("state") final String state, @Param("room_id") final int roomId);
+}

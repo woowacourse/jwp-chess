f42e37577e5b23047aae631c73ed0e14b1760080
juyeongjung <juyeong.dev@gmail.com>
2020-04-28 15:58:29 +0900

    refactor: testcode 는 H2 DB 만을 사용 하도록 변경

--- src/test/java/wooteco/chess/repository/ChessGameRepositoryTest.java
+++ src/test/java/wooteco/chess/repository/ChessGameRepositoryTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import wooteco.chess.entity.ChessGame;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ChessGameRepositoryTest {
 

56fd9e59cf422973b30add92106805f3db22a548
KS-KIM <nullable@kakao.com>
2020-04-28 17:46:28 +0900

    refactor: 불필요한 구문 제거

--- src/test/java/wooteco/chess/ChessApplicationTests.java
+++ src/test/java/wooteco/chess/ChessApplicationTests.java
@@ -3,7 +3,7 @@ package wooteco.chess;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest()
+@SpringBootTest
 class ChessApplicationTests {
 	@Test
 	void contextLoads() {

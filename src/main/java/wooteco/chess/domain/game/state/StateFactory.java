6c7305b0a1d7a168545da7c6f0cf0ae0a8405de1
Sony <sonypark0204@gmail.com>
2020-04-29 19:17:53 +0900

    refactor: StateFactory 생성

---
+++ src/main/java/wooteco/chess/domain/game/state/StateFactory.java
@@ -0,0 +1,21 @@
+package wooteco.chess.domain.game.state;
+
+import java.util.function.BiFunction;
+
+import wooteco.chess.domain.game.Board;
+import wooteco.chess.domain.game.Turn;
+
+public enum StateFactory {
+    READY((board, turn) -> new Ready()),
+    PLAYING((board, turn) -> new Playing(Board.from(board), Turn.from(turn))),
+    FINISHED((board, turn) -> new Finished(Board.from(board), Turn.from(turn)));
+    private final BiFunction<String, String, State> creator;
+
+    StateFactory(BiFunction<String, String, State> creator) {
+        this.creator = creator;
+    }
+
+    public State create(String board, String turn) {
+        return creator.apply(board, turn);
+    }
+}
--- src/main/java/wooteco/chess/entity/ChessGameEntity.java
+++ src/main/java/wooteco/chess/entity/ChessGameEntity.java
@@ -3,19 +3,12 @@ package wooteco.chess.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-import wooteco.chess.domain.game.Board;
 import wooteco.chess.domain.game.ChessGame;
-import wooteco.chess.domain.game.Turn;
-import wooteco.chess.domain.game.state.Finished;
-import wooteco.chess.domain.game.state.Playing;
-import wooteco.chess.domain.game.state.Ready;
-import wooteco.chess.domain.game.state.State;
+import wooteco.chess.domain.game.state.StateFactory;
 import wooteco.chess.dto.BoardDto;
 
 @Table("chess_game")
 public class ChessGameEntity {
-    private static final String PLAYING = "PLAYING";
-    private static final String FINISHED = "FINISHED";
 
     @Id
     private int id;
@@ -53,16 +46,7 @@ public class ChessGameEntity {
     }
 
     public ChessGame toModel() {
-        return ChessGame.of(toStatus());
+        return ChessGame.of(StateFactory.valueOf(state).create(board, turn));
     }
 
-    public State toStatus() {
-        if (state.equals(PLAYING)) {
-            return new Playing(Board.from(board), Turn.from(turn));
-        }
-        if (state.equals(FINISHED)) {
-            return new Finished(Board.from(board), Turn.from(turn));
-        }
-        return new Ready();
-    }
 }

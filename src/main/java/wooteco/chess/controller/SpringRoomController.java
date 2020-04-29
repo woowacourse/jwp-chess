a62fe081abd2d0e030b6b3c77083009984baf862
aegis <aegis1920@gmail.com>
2020-04-30 02:33:42 +0900

    refactor : HTTP 메서드에 맞게 Mapping 어노테이션 수정,@PathVariable 적용

--- src/main/java/wooteco/chess/controller/SpringRoomController.java
+++ src/main/java/wooteco/chess/controller/SpringRoomController.java
@@ -1,19 +1,24 @@
 package wooteco.chess.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import wooteco.chess.dto.AuthorizeDto;
 import wooteco.chess.dto.RoomRequestDto;
 import wooteco.chess.dto.RoomResponseDto;
 import wooteco.chess.service.SpringRoomService;
-
-import javax.validation.Valid;
-import java.sql.SQLException;
-import java.util.List;
 
 @Controller
 @RequestMapping("/rooms")
@@ -25,23 +30,23 @@ public class SpringRoomController {
         this.roomService = roomService;
     }
 
+    // TODO: 2020/04/30 Room 목록을 동기방식이 아닌 비동기 방식으로 수정해야됨
     @GetMapping
-    public String getAllRooms(Model model) throws SQLException {
+    public String getAllRooms(Model model) {
         List<RoomResponseDto> rooms = roomService.findAllRoom();
         model.addAttribute("rooms", rooms);
 
         return "index";
     }
 
-    @GetMapping("/enter")
-    public String enterRoom(@RequestParam(value = "roomId") Integer roomId, Model model) {
-        model.addAttribute("roomId", roomId);
+    @GetMapping("/enter/{id}")
+    public String enterRoom(@PathVariable Long id, Model model) {
+        model.addAttribute("id", id);
         return "game";
     }
 
-    // TODO: 2020/04/22 valid 에러페이지 이동 문제
     @PostMapping("/create")
-    public String createRoom(@Valid RoomRequestDto roomRequestDto, BindingResult bindingResult, Model model) throws SQLException {
+    public String createRoom(@Valid RoomRequestDto roomRequestDto, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("nameError", bindingResult.getFieldError("name"));
             model.addAttribute("passwordError", bindingResult.getFieldError("password"));
@@ -51,28 +56,14 @@ public class SpringRoomController {
         return "redirect:/rooms";
     }
 
-    @GetMapping("/remove")
-    public String removeRoom(@RequestParam(value = "roomId") RoomResponseDto roomResponseDto) throws SQLException {
-        roomService.removeRoom(roomResponseDto);
-        return "redirect:/rooms";
+    @DeleteMapping("/remove/{id}")
+    public void removeRoom(@PathVariable Long id) {
+        roomService.removeRoom(id);
     }
 
     @PostMapping("/authorize")
     @ResponseBody
-    public boolean authorize(@Valid @RequestBody AuthorizeDto authorizeDto, Errors errors) {
-//        ModelAndView model = new ModelAndView("index");
-//
-//        if(errors.hasErrors()){
-//            model.addObject("errors", errors);
-//            return model;
-//        }
+    public boolean authorize(@Valid @RequestBody AuthorizeDto authorizeDto) {
         return roomService.authorize(authorizeDto.getId(), authorizeDto.getPassword());
-//        model.addObject("result", result);
-//        if (!result) {
-//            errors.rejectValue("password", "wrongPassword", "wrong Password");
-//            model.addObject("authorizeError", errors.getFieldValue("password"));
-//        }
-//        return model;
-
     }
 }

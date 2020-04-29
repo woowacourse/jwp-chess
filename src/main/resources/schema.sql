1839c71ffb159eba9609ea85994bad61a85f9eef
toneyparky <qkrtmddhks95@gmail.com>
2020-04-29 12:08:09 +0900

    feat: 데이터베이스 스키마 업데이트
    schema.sql 에 game 테이블 정보 추가, history 테이블과 game 테이블 연결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,16 @@
+create table game (
+    id bigint not null auto_increment,
+    uuid varchar(36) not null,
+    name varchar(255) not null,
+    can_continue tinyint(1) default 1 not null,
+    primary key(id)
+);
+
 create table history (
     id bigint not null auto_increment,
     start varchar(2) not null,
     end varchar(2) not null,
-    primary key(id)
+    game_id bigint not null,
+    primary key(id),
+    foreign key(game_id) references game(id)
 );

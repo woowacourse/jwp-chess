da19c9c296bef07b6c72348519a3a77d65759b53
hyochan-kim <summacum94@naver.com>
2020-04-29 10:52:43 +0900

    [refactor] spark에서 사용하던 코드 삭제

--- src/main/java/wooteco/chess/utils/validator/ChessValidator.java
+++
@@ -1,9 +0,0 @@
-package wooteco.chess.utils.validator;
-
-public class ChessValidator {
-    public static void checkChessId(int chessId) {
-        if (chessId <= 0) {
-            throw new IllegalArgumentException("Chess id must be positive number.");
-        }
-    }
-}

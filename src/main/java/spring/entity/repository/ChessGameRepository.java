3f5970009c5063f4faecc66bf05d7555265873ff
Tak Hyungmin <milkho11@naver.com>
2020-04-29 15:17:32 +0900

    [refactor] repository 패키지화

--- src/main/java/spring/entity/repository/ChessGameRepository.java
+++ src/main/java/spring/entity/repository/ChessGameRepository.java
@@ -1,6 +1,7 @@
-package spring.entity;
+package spring.entity.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import spring.entity.ChessGameEntity;
 
 public interface ChessGameRepository extends CrudRepository<ChessGameEntity, Long> {
 }

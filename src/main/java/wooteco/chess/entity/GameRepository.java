a48d90cd75c171f3d5328c0fc6f0c8d38c6bfbad
toneyparky <qkrtmddhks95@gmail.com>
2020-04-29 15:31:33 +0900

    feat: game 업데이트 기능 구현
    GameRepository findByUuid() 추가
    GameRepositoryTest 업데이트 테스트 작성
    Game 어노테이션 컨벤션 수정
    History 어노테이션 컨벤션 수정

--- src/main/java/wooteco/chess/entity/GameRepository.java
+++ src/main/java/wooteco/chess/entity/GameRepository.java
@@ -1,6 +1,8 @@
 package wooteco.chess.entity;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 public interface GameRepository extends CrudRepository<Game, Long> {
 	@Override
 	List<Game> findAll();
+
+	@Query("SELECT * FROM game WHERE uuid = :uuid")
+	Game findByUuid(@Param("uuid") String uuid);
 }

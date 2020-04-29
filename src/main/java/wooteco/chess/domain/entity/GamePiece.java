3e6eb11b09a3319499fc7906e01d4b6bfab222d1
또링 <jnsorn@gmail.com>
2020-04-29 19:31:16 +0900

    [또링] 웹 기반 체스 게임 3단계 구현 (#129)
    * docs : 2단계 README 작성
    
    * refactor : Spring Application으로 변경
    
    * refactor : exception handler를 위한 advice 생성
    
    * test : controller 테스트 추가
    
    * test : controller의 save, load 테스트 추가
    
    * docs : 3단계 README 작성
    
    * feat : entity 생성
    
    * feat : repository 생성
    
    * feat : 3단계 Spring Data JDBC 적용
    
    * refactor : 서비스 코드 개선

---
+++ src/main/java/wooteco/chess/domain/entity/GamePiece.java
@@ -0,0 +1,38 @@
+package wooteco.chess.domain.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("PIECE")
+public class GamePiece {
+	@Id
+	private Long id;
+	private String symbol;
+	private String position;
+	private String team;
+
+	protected GamePiece() {
+	}
+
+	public GamePiece(String symbol, String position, String team) {
+		this.symbol = symbol;
+		this.position = position;
+		this.team = team;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+}
--- src/main/java/wooteco/chess/service/ChessService.java
+++ src/main/java/wooteco/chess/service/ChessService.java
@@ -5,11 +5,10 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
-import wooteco.chess.dao.GameDao;
-import wooteco.chess.dao.PieceDao;
 import wooteco.chess.domain.Score;
 import wooteco.chess.domain.board.ChessGame;
 import wooteco.chess.domain.command.MoveCommand;
+import wooteco.chess.domain.entity.Game;
 import wooteco.chess.domain.piece.Piece;
 import wooteco.chess.domain.piece.position.Position;
 import wooteco.chess.domain.piece.team.Team;
@@ -17,101 +16,61 @@ import wooteco.chess.dto.BoardDto;
 import wooteco.chess.dto.GameDto;
 import wooteco.chess.dto.MoveRequestDto;
 import wooteco.chess.dto.MoveResponseDto;
-import wooteco.chess.dto.PieceDto;
+import wooteco.chess.repository.GameRepository;
 import wooteco.chess.util.ScoreConverter;
 import wooteco.chess.util.UnicodeConverter;
 
 @Service
 public class ChessService {
-	private final PieceDao pieceDao;
-	private final GameDao gameDao;
+	private final GameRepository gameRepository;
 
-	public ChessService(PieceDao pieceDao, GameDao gameDao) {
-		this.pieceDao = pieceDao;
-		this.gameDao = gameDao;
+	public ChessService(GameRepository gameRepository) {
+		this.gameRepository = gameRepository;
 	}
 
 	public BoardDto createGame() {
 		ChessGame chessGame = new ChessGame();
 		Long gameId = saveGame(chessGame)
 			.getId();
-		return savePieces(gameId, chessGame);
+		return createBoardDto(gameId, chessGame);
 	}
 
 	public MoveResponseDto move(MoveRequestDto moveRequestDto) {
-		Long gameId = moveRequestDto.getGameId();
-		String command = moveRequestDto.getCommand();
+		Game game = findGameById(moveRequestDto.getGameId());
+		MoveCommand moveCommand = new MoveCommand(moveRequestDto.getCommand());
 
-		List<Piece> pieces = pieceDao.findAllByGameId(gameId);
-		GameDto gameDto = gameDao.findById(gameId)
-			.orElseThrow(IllegalArgumentException::new);
+		game.move(moveCommand);
+		gameRepository.save(game);
 
-		ChessGame chessGame = new ChessGame(pieces, gameDto.getTurn());
-		MoveCommand moveCommand = new MoveCommand(command);
-		chessGame.move(moveCommand);
+		ChessGame chessGame = game.getChessGame();
 
-		String originalPosition = moveCommand.getSourcePosition().getName();
-		String newPosition = moveCommand.getTargetPosition().getName();
-		updateGame(gameId, chessGame);
-		return updateBoard(chessGame, gameId, originalPosition, newPosition);
+		Position targetPosition = moveCommand.getTargetPosition();
+		Piece piece = chessGame.findPieceByPosition(targetPosition);
+
+		return new MoveResponseDto(game.getId(), chessGame.isKingAlive(), UnicodeConverter.convert(piece.getSymbol()),
+			chessGame.getTurnName());
+	}
+
+	private Game findGameById(Long gameId) {
+		return gameRepository.findById(gameId)
+			.orElseThrow(() -> new IllegalArgumentException(gameId + "은 존재하지 않는 게임입니다."));
 	}
 
 	public BoardDto load(Long gameId) {
-		GameDto gameDto = gameDao.findById(gameId)
-			.orElseThrow(() -> new IllegalArgumentException(String.format("%d : 존재하지 않는 게임입니다.", gameId)));
-		List<Piece> pieces = pieceDao.findAllByGameId(gameId);
-		ChessGame chessGame = new ChessGame(pieces, gameDto.getTurn());
-
+		Game game = findGameById(gameId);
+		ChessGame chessGame = game.getChessGame();
 		return createBoardDto(gameId, chessGame);
 	}
 
 	private GameDto saveGame(ChessGame chessGame) {
-		Team turn = chessGame.getTurn();
-		GameDto gameDto = new GameDto(turn.getName());
-		return gameDao.save(gameDto);
-	}
-
-	private BoardDto savePieces(Long gameId, ChessGame chessGame) {
-		List<Piece> pieces = chessGame.getPieces();
-		for (Piece piece : pieces) {
-			savePiece(gameId, piece);
-		}
-		return createBoardDto(gameId, chessGame);
-	}
-
-	private void updateGame(Long id, ChessGame chessGame) {
-		Team turn = chessGame.getTurn();
-		GameDto gameDto = new GameDto(id, turn.getName());
-		gameDao.update(gameDto);
-	}
-
-	private void savePiece(Long gameId, Piece piece) {
-		Team team = piece.getTeam();
-		String position = piece.getPosition().getName();
-		String symbol = piece.getSymbol();
-		PieceDto pieceDto = new PieceDto(gameId, symbol, team.getName(), position);
-		pieceDao.save(pieceDto);
-	}
-
-	private MoveResponseDto updateBoard(ChessGame chessGame, Long gameId, String originalPosition,
-		String targetPosition) {
-		PieceDto originalPiece = pieceDao.findByGameIdAndPosition(gameId, originalPosition)
-			.orElseThrow(IllegalArgumentException::new);
-		pieceDao.deleteByGameIdAndPosition(gameId, targetPosition);
-		pieceDao.update(originalPiece.getId(), targetPosition);
-		return createMoveResponseDto(gameId, chessGame, Position.of(targetPosition));
-	}
-
-	private MoveResponseDto createMoveResponseDto(Long gameId, ChessGame chessGame, Position position) {
-		Piece piece = chessGame.findPieceByPosition(position);
-
-		return new MoveResponseDto(gameId, chessGame.isKingAlive(), UnicodeConverter.convert(piece.getSymbol()),
-			chessGame.getTurn().getName());
+		Game game = new Game(chessGame);
+		game = gameRepository.save(game);
+		return new GameDto(game);
 	}
 
 	private BoardDto createBoardDto(Long gameId, ChessGame chessGame) {
 		List<String> symbols = UnicodeConverter.convert(chessGame.getReverse());
 		Map<Team, Double> score = Score.calculateScore(chessGame.getPieces(), Team.values());
-		return new BoardDto(gameId, symbols, ScoreConverter.convert(score), chessGame.getTurn().getName());
+		return new BoardDto(gameId, symbols, ScoreConverter.convert(score), chessGame.getTurnName());
 	}
 }

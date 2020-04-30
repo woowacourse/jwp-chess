6f585cca88244d35092ff658ea9ab1b7fbb9bfcf
sujin <sujinemilycho@gmail.com>
2020-04-30 15:11:09 +0900

    refactor: 레벨2 피드백 적용(하비 리뷰)

--- src/main/java/wooteco/chess/controller/SpringChessController.java
+++ src/main/java/wooteco/chess/controller/SpringChessController.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import wooteco.chess.service.SpringChessService;
 
-import java.sql.SQLException;
-import java.util.Map;
-
 @Controller
 public class SpringChessController {
-    private final SpringChessService service;
+    private final SpringChessService springChessService;
 
-    public SpringChessController(SpringChessService service) {
-        this.service = service;
+    public SpringChessController(SpringChessService springChessService) {
+        this.springChessService = springChessService;
     }
 
     @GetMapping("/")
@@ -24,8 +21,8 @@ public class SpringChessController {
     }
 
     @PostMapping("/ready")
-    public String enterGameRoom(@RequestParam(value = "game_id") String gameId, Model model) throws SQLException {
-        boolean canResume = service.canResume(gameId);
+    public String enterGameRoom(@RequestParam(value = "game_id") String gameId, Model model) {
+        boolean canResume = springChessService.canResume(gameId);
 
         model.addAttribute("canResume", canResume);
         model.addAttribute("game_id", gameId);
@@ -33,34 +30,31 @@ public class SpringChessController {
     }
 
     @PostMapping("/play")
-    public String startGame(@RequestParam(value = "game_id") String gameId, Model model) throws SQLException {
-        service.startNewGame(gameId);
+    public String startGame(@RequestParam(value = "game_id") String gameId, Model model) {
+        springChessService.startNewGame(gameId);
 
-        Map<String, Object> gameInfo = service.provideGameInfo(gameId);
-        model.addAllAttributes(gameInfo);
+        model.addAllAttributes(springChessService.provideGameInfo(gameId));
         model.addAttribute("game_id", gameId);
         return "game_room";
     }
 
     @PostMapping("/resume")
-    public String resumeGame(@RequestParam(value = "game_id") String gameId, Model model) throws SQLException {
-        service.resumeGame(gameId);
+    public String resumeGame(@RequestParam(value = "game_id") String gameId, Model model) {
+        springChessService.resumeGame(gameId);
 
-        Map<String, Object> gameInfo = service.provideGameInfo(gameId);
-        model.addAllAttributes(gameInfo);
+        model.addAllAttributes(springChessService.provideGameInfo(gameId));
         model.addAttribute("game_id", gameId);
         return "game_room";
     }
 
     @PostMapping("/move")
     public String move(@RequestParam(value = "game_id") String gameId, @RequestParam String source,
-                       @RequestParam String target, Model model) throws SQLException {
-        service.move(gameId, source, target);
+                       @RequestParam String target, Model model) {
+        springChessService.move(gameId, source, target);
 
-        Map<String, Object> gameInfo = service.provideGameInfo(gameId);
-        model.addAllAttributes(gameInfo);
+        model.addAllAttributes(springChessService.provideGameInfo(gameId));
         model.addAttribute("game_id", gameId);
-        model.addAttribute("end", service.provideWinner(gameId));
+        model.addAttribute("end", springChessService.provideWinner(gameId));
         return "game_room";
     }
 }

b9c15623cd27fbd67ca213b24137865b38619fec
sujin <sujinemilycho@gmail.com>
2020-04-30 17:12:39 +0900

    refactor: 레벨2 피드백 적용(디디 리뷰)
    - 에러 처리: 잘못된 움직임 alert 적용
    - 여러 RequestParam을 map으로 받음
    - /ready 요청 PathVariable 적용

--- src/main/java/wooteco/chess/controller/SpringChessController.java
+++ src/main/java/wooteco/chess/controller/SpringChessController.java
@@ -2,10 +2,10 @@ package wooteco.chess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import wooteco.chess.service.SpringChessService;
+
+import java.util.Map;
 
 @Controller
 public class SpringChessController {
@@ -20,8 +20,8 @@ public class SpringChessController {
         return "index";
     }
 
-    @PostMapping("/ready")
-    public String enterGameRoom(@RequestParam(value = "game_id") String gameId, Model model) {
+    @PostMapping("/ready/{gameId}")
+    public String enterGameRoom(@PathVariable String gameId, Model model) {
         boolean canResume = springChessService.canResume(gameId);
 
         model.addAttribute("canResume", canResume);
@@ -48,13 +48,19 @@ public class SpringChessController {
     }
 
     @PostMapping("/move")
-    public String move(@RequestParam(value = "game_id") String gameId, @RequestParam String source,
-                       @RequestParam String target, Model model) {
-        springChessService.move(gameId, source, target);
+    public String move(@RequestParam Map<String, String> params, Model model) {
+        String gameId = params.get("game_id");
+        springChessService.move(gameId, params.get("source"), params.get("target"));
 
         model.addAllAttributes(springChessService.provideGameInfo(gameId));
         model.addAttribute("game_id", gameId);
         model.addAttribute("end", springChessService.provideWinner(gameId));
+        return "game_room";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String errorMessage(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
         return "game_room";
     }
 }

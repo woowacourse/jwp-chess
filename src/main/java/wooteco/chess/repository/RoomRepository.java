d51c876ad403116ff6ae3f2422ed1c27f8be6184
yelimkim98 <kiel0103@naver.com>
2020-04-30 14:25:09 +0900

    refactor: 컨벤션 체크

--- src/main/java/wooteco/chess/repository/RoomRepository.java
+++ src/main/java/wooteco/chess/repository/RoomRepository.java
@@ -1,15 +1,16 @@
 package wooteco.chess.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
 import wooteco.chess.entity.Room;
 
 public interface RoomRepository extends CrudRepository<Room, Long> {
 
-    @Query("SELECT * FROM room JOIN (SELECT game.room AS game_room, game.turn AS game_turn FROM game) AS tmp ON game_room = room.id WHERE room.name = :name")
+    @Query("SELECT * FROM room JOIN "
+        + "(SELECT game.room AS game_room, game.turn AS game_turn FROM game) AS tmp "
+        + "ON game_room = room.id "
+        + "WHERE room.name = :name")
     Optional<Room> findByName(@Param("name") String name);
 }

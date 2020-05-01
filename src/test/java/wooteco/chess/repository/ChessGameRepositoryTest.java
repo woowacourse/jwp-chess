a61da5cad0aa536dd2c9cc6fbc1590d026cbdcc0
juyeongjung <juyeong.dev@gmail.com>
2020-05-01 16:15:17 +0900

    refactor: 테스트 코드 h2 에서 잘 작동 되도록 수정

--- src/test/java/wooteco/chess/repository/ChessGameRepositoryTest.java
+++ src/test/java/wooteco/chess/repository/ChessGameRepositoryTest.java
@@ -1,16 +1,17 @@
 package wooteco.chess.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 import wooteco.chess.entity.ChessGame;
 import wooteco.chess.exceptions.RoomNotFoundException;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -19,13 +20,18 @@ class ChessGameRepositoryTest {
 	@Autowired
 	private ChessGameRepository chessGameRepository;
 
+	@BeforeEach
+	public void resetDatabase() {
+		chessGameRepository.deleteAll();
+	}
+
 	@Test
 	public void CrudTest() {
 		chessGameRepository.save(
-				new ChessGame("rnbqkbnrppp.pppp....................p...........PPPP.PPPRNBQKBNR", "WHITE", 123456));
+			new ChessGame("rnbqkbnrppp.pppp....................p...........PPPP.PPPRNBQKBNR", "WHITE", 123456));
 
 		ChessGame game = chessGameRepository.findChessGameByRoomNo(123456)
-				.orElseThrow(RoomNotFoundException::new);
+			.orElseThrow(RoomNotFoundException::new);
 
 		assertThat(game.getBoard()).isEqualTo("rnbqkbnrppp.pppp....................p...........PPPP.PPPRNBQKBNR");
 

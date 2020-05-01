664662affc177d2fb9ac8b4cec596882cb2d6672
Andy H. Jung <commaniakr@gmail.com>
2020-05-01 16:29:56 +0900

    refactor: 랜덤 방 번호 생성 부분 로직을 chessGameDao로 옮김

--- src/main/java/wooteco/chess/repository/ChessGameRepository.java
+++ src/main/java/wooteco/chess/repository/ChessGameRepository.java
@@ -1,6 +1,5 @@
 package wooteco.chess.repository;
 
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +10,4 @@ import java.util.Optional;
 public interface ChessGameRepository extends CrudRepository<ChessGame, Long> {
 	@Query("SELECT * FROM chess_game WHERE room_no = :room_no")
 	Optional<ChessGame> findChessGameByRoomNo(@Param("room_no") int roomNo);
-
-	@Modifying
-	@Query("UPDATE chess_game SET board = :board, turn = :turn WHERE room_no = :room_no")
-	void updateChessGame(@Param("board") String serializedBoard, @Param("turn") String turn,
-		@Param("room_no") int roomNo);
 }

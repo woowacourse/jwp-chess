f7df1dd14d70d7ec261f9f0c2e5679676dee7aaf
Tak Hyungmin <milkho11@naver.com>
2020-05-01 15:17:05 +0900

    [refactor] 뷰 리팩토링

--- src/main/resources/static/chess.js
+++ src/main/resources/static/chess.js
@@ -41,84 +41,14 @@ var main = {
     }
 }
 
-function getChessBoard(id) {
-    $.ajax({
-        url: "/start/board",
-        type: "get",
-        data: {id: id},
-        success: function (data) {
-            console.log(JSON.stringify(data));
-            $('.gamecell').html('');
-            $('.gamecell').attr('chess', 'null');
-            $('#game_id').html(id);
-
-            $('.gamecell grey').html('');
-            $('.gamecell grey').attr('chess', 'null');
-
-            var jsonData = JSON.parse(data);
-            for (var i = 0; i < jsonData.boardDto.boardValue.length; i++) {
-                var piece = jsonData.boardDto.boardValue[i];
-                $('#' + piece.location).html(main.variables.pieces[piece.pieceName].img);
-                $('#' + piece.location).attr('chess', piece.pieceName);
-            }
-
-            $('#whiteScore').html("White score : " + jsonData.whiteScore);
-            $('#blackScore').html("Black score : " + jsonData.blackScore);
-
-            var turn = "white";
-            if (jsonData.turnIsBlack === "1") {
-                turn = "black";
-            }
-            $('#turn').html("It's " + turn + " Turn!");
-        },
-        error: function (errorThrown) {
-            alert(JSON.stringify(errorThrown));
-        },
+$(document).ready(function () {
+    $('.gamecell').attr({
+        "draggable": "true",
+        "ondragstart": "drag(event)",
+        "ondrop": "drop(event)",
+        "ondragover": "allowDrop(event)"
     });
-}
-
-// function postNewGame() {
-//     var game_id = document.getElementById("game_id").innerHTML;
-//
-//     $.ajax({
-//             type: 'post',
-//             url: '/start/new/game',
-//             data: {"game_id": game_id.toString()},
-//             success: function (data) {
-//                 $('#game_id').html(game_id);
-//
-//                 $('.gamecell').html('');
-//                 $('.gamecell').attr('chess', 'null');
-//
-//                 $('.gamecell grey').html('');
-//                 $('.gamecell grey').attr('chess', 'null');
-//
-//                 var jsonData = JSON.parse(data);
-//                 for (var i = 0; i < jsonData.boardValue.length; i++) {
-//                     var piece = jsonData.boardValue[i];
-//                     $('#' + piece.location).html(main.variables.pieces[piece.pieceName].img);
-//                     $('#' + piece.location).attr('chess', piece.pieceName);
-//                 }
-//
-//                 $('#turn').html("It's White Turn!");
-//             },
-//             error: function (xhr, status, error) {
-//                 alert(error.toString());
-//             },
-//         }
-//     );
-// }
-
-
-function convertPiece(pieceName) {
-    for (let piecesKey in main.variables.pieces) {
-        if (piecesKey == pieceName) {
-            return piecesKey;
-        }
-    }
-    return null;
-}
-
+});
 
 function allowDrop(ev) {
     // 고유의 동작 제거
@@ -129,62 +59,43 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function getChessGames() {
-    $.ajax({
-        url: "/start/boards",
-        type: "get",
-        success: function (data) {
-            var res = JSON.parse(data);
-            // console.log(JSON.stringify(data));
-            var chessGameVos = res.chessGameVos;
-            for (var i = 0; i < chessGameVos.length; i++) {
-                add(chessGameVos[i].id);
-            }
-        },
-        error: function (errorThrown) {
-
-        },
-    });
-}
-
-function add(id) {
-    var element = document.createElement("input");
-    element.type = "button";
-    element.value = id + "번 체스 게임";
-    element.onclick = function () {
-        getChessBoard(id);
-    };
-
-    var chessBar = document.getElementById("chessBar");
-    chessBar.appendChild(element);
-}
-
-$(document).ready(function () {
-    $('.gamecell').attr({
-        "draggable": "true",
-        "ondragstart": "drag(event)",
-        "ondrop": "drop(event)",
-        "ondragover": "allowDrop(event)"
-    });
-});
-
-function postEndGame() {
+function drop(ev) {
+    ev.preventDefault();
+    var now = ev.dataTransfer.getData("text");
+    var des = ev.target.id;
     var game_id = document.getElementById("game_id").innerHTML;
-
-    $.ajax({
-        type: 'post',
-        url: '/end',
-        data: {"game_id": game_id},
-        dataType: 'text',
-        error: function (xhr, status, error) {
-            alert(error.toString());
-        },
-        success: function (data) {
-        }
-    });
+    move({"game_id": "1", "now": now.toString(), "des": des.toString()});
 }
 
-/////
+// function getChessGames() {
+//     $.ajax({
+//         url: "/start/boards",
+//         type: "get",
+//         success: function (data) {
+//             var res = JSON.parse(data);
+//             // console.log(JSON.stringify(data));
+//             var chessGameVos = res.chessGameVos;
+//             for (var i = 0; i < chessGameVos.length; i++) {
+//                 add(chessGameVos[i].id);
+//             }
+//         },
+//         error: function (errorThrown) {
+//
+//         },
+//     });
+// }
+
+// function add(id) {
+//     var element = document.createElement("input");
+//     element.type = "button";
+//     element.value = id + "번 체스 게임";
+//     element.onclick = function () {
+//         getChessBoard(id);
+//     };
+//
+//     var chessBar = document.getElementById("chessBar");
+//     chessBar.appendChild(element);
+// }
 
 function postNewGame() {
     $.ajax({
@@ -196,7 +107,6 @@ function postNewGame() {
         },
         success: function (data) {
             $('.board').css('display', 'block');
-            // TODO : game_id를 받아와야 합니다.
             $('#game_id').html("1");
 
             $('.gamecell').html('');
@@ -216,15 +126,6 @@ function postNewGame() {
             $('#turn').html("It's White Turn!");
         }
     });
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var now = ev.dataTransfer.getData("text");
-    var des = ev.target.id;
-    var game_id = document.getElementById("game_id").innerHTML;
-    console.log("이건 로그" + game_id + " " + now + " " + des);
-    move({"game_id": "1", "now": now.toString(), "des": des.toString()});
 }
 
 function move(json) {
@@ -258,14 +159,12 @@ function move(json) {
                 getChessBoardResult();
                 deleteChessGame();
             }
-            // alert(JSON.stringify(jsonData));
         }
     });
 }
 
 function getChessBoardResult() {
     const id = document.getElementById("game_id").innerHTML;
-    console.log("아이디는 " + id);
     $.ajax({
         url: "/api/games/" + id + "/result",
         type: "get",
@@ -298,10 +197,9 @@ function deleteChessGame() {
 
 function resume() {
     $.ajax({
-        url: "/api/resume",
+        url: "/api/game",
         type: "get",
         success: function (data) {
-            console.log(JSON.stringify(data));
             $('.board').css('display', 'block');
             $('.gamecell').html('');
             $('.gamecell').attr('chess', 'null');

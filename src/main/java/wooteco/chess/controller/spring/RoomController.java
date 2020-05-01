d41d9e1c6f8ff9ac61afb1a3b939f6bca3c94912
moonyoungCHAE <xpf_fl@naver.com>
2020-05-01 17:20:41 +0900

    feat: 방 목록 출력

--- src/main/java/wooteco/chess/controller/spring/RoomController.java
+++ src/main/java/wooteco/chess/controller/spring/RoomController.java
@@ -5,20 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import wooteco.chess.db.entity.PlayerEntity;
+import wooteco.chess.db.entity.RoomEntity;
 import wooteco.chess.dto.RoomDto;
 import wooteco.chess.dto.req.PlayersDto;
 import wooteco.chess.service.spring.SpringGameService;
 import wooteco.chess.service.spring.SpringPlayerService;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/room")
 public class RoomController {
 	private final SpringPlayerService playerService;
-	private final SpringGameService boardService;
+	private final SpringGameService gameService;
 
-	public RoomController(SpringPlayerService playerService, SpringGameService boardService) {
+	public RoomController(SpringPlayerService playerService, SpringGameService gameService) {
 		this.playerService = playerService;
-		this.boardService = boardService;
+		this.gameService = gameService;
 	}
 
 	@GetMapping
@@ -30,11 +33,15 @@ public class RoomController {
 	@ResponseBody
 	public ResponseEntity<Object> createNewGame(@RequestBody PlayersDto playersDto) {
 		try {
-			long roomId = createRoom(playersDto);
-			boardService.createBoard(roomId);
+			long player1Id = playerService.save(
+					new PlayerEntity(playersDto.getPlayer1Name(), playersDto.getPlayer1Password(), "white"));
+			long player2Id = playerService.save(
+					new PlayerEntity(playersDto.getPlayer2Name(), playersDto.getPlayer2Password(), "black"));
+			long roomId = gameService.createRoom(player1Id, player2Id);
+			gameService.createBoard(roomId);
 			return ResponseEntity
-				.status(200)
-				.body(new RoomDto(roomId, playersDto.getPlayer1Name(), playersDto.getPlayer2Name()));
+					.status(200)
+					.body(new RoomDto(roomId, playersDto.getPlayer1Name(), playersDto.getPlayer2Name()));
 		} catch (Exception e) {
 			return ResponseEntity.status(400).body(e.getMessage());
 		}
@@ -46,11 +53,12 @@ public class RoomController {
 		return "play";
 	}
 
-	private long createRoom(PlayersDto playersDto) {
-		long player1Id = playerService.save(
-			new PlayerEntity(playersDto.getPlayer1Name(), playersDto.getPlayer1Password(), "white"));
-		long player2Id = playerService.save(
-			new PlayerEntity(playersDto.getPlayer2Name(), playersDto.getPlayer2Password(), "black"));
-		return boardService.createRoom(player1Id, player2Id);
+	@GetMapping("/all")
+	public ResponseEntity<List<RoomEntity>> showRooms() {
+		List<RoomEntity> list = gameService.getRooms();
+		for (RoomEntity roomEntity : list) {
+			System.out.println(roomEntity.getId());
+		}
+		return ResponseEntity.ok(gameService.getRooms());
 	}
 }

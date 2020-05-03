2027c38ca28c44b2990325e4bfe5f037126162b9
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-05-03 14:52:41 +0900

    refactor : DB 이동, 환경 재설정

--- src/main/java/wooteco/chess/db/MoveHistoryRepository.java
+++ src/main/java/wooteco/chess/db/MoveHistoryRepository.java
@@ -1,23 +1,23 @@
 package wooteco.chess.db;
 
+import java.util.Optional;
+
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface MoveHistoryRepository extends CrudRepository<MoveHistory, String> {
-
     @Modifying
     @Query("INSERT INTO move_history (game_id, moves, team, source_position, target_position) " +
-            "VALUES (:gameId, (SELECT IFNULL(MAX(moves) + 1, 1) FROM move_history AS INNERTABLE WHERE game_id = :gameId), :team, :source, :target)")
-    void addMoveHistory(String gameId, String team, String source, String target);
-
+        "VALUES (:gameId, (SELECT IFNULL(MAX(moves) + 1, 1) FROM move_history AS INNERTABLE WHERE game_id = :gameId), :team, :source, :target)")
+    void addMoveHistory(@Param("gameId") String gameId, @Param("team") String team, @Param("source") String source,
+        @Param("target") String target);
 
     @Query("SELECT team FROM move_history WHERE game_id = :gameId ORDER BY moves DESC LIMIT 1")
-    Optional<String> findLastTurn(String gameId);
+    Optional<String> findLastTurn(@Param("gameId") String gameId);
 
     @Override
     void deleteById(String gameId);

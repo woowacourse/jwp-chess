db99f69e49ea60ef9643255a0c1043665b26fa11
hyochan-kim <summacum94@naver.com>
2020-05-04 14:22:08 +0900

    [refactor] requestDto 생성에 따른 html/js 수정

---
+++ src/main/resources/public/index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    $('.btn-makeRoom').click(async function () {
+        $.ajax({
+            type: "POST",
+            url: "/room/create",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                roomName: $('#makeRoomName').val(),
+                userPassword: $('#creatorPassword').val()
+            }),
+            success: function (data) {
+                if(data.responseCode ==200) {
+                    roomId = data.responseData
+                    window.location.href = "board.html?id=" + roomId + "?password=" + $('#creatorPassword').val()
+                }
+                else {
+                    alert(data.message)
+                }
+            },
+            error: function (e) {
+                console.log(e.message);
+            }
+        });
+    });
+
+    $('.btn-joinRoom').click(async function () {
+        $.ajax({
+            type: "POST",
+            url: "/room/join",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                roomName: $('#joinRoomName').val(),
+                userPassword: $('#joinerPassword').val()
+            }),
+            success: function (data) {
+                roomId = data.responseData
+                window.location.href = "board.html?id=" + roomId + "?password=" + $('#joinerPassword').val()
+            },
+            error: function (e) {
+                console.log(e.message);
+            }
+        });
+    });
+});

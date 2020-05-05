6d1f25447663c39563280cd11492fd3d38d3a7e1
KIMSIYOUNG <49060374+KIMSIYOUNG@users.noreply.github.com>
2020-05-05 23:04:33 +0900

    [카일] 체스 4단계 제출합니다. (#193)
    * feat : 방 목록 보여주는 로직 구현
    
    * feat : 화면 출력 부분 수정

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -2,5 +2,14 @@ function on_games() {
   fetch("http://localhost:4567/room/games").then(response => response.json()).then(playersDto => {
     console.log(playersDto.player1Name);
     console.log(playersDto);
+    const point = document.querySelector(".rooms")
+    console.log(point);
+
+    let result = "";
+    for (let i = 0; i < playersDto.length; i++) {
+      console.log(playersDto[i].name)
+      result += `<li onclick="location.href = 'http://localhost:4567/room/' + ${playersDto[i].id}"> ${playersDto[i].name} </li>`;
+    }
+    point.innerHTML = result;
   })
 }

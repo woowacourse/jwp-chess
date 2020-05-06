06ac56e204dcd49294cef2d2fab1d0a65a609e8c
moon <mjj920930@gmail.com>
2020-05-06 22:09:12 +0900

    feat : DB에 GameRoom이 존재하지 않는 경우 예외 발생 기능 구현

--- src/main/java/wooteco/chess/database/GameRoomRepository.java
+++ src/main/java/wooteco/chess/database/GameRoomRepository.java
@@ -1,11 +1,13 @@
 package wooteco.chess.database;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import wooteco.chess.entity.GameRoom;
 
-import java.util.List;
+import wooteco.chess.entity.GameRoom;
 
 public interface GameRoomRepository extends CrudRepository<GameRoom, Long> {
 
@@ -13,6 +15,6 @@ public interface GameRoomRepository extends CrudRepository<GameRoom, Long> {
 	List<GameRoom> findAll();
 
 	@Query("SELECT * FROM game_room WHERE name = :name")
-	GameRoom findByName(@Param("name") final String name);
+	Optional<GameRoom> findByName(@Param("name") final String name);
 
 }
--- src/test/java/wooteco/chess/database/GameRoomRepositoryTest.java
+++ src/test/java/wooteco/chess/database/GameRoomRepositoryTest.java
@@ -1,5 +1,9 @@
 package wooteco.chess.database;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +12,8 @@ import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import wooteco.chess.entity.GameRoom;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import wooteco.chess.entity.GameRoom;
 
 @ExtendWith(SpringExtension.class)
 @DataJdbcTest
@@ -27,7 +29,8 @@ class GameRoomRepositoryTest {
 		String gameRoomName = "test";
 		GameRoom savedGameRoom = gameRoomRepository.save(new GameRoom(gameRoomName));
 
-		GameRoom expected = gameRoomRepository.findByName(gameRoomName);
+		GameRoom expected = gameRoomRepository.findByName(gameRoomName)
+			.orElseThrow(() -> new NoSuchElementException("해당 이름을 가진 게임방이 존재하지 않습니다."));
 		assertThat(savedGameRoom.getName()).isEqualTo(expected.getName());
 	}
 

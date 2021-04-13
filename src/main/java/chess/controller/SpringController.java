2628ae729983b6cebb8faaeb3a6862af0e3b75fa
joanne <mathmjseo@naver.com>
2021-04-13 17:34:46 +0900

    feat: get으로 들어오는 요청 처리

--- src/main/java/chess/controller/SpringController.java
+++ src/main/java/chess/controller/SpringController.java
@@ -6,9 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
@@ -26,6 +24,33 @@ public class SpringController {
     @GetMapping("")
     public String play(Model model) throws SQLException {
         model.addAllAttributes(ModelView.startResponse(chessService.loadHistory()));
+        return "play";
+    }
+
+    @GetMapping("/new")
+    public String playNewGameWithNoSave(Model model) {
+        model.addAllAttributes(ModelView.newGameResponse(chessService.initialGameInfo()));
+        return "chessGame";
+    }
+
+    @GetMapping("/{name}/new")
+    public String playNewGameWithSave(Model model, @PathVariable String name) throws SQLException {
+        model.addAllAttributes(ModelView.newGameResponse(
+                chessService.initialGameInfo(),
+                chessService.addHistory(name)
+        ));
+        return "chessGame";
+    }
+
+    @GetMapping("/continue")
+    public String continueGame(Model model, @RequestParam("name") String name) throws SQLException {
+        final String id = chessService.getIdByName(name);
+        model.addAllAttributes(ModelView.commonResponseForm(chessService.continuedGameInfo(id), id));
+        return "chessGame";
+    }
+
+    @GetMapping("/end")
+    public String endGame() {
         return "play";
     }
 }

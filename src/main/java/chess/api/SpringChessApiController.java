21a308df8bb1f6c6eb9237fb679530079bd4fd1a
rok93 <goodboy302@naver.com>
2021-04-14 15:21:43 +0900

    feat: 체스게임을 종료하는 API 기능

--- src/main/java/chess/api/SpringChessApiController.java
+++ src/main/java/chess/api/SpringChessApiController.java
@@ -3,9 +3,9 @@ package chess.api;
 import chess.domain.piece.Position;
 import chess.service.ChessGameService;
 import chess.view.dto.ChessGameDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import chess.view.dto.ScoreDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SpringChessApiController {

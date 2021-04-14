777d7dabdd78241f832fe4ad2a3dee9a4b1d5bb0
Jinhong <xlffm3@gmail.com>
2021-04-14 14:40:27 +0900

    refactor: controller test에 응답 메시지 검증 기능 추가

--- src/test/java/chess/spring/controller/ChessControllerTest.java
+++ src/test/java/chess/spring/controller/ChessControllerTest.java
@@ -1,10 +1,17 @@
 package chess.spring.controller;
 
+import chess.domain.board.ChessBoard;
+import chess.domain.piece.TeamType;
 import chess.dto.MoveRequestDTO;
+import chess.dto.board.BoardDTO;
+import chess.spring.service.ChessService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -12,32 +19,38 @@ import org.springframework.http.MediaType;
 
 import static org.hamcrest.core.Is.is;
 
-@DisplayName("ChessController Method")
+@DisplayName("ChessController HTTP Method Test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChessControllerTest {
 
     @LocalServerPort
     int port;
 
+    @Autowired
+    private ChessService chessService;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
     }
 
-    @DisplayName("보드를 가져온다.")
+    @DisplayName("보드를 조회한다.")
     @Test
-    void showBoard() {
+    void showBoard() throws JsonProcessingException {
+
+
         RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/chessgame/show")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
-                .body("size()", is(3));
+                .body("size()", is(3))
+                .body(is(parseExpectedResponse()));
     }
 
     @DisplayName("체스 기물을 이동한다.")
     @Test
-    void move() {
+    void move() throws JsonProcessingException {
         MoveRequestDTO moveRequestDTO = new MoveRequestDTO("a2", "a3", "WHITE");
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -45,6 +58,14 @@ class ChessControllerTest {
                 .when().post("/chessgame/move")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
-                .body("size()", is(3));
+                .body("size()", is(3))
+                .body(is(parseExpectedResponse()));
+    }
+
+    private String parseExpectedResponse() throws JsonProcessingException {
+        ChessBoard chessBoard = chessService.findChessBoard();
+        TeamType currentTeamType = chessService.findCurrentTeamType();
+        BoardDTO boardDTO = BoardDTO.of(chessBoard, currentTeamType);
+        return new ObjectMapper().writeValueAsString(boardDTO);
     }
 }

089732bd0011f5c0d9ab9b188757357085f899e1
gimjinhwan <ecsimsw@gmail.com>
2021-04-14 21:49:23 +0900

    feat : ExceptionHandler 정의

--- src/main/java/chess/controller/web/MainController.java
+++ src/main/java/chess/controller/web/MainController.java
@@ -1,46 +1,17 @@
-//package chess.controller.web;
-//
-//import chess.view.OutputView;
-//import org.springframework.stereotype.Controller;
-//
-//import java.sql.SQLException;
-//
-//import static spark.Spark.exception;
-//import static spark.Spark.get;
-//
-//@Controller
-//public class MainController {
-//
-//    public MainController() {
-//    }
-//
-//    public void mapping() {
-//        handlingSQLException();
-//        handlingIllegalArgumentException();
-//        handlingPageNotFoundError();
-//    }
-//
-//    private void handlingSQLException() {
-//        exception(SQLException.class, (e, req, res) -> {
-//            res.status(404);
-//            res.body("SQL error : " + e.getMessage());
-//        });
-//    }
-//
-//    private void handlingIllegalArgumentException() {
-//        exception(IllegalArgumentException.class, (e, req, res) -> {
-//            res.status(404);
-//            res.body("Unexpected error : " + e.getMessage());
-//        });
-//    }
-//
-//    private void handlingPageNotFoundError() {
-//        get("*", (req, res) -> {
-//            if (req.pathInfo().startsWith("/static")) {
-//                return null;
-//            }
-//            res.status(404);
-////            return OutputView.printErrorMessage("Page not found");
-//        });
-//    }
-//}
+package chess.controller.web;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class MainController {
+
+    public MainController() { }
+
+    @ExceptionHandler({Exception.class})
+    public String handlingException(Exception e, Model model) {
+        model.addAttribute("msg", e.getMessage());
+        return "errorMessage";
+    }
+}

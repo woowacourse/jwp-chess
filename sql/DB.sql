cb86291614d78bca57f23ec0f2a1f9215f31e676
joanne <mathmjseo@naver.com>
2021-04-14 13:46:29 +0900

    feat: sql 수정

--- sql/DB.sql
+++ sql/DB.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS `History`;
+
+CREATE TABLE `History` (
+                           `history_id` int not null auto_increment,
+                           `name` varchar(100) not null,
+                           `is_end` boolean not null default false,
+                           PRIMARY KEY (`history_id`)
+) ENGINE=InnoDB;
+
 DROP TABLE IF EXISTS `Command`;
 
 CREATE TABLE `Command` (
@@ -6,13 +15,4 @@ CREATE TABLE `Command` (
                            `history_id` int not null,
                            PRIMARY KEY (`command_id`),
                            FOREIGN KEY (`history_id`) REFERENCES History(history_id)
-) ENGINE=InnoDB;
-
-DROP TABLE IF EXISTS `History`;
-
-CREATE TABLE `History` (
-                           `history_id` int not null auto_increment,
-                           `name` varchar(100) not null,
-                           `is_end` boolean not null default false,
-                           PRIMARY KEY (`history_id`)
 ) ENGINE=InnoDB;

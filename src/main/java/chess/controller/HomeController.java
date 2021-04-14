71d41da33d6407fde4fe97bff1032db000010c4a
cjy17 <cjy1705@naver.com>
2021-04-14 14:26:37 +0900

    feat : 홈화면 컨트롤러 생성

---
+++ src/main/java/chess/controller/HomeController.java
@@ -0,0 +1,13 @@
+package chess.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}

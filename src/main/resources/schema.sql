8448669f6fdd2f552d20d00f29d12aad6491436f
Jinhong <xlffm3@gmail.com>
2021-04-14 19:36:26 +0900

    feat: 초기 테이블 ddl 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS History
+(
+    ID
+    INT
+    NOT
+    NULL
+    AUTO_INCREMENT,
+    SOURCE
+    VARCHAR
+(
+    255
+),
+    DESTINATION VARCHAR
+(
+    255
+),
+    TEAM_TYPE VARCHAR
+(
+    255
+),
+    PRIMARY KEY
+(
+    ID
+)
+    );

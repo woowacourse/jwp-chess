e96cd9353ce6f79e28a7e8251898940ff9a9505c
rok93 <goodboy302@naver.com>
2021-04-14 13:29:47 +0900

    feat: 체스게임을 생성하는 API 기능

--- src/main/java/chess/api/SpringChessApiController.java
+++ src/main/java/chess/api/SpringChessApiController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 public class SpringChessApiController {
 
@@ -27,6 +29,12 @@ public class SpringChessApiController {
     @GetMapping("/chessgames")
     public ChessGameDto findChessGame() {
         return chessGameService.findLatestPlayingGame();
+    }
+
+    @PostMapping("/chessgames")
+    public ResponseEntity<ChessGameDto> createNewChessGame() {
+        ChessGameDto newChessGame = chessGameService.createNewChessGame();
+        return ResponseEntity.ok(newChessGame);
     }
 
 }

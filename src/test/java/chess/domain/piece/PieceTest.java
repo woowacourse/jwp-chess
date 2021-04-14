e20a4a362b670afead867a27be557a7d9862b827
TaeJeong Kim <xowjd41@naver.com>
2021-04-14 19:35:11 +0900

    fix(PieceTest): 에러 나는 부분 수정

--- src/test/java/chess/domain/piece/PieceTest.java
+++ src/test/java/chess/domain/piece/PieceTest.java
@@ -11,7 +11,7 @@ class PieceTest {
     @Test
     @DisplayName("색에 따라 이름이 정해지는지 확인")
     void create() {
-        final Piece piece = new King(Color.BLACK, new Position("2", "a"));
+        final Piece piece = new King(Color.BLACK, new Position("a", "2"));
         assertThat(piece.color()).isEqualTo(Color.BLACK);
         assertThat(piece.name()).isEqualTo("K");
     }
@@ -19,18 +19,18 @@ class PieceTest {
     @Test
     @DisplayName("정상 위치 생성 확인")
     void position() {
-        final Pawn pawn = new Pawn(Color.BLACK, new Position("2", "a"));
-        assertThat(pawn.positionInfo()).isEqualTo("2a");
+        final Pawn pawn = new Pawn(Color.BLACK, new Position("a", "2"));
+        assertThat(pawn.positionInfo()).isEqualTo("a2");
     }
 
     @Test
     @DisplayName("없는 위치로 객체를 생성하려 하면 에러 발생")
     void positionException() {
-        assertThatThrownBy( () ->  new Pawn(Color.BLACK, new Position("9", "a")))
+        assertThatThrownBy( () ->  new Pawn(Color.BLACK, new Position("a", "9")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당하는 Rank 위치를 찾을 수 없습니다.");
 
-        assertThatThrownBy( () ->  new Pawn(Color.BLACK, new Position("1", "z")))
+        assertThatThrownBy( () ->  new Pawn(Color.BLACK, new Position("z", "1")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당하는 File 위치를 찾을 수 없습니다.");
     }

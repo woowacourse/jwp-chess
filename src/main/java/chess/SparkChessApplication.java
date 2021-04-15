f959cdcab830c7fe924f76ba16879e4a19ec6e80
Jinho <jh8579@gmail.com>
2021-04-16 00:32:47 +0900

    refactor : application 구분
    # Conflicts:
    #	src/main/java/chess/SparkChessApplication.java

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -9,13 +9,11 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class SparkChessApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.hbs");
-        });
-    }
-
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+//        port(8080);
+//        staticFiles.location("/public");
+//        ChessService chessService = new ChessService(new GameDao(), new GameHistoryDao());
+//        UserService userService = new UserService(new UserDao());
+//        SparkWebController sparkWebController = new SparkWebController(chessService, userService);
+//        sparkWebController.run();
     }
 }

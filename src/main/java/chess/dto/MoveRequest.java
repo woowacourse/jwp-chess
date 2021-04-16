7eb818446cf5e3c77b22069fea9c3b79eb821246
RinSabbit <celena1004@naver.com>
2021-04-16 21:31:53 +0900

    refactor: ConstructorProperties 사용. 역직렬화를 위한 빈 생성자 제거

--- src/main/java/chess/dto/MoveRequest.java
+++ src/main/java/chess/dto/MoveRequest.java
@@ -1,6 +1,7 @@
 package chess.dto;
 
 
+import chess.domain.position.Position;
 import java.beans.ConstructorProperties;
 
 public class MoveRequest {
@@ -8,17 +9,17 @@ public class MoveRequest {
     private final String from;
     private final String to;
 
-    @ConstructorProperties({"from", "to"})
+    @ConstructorProperties({"from","to"})
     public MoveRequest(final String from, final String to) {
         this.from = from;
         this.to = to;
     }
 
-    public String getFrom() {
-        return from;
+    public Position getFrom() {
+        return Position.of(from);
     }
 
-    public String getTo() {
-        return to;
+    public Position getTo() {
+        return Position.of(to);
     }
 }

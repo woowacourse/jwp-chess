95ad94f70a88f82f8e59020ed6d1b11ae1a0e1b9
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-04-16 16:25:22 +0900

    refactor: @Controller -> @RestController

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -2,6 +2,8 @@ package chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ChessApplication {
--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -6,12 +6,9 @@ import chess.domain.dto.MoveInfoDto;
 import chess.domain.dto.ScoreDto;
 import chess.service.ChessService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ChessController {
     private final ChessService chessService;
     private final ChessGame chessGame;
@@ -21,31 +18,22 @@ public class ChessController {
         this.chessGame = new ChessGame();
     }
 
-    @GetMapping("/")
-    public String start() {
-        return "chess.html";
-    }
-
     @GetMapping("/loadSavedBoard")
-    @ResponseBody
     public BoardDto loadSavedBoard() {
         return chessService.getSavedBoardInfo(chessGame);
     }
 
     @GetMapping("/resetBoard")
-    @ResponseBody
     public BoardDto resetBoard() {
         return chessService.initiateBoard(chessGame);
     }
 
     @GetMapping("/scoreStatus")
-    @ResponseBody
     public ScoreDto scoreStatus() {
         return ScoreDto.of(chessGame.scoreStatus());
     }
 
     @PostMapping("/move")
-    @ResponseBody
     public BoardDto move(@RequestBody MoveInfoDto moveInfoDTO) {
         return chessService.move(chessGame, moveInfoDTO);
     }

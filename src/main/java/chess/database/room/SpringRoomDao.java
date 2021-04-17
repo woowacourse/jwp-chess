e6b9a020a4ac7e903a6c1f5eb8c17c2a2b2dd6cd
sakjung <ssangyu123@gmail.com>
2021-04-17 12:02:07 +0900

    test: springRoomDao에 관한 jdbcTest 추가

--- src/main/java/chess/database/room/SpringRoomDao.java
+++ src/main/java/chess/database/room/SpringRoomDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Repository
 public class SpringRoomDao {
+    public static final String NO_SUCH_ROOM_NAME_ERROR = "존재하지 않는 방 이름입니다.";
     private JdbcTemplate jdbcTemplate;
 
     public SpringRoomDao(JdbcTemplate jdbcTemplate) {
@@ -16,7 +17,7 @@ public class SpringRoomDao {
     }
 
     public void addRoom(Room room) {
-        String query = "INSERT INTO room (name, turn, state) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE turn = VALUES(turn), state = VALUES(state)";
+        String query = "INSERT INTO rooms (name, turn, state) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE turn = VALUES(turn), state = VALUES(state)";
 
         jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(query);
@@ -28,26 +29,29 @@ public class SpringRoomDao {
     }
 
     public Room findByRoomName(String name) {
-        String query = "SELECT * FROM room WHERE name = ?";
+        String query = "SELECT * FROM rooms WHERE name = ?";
         return jdbcTemplate.queryForObject(
                 query,
-                (resultSet, rowNum) -> new Room(
-                        resultSet.getString("name"),
-                        resultSet.getString("turn"),
-                        JsonConverter.toJsonObject(resultSet.getString("state"))),
+                (resultSet, rowNum) -> {
+                    System.out.println(resultSet.getString("state"));
+                    return new Room(
+                            resultSet.getString("name"),
+                            resultSet.getString("turn"),
+                            JsonConverter.toJsonObject(resultSet.getString("state")));
+                },
                 name);
     }
 
     public void validateRoomExistence(String name) {
-        String query = "SELECT COUNT(*) FROM room WHERE name = ?";
+        String query = "SELECT COUNT(*) FROM rooms WHERE name = ?";
         int result = jdbcTemplate.queryForObject(query, Integer.class, name);
         if (result == 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NO_SUCH_ROOM_NAME_ERROR);
         }
     }
 
     public List<String> getAllRoom() {
-        String query = "SELECT name FROM room";
+        String query = "SELECT name FROM rooms";
         return jdbcTemplate.query(query, (resultSet, rowNum) -> resultSet.getString("name"));
     }
 }

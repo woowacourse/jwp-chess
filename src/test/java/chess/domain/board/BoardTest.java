34010f239dba35825b4dd28aaf5e98cf28cb965d
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-17 16:09:04 +0900

    refactor: King이 잡혔는지 확인하는 테스트 수정

--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -1,5 +1,6 @@
 package chess.domain.board;
 
+import static chess.utils.TestFixture.TEST_TITLE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,7 +8,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import chess.domain.board.move.MoveRequest;
 import chess.domain.color.type.TeamColor;
 import chess.domain.position.Position;
+import java.sql.SQLException;
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
@@ -48,5 +52,60 @@ class BoardTest {
         assertThatThrownBy(() -> board.movePiece(moveRequest))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("자신의 기물이 아닙니다.");
+    }
+
+    @DisplayName("King이 잡혔는지 확인")
+    @Nested
+    class KingDead {
+        @DisplayName("King이 1개만 잡혔을 때")
+        @Test
+        void isOneKingDead() {
+            Board board = new Board(""
+                + ".KR....."
+                + "P.PB...."
+                + ".P..Q..."
+                + "........"
+                + ".....nq."
+                + ".....p.p"
+                + ".....pp."
+                + "....r..."
+            );
+
+            assertThat(board.isKingDead()).isTrue();
+        }
+
+        @DisplayName("2개의 킹들이 모두 잡혔을 때")
+        @Test
+        void isAllKingsDead() {
+            Board board = new Board(""
+                + "..R....."
+                + "P.PB...."
+                + ".P..Q..."
+                + "........"
+                + ".....nq."
+                + ".....p.p"
+                + ".....pp."
+                + "....r..."
+            );
+
+            assertThat(board.isKingDead()).isTrue();
+        }
+
+        @DisplayName("King이 잡히지 않았을 때")
+        @Test
+        void isNotKingDead() {
+            Board board = new Board(""
+                + ".KR....."
+                + "P.PB...."
+                + ".P..Q..."
+                + "........"
+                + ".....nq."
+                + ".....p.p"
+                + ".....pp."
+                + "....rk.."
+            );
+
+            assertThat(board.isKingDead()).isFalse();
+        }
     }
 }

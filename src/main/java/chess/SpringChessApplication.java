4198026ebe77e85d806126e78fa833db3ff21781
멍토 <daum7766@naver.com>
2021-04-18 01:34:29 +0900

    refactor: 컨트롤러 분리

--- src/main/java/chess/SpringChessApplication.java
+++ src/main/java/chess/SpringChessApplication.java
@@ -2,24 +2,11 @@ package chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class SpringChessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringChessApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String index() {
-        return "index.html";
-    }
-
-    @GetMapping("/room/**")
-    public String enter() {
-        return "/game.html";
     }
 }

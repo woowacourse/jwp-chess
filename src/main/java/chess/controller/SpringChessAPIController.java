f5808d7e3233437b8a975aa9a4e8a7253897c875
멍토 <daum7766@naver.com>
2021-04-18 01:32:47 +0900

    refactor: 클래스명 변경

--- src/main/java/chess/controller/SpringChessAPIController.java
+++ src/main/java/chess/controller/SpringChessAPIController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class SpringChessController {
+public class SpringChessAPIController {
     @Autowired
     private ChessService chessService;
 

5f5dc88979e62013479fcfca2840617c2aa5da9b
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-18 20:01:05 +0900

    test : UserController API 테스트 추가

---
+++ src/test/java/chess/controller/web/UserControllerTest.java
@@ -0,0 +1,20 @@
+package chess.controller.web;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.hamcrest.Matchers.*;
+
+class UserControllerTest {
+    @DisplayName("활성화 중인 게임을 가져올 수 있는지 확인")
+    @Test
+    void getGamesTest() {
+        RestAssured
+                .when().get("/user")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .assertThat().body("runningGames", not(nullValue()));
+    }
+}

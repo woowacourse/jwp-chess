0d0352ebe5a1498aec6fe792373090aae2f17a81
Gomding <backchida@gmail.com>
2021-04-18 21:37:56 +0900

    refactor(SpringWebChessController.java) : 클래스명 오타 수정 SpringWebChessContoller -> SpringWebChessController
    contoller 에서 빼먹은 r을 추가

--- src/main/java/chess/controller/web/SpringWebChessController.java
+++ src/main/java/chess/controller/web/SpringWebChessController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class SpringWebChessContoller {
+public class SpringWebChessController {
 
     private final ChessService chessService;
 
-    public SpringWebChessContoller(ChessService chessService) {
+    public SpringWebChessController(ChessService chessService) {
         this.chessService = chessService;
     }
 

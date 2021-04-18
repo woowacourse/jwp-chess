586189e8979b9b3a15952d53efcbebb44003cb2b
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-04-18 19:18:35 +0900

    refactor(SpringChessService): 가독성을 위한 공백 추가

--- src/main/java/chess/service/SpringChessService.java
+++ src/main/java/chess/service/SpringChessService.java
@@ -36,29 +36,30 @@ public class SpringChessService {
     public ChessGameDto move(final String start, final String destination) {
         final ChessGame chessGame = springChessGameDao.readChessGame();
         chessGame.move(Position.of(start), Position.of(destination));
-        springChessGameDao.updateChessGame(chessGame, currentTurnTeamToString(chessGame));
+        springChessGameDao.updateChessGame(chessGame, currentTurnTeamAsString(chessGame));
         return generateChessGameDto(chessGame);
     }
 
     private ChessGameDto generateChessGameDto(final ChessGame chessGame) {
-        final Map<String, Map<String, String>> piecePositionToString = generatePiecePositionToString(chessGame);
-        final String currentTurnTeam = currentTurnTeamToString(chessGame);
+        final Map<String, Map<String, String>> piecePositionAsString = generatePiecePositionAsString(chessGame);
+
         final Map<String, Double> teamScore = new HashMap<>();
         teamScore.put(WHITE_TEAM.asDtoFormat(), chessGame.calculateWhiteTeamScore());
         teamScore.put(BLACK_TEAM.asDtoFormat(), chessGame.calculateBlackTeamScore());
-        final boolean isPlaying = chessGame.isPlaying();
-        return new ChessGameDto(piecePositionToString, currentTurnTeam, teamScore, isPlaying);
+
+        return new ChessGameDto(piecePositionAsString, currentTurnTeamAsString(chessGame), teamScore, chessGame.isPlaying());
     }
 
-
-    private Map<String, Map<String, String>> generatePiecePositionToString(final ChessGame chessGame) {
+    private Map<String, Map<String, String>> generatePiecePositionAsString(final ChessGame chessGame) {
         final Map<String, Map<String, String>> piecePosition = new HashMap<>();
-        piecePosition.put(WHITE_TEAM.asDtoFormat(), generatePiecePositionByTeamToString(chessGame.currentWhitePiecePosition()));
-        piecePosition.put(BLACK_TEAM.asDtoFormat(), generatePiecePositionByTeamToString(chessGame.currentBlackPiecePosition()));
+        final Map<Position, Piece> whitePiecePosition = chessGame.currentWhitePiecePosition();
+        piecePosition.put(WHITE_TEAM.asDtoFormat(), generateTeamPiecePositionAsString(whitePiecePosition));
+        final Map<Position, Piece> blackPiecePosition = chessGame.currentBlackPiecePosition();
+        piecePosition.put(BLACK_TEAM.asDtoFormat(), generateTeamPiecePositionAsString(blackPiecePosition));
         return piecePosition;
     }
 
-    private Map<String, String> generatePiecePositionByTeamToString(final Map<Position, Piece> piecePosition) {
+    private Map<String, String> generateTeamPiecePositionAsString(final Map<Position, Piece> piecePosition) {
         final Map<String, String> piecePositionConverted = new HashMap<>();
         for (Position position : piecePosition.keySet()) {
             final String positionInitial = position.getPositionInitial();
@@ -69,7 +70,7 @@ public class SpringChessService {
         return piecePositionConverted;
     }
 
-    private String currentTurnTeamToString(final ChessGame chessGame) {
+    private String currentTurnTeamAsString(final ChessGame chessGame) {
         if (chessGame.isWhiteTeamTurn()) {
             return WHITE_TEAM.asDaoFormat();
         }

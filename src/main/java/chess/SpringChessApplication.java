f83f2cea08a23f7bcfbeef358efadec7e9e7cbfb
jaeseongDev <qkrwotjd1445@naver.com>
2021-04-18 18:07:20 +0900

    refactor : 불필요한 코드 삭제

--- src/main/java/chess/SpringChessApplication.java
+++ src/main/java/chess/SpringChessApplication.java
@@ -2,13 +2,8 @@ package chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringChessApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringChessApplication.class, args);

596b731657d53f12209a4a337e93dd776a7307c4
NewWisdom <wlgp2500@gmail.com>
2021-04-18 16:09:33 +0900

    refactor(): template 디렉토리에 있는 템플릿을 로드하도록 변경

--- src/main/java/chess/config/WebConfiguration.java
+++ src/main/java/chess/config/WebConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/").setViewName("index");
     }
 }

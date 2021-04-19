2dd5abb0e9ee72bf46e6f508a9e78d44528dcefa
Minsung <sns2831@gmail.com>
2021-04-19 16:55:06 +0900

    refactor: 사용자의 체스말 이동 명령의 검증을 도메인에서 하도록 변경

--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -4,7 +4,6 @@ import chess.domain.board.Board;
 import chess.domain.game.ChessGame;
 import chess.domain.piece.PieceFactory;
 import chess.domain.piece.Position;
-import chess.exception.CommandFormatException;
 import chess.repository.GameRepository;
 import chess.web.dto.GameDto;
 import chess.web.dto.MessageDto;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChessService {
-    private static final String POSITION_FORMAT = "[a-h][1-8]";
 
     private final GameRepository gameRepository;
 
@@ -83,11 +81,11 @@ public class ChessService {
     public GameDto move(String gameId, String source, String target) {
         ChessGame chessGame = gameRepository.findByGameIdFromCache(gameId);
 
-        validateRightInputs(source, target);
-        Position sourcePosition = getPositionFromInput(source);
-        Position targetPosition = getPositionFromInput(target);
-
-        return executeMove(sourcePosition, targetPosition, chessGame);
+        return executeMove(
+                Position.ofChessPiece(source),
+                Position.ofChessPiece(target),
+                chessGame
+        );
     }
 
     private GameDto executeMove(Position sourcePosition, Position targetPosition, ChessGame chessGame) {
@@ -96,25 +94,6 @@ public class ChessService {
         return new GameDto(chessGame);
     }
 
-    private void validateRightInputs(String source, String target) {
-        if (isRightPositionFormat(source) && isRightPositionFormat(target)) {
-            return;
-        }
 
-        throw new CommandFormatException();
-    }
-
-    private Position getPositionFromInput(String input) {
-        String[] inputs = input.split("");
-
-        int column = inputs[0].charAt(0) - 'a';
-        int row = Board.getRow() - Integer.parseInt(inputs[1]);
-
-        return new Position(row, column);
-    }
-
-    private boolean isRightPositionFormat(String inputs) {
-        return inputs.matches(POSITION_FORMAT);
-    }
 
 }

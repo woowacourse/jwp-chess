453e0fa883a6485986f31262c7c98635c80abc6a
Jinhong <xlffm3@gmail.com>
2021-04-19 13:03:32 +0900

    refactor: ChessController 테스트 중복 제거

--- src/test/java/chess/controller/spring/ChessControllerTest.java
+++ src/test/java/chess/controller/spring/ChessControllerTest.java
@@ -9,6 +9,7 @@ import chess.service.spring.RoomService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,74 +42,78 @@ class ChessControllerTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-        roomService.addRoom("test1");
+        roomService.addRoom("room1");
     }
 
     @DisplayName("보드를 조회한다.")
     @Test
     void showBoard() throws JsonProcessingException {
-        RestAssured.given().log().all()
+        String expectedResponseBody = writeResponse();
+
+        Response response = RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/chessgame/1/chessboard")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .body("size()", is(3))
-                .body(is(parseExpectedResponse()));
+                .when().get("/chessgame/1/chessboard");
+
+        assertResponse(response, HttpStatus.OK, expectedResponseBody);
     }
 
-    @DisplayName("체스 기물을 이동한다.")
-    @Test
-    void move() throws JsonProcessingException {
-        MoveRequestDTO moveRequestDTO = new MoveRequestDTO("a2", "a3", "WHITE");
-        RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(moveRequestDTO)
-                .when().put("/chessgame/1/chessboard")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .body("size()", is(3))
-                .body(is(parseExpectedResponse()));
-    }
-
-    @DisplayName("현재 턴이 아닌 기물을 조작시 예외가 발생한다.")
-    @Test
-    void cannotMove() {
-        MoveRequestDTO moveRequestDTO = new MoveRequestDTO("a2", "a3", "BLACK");
-        RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(moveRequestDTO)
-                .when().put("/chessgame/1/chessboard")
-                .then().log().all()
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .body(is("조작할 수 있는 기물이 없습니다."));
-    }
-
-    private String parseExpectedResponse() throws JsonProcessingException {
+    private String writeResponse() throws JsonProcessingException {
         ChessBoard chessBoard = chessService.findChessBoardByRoomId(1);
         TeamType currentTeamType = chessService.findCurrentTeamTypeByRoomId(1);
         BoardDTO boardDTO = BoardDTO.of(chessBoard, currentTeamType);
         return new ObjectMapper().writeValueAsString(boardDTO);
     }
 
+    private void assertResponse(Response response, HttpStatus httpStatus, String expectedResponseBody) {
+        response.then().log().all()
+                .statusCode(httpStatus.value())
+                .body(is(expectedResponseBody));
+    }
+
+    @DisplayName("체스 기물을 이동한다.")
+    @Test
+    void move() throws JsonProcessingException {
+        MoveRequestDTO moveRequestDTO = new MoveRequestDTO("a2", "a3", "WHITE");
+
+        Response response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(moveRequestDTO)
+                .when().put("/chessgame/1/chessboard");
+        String expectedResponseBody = writeResponse();
+
+        assertResponse(response, HttpStatus.OK, expectedResponseBody);
+    }
+
+    @DisplayName("현재 턴이 아닌 기물을 조작시 예외가 발생한다.")
+    @Test
+    void cannotMove() {
+        MoveRequestDTO moveRequestDTO = new MoveRequestDTO("a2", "a3", "BLACK");
+
+        Response response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(moveRequestDTO)
+                .when().put("/chessgame/1/chessboard");
+
+        assertResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, "조작할 수 있는 기물이 없습니다.");
+    }
+
     @DisplayName("리스타트시 응답 결과는 메인 페이지 url이다.")
     @Test
     void restart() {
-        RestAssured.given().log().all()
-                .when().delete("/chessgame/1/histories")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .body(is("/"));
+        Response response = RestAssured.given().log().all()
+                .when().delete("/chessgame/1/histories");
+
+        assertResponse(response, HttpStatus.OK, "/");
     }
 
     @DisplayName("도메인 예외가 발생하면 500 에러와 메시지가 발생한다.")
     @Test
     void handleException() {
-        RestAssured.given().log().all()
-                .when().get("/chessgame/1/result")
-                .then().log().all()
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .body(is("승리한 팀을 찾을 수 없습니다."));
+        Response response = RestAssured.given().log().all()
+                .when().get("/chessgame/1/result");
+
+        assertResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, "승리한 팀을 찾을 수 없습니다.");
     }
 }

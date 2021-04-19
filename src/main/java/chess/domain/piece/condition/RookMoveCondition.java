542dadc055d57d1b88d4460a0d0cc73907b0d68a
seunghyun <ggyool@naver.com>
2021-04-19 15:38:32 +0900

    fix: rook 움직임 관련 버그 수정

--- src/main/java/chess/domain/piece/condition/RookMoveCondition.java
+++ src/main/java/chess/domain/piece/condition/RookMoveCondition.java
@@ -22,20 +22,46 @@ public class RookMoveCondition extends MoveCondition {
     }
 
     private boolean isNotExistObstacleOnCrossPath(Board board, Piece piece, Position target) {
-        return board.getPieces().stream()
-            .filter(pieceOnBoard -> !pieceOnBoard.equals(piece))
-            .noneMatch(isExistObstacleOnCrossPath(piece, target));
+        return (isHorizontalMovable(piece, target) &&
+            isNotExistObstacleOnHorizontalPath(board, piece, target))
+
+            || (isVerticalMovable(piece, target) &&
+            isNotExistObstacleOnVerticalPath(board, piece, target));
     }
 
-    private Predicate<Piece> isExistObstacleOnCrossPath(final Piece piece, final Position target) {
+    private boolean isHorizontalMovable(final Piece piece, final Position target) {
+        return piece.getRow() == target.getRow();
+    }
+
+    private boolean isVerticalMovable(final Piece piece, final Position target) {
+        return piece.getColumn() == target.getColumn();
+    }
+
+    private boolean isNotExistObstacleOnHorizontalPath(final Board board, final Piece piece,
+        final Position target) {
         int maxCol = Math.max(piece.getColumn(), target.getColumn());
         int minCol = Math.min(piece.getColumn(), target.getColumn());
+
+        return board.getPieces().stream()
+            .filter(pieceOnBoard -> !pieceOnBoard.equals(piece))
+            .noneMatch(pieceOnBoard ->
+                (pieceOnBoard.getRow() == piece.getRow()) &&
+                    (minCol < pieceOnBoard.getColumn()) &&
+                    (pieceOnBoard.getColumn() < maxCol)
+            );
+    }
+
+    private boolean isNotExistObstacleOnVerticalPath(final Board board, final Piece piece,
+        final Position target) {
         int maxRow = Math.max(piece.getRow(), target.getRow());
         int minRow = Math.min(piece.getRow(), target.getRow());
 
-        return pieceOnBoard ->
-            (minRow < pieceOnBoard.getRow()) && (pieceOnBoard.getRow() < maxRow) &&
-                (minCol < pieceOnBoard.getColumn() && pieceOnBoard.getColumn() < maxCol);
+        return board.getPieces().stream()
+            .filter(pieceOnBoard -> !pieceOnBoard.equals(piece))
+            .noneMatch(pieceOnBoard ->
+                (pieceOnBoard.getColumn() == piece.getColumn()) &&
+                    (minRow < pieceOnBoard.getRow()) &&
+                    (pieceOnBoard.getRow() < maxRow)
+            );
     }
-
 }

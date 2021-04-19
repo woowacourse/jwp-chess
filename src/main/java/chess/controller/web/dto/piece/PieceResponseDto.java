28c9f1191c37ef93251026e185e99e7ced668265
Gomding <backchida@gmail.com>
2021-04-19 18:50:43 +0900

    refactor(PieceResponseDto.java) : toPiece(), toPosition 메서드 삭제하고 밖에서 객체를 생성하도록 변경

--- src/main/java/chess/controller/web/dto/piece/PieceResponseDto.java
+++ src/main/java/chess/controller/web/dto/piece/PieceResponseDto.java
@@ -1,8 +1,6 @@
 package chess.controller.web.dto.piece;
 
 import chess.domain.board.position.Position;
-import chess.domain.piece.Piece;
-import chess.util.PieceConverter;
 
 public class PieceResponseDto {
 
@@ -12,14 +10,6 @@ public class PieceResponseDto {
     public PieceResponseDto(final String symbol, final String position) {
         this.symbol = symbol;
         this.position = position;
-    }
-
-    public Piece toPiece() {
-        return PieceConverter.parsePiece(symbol);
-    }
-
-    public Position toPosition() {
-        return Position.of(position);
     }
 
     public String getSymbol() {

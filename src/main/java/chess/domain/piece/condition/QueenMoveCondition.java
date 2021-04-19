d2039f443c534edb216a4d88f5ed7d29f4a958cc
cjy17 <cjy1705@naver.com>
2021-04-19 22:04:36 +0900

    fix : rook, queen 직선 움직임 버그 수정

--- src/main/java/chess/domain/piece/condition/QueenMoveCondition.java
+++ src/main/java/chess/domain/piece/condition/QueenMoveCondition.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class QueenMoveCondition extends MoveCondition {
+
     @Override
     public boolean isSatisfyBy(final Board board, final Piece piece, final Position target) {
         return !piece.isSamePosition(target) &&
@@ -18,8 +19,15 @@ public class QueenMoveCondition extends MoveCondition {
     }
 
     private boolean isNotExistObstacleOnPath(Board board, Piece piece, Position target) {
-        return isNotExistObstacleOnCrossPath(board, piece, target) ||
-                isNotExistObstacleOnXPath(board, piece, target);
+        return
+                (isHorizontalMovable(piece, target) &&
+                        isNotExistObstacleOnHorizontalPath(board, piece, target))
+
+                        || (isVerticalMovable(piece, target) &&
+                        isNotExistObstacleOnVerticalPath(board, piece, target))
+
+                        || (isRightXCondition(piece, target) &&
+                        isNotExistObstacleOnXPath(board, piece, target));
     }
 
     private boolean isMovablePath(final Piece piece, final Position target) {
@@ -27,7 +35,15 @@ public class QueenMoveCondition extends MoveCondition {
     }
 
     private boolean isCrossMovable(final Piece piece, final Position target) {
-        return piece.getRow() == target.getRow() || piece.getColumn() == target.getColumn();
+        return isHorizontalMovable(piece, target) || isVerticalMovable(piece, target);
+    }
+
+    private boolean isHorizontalMovable(final Piece piece, final Position target) {
+        return piece.getRow() == target.getRow();
+    }
+
+    private boolean isVerticalMovable(final Piece piece, final Position target) {
+        return piece.getColumn() == target.getColumn();
     }
 
     private boolean isRightXCondition(final Piece piece, final Position target) {
@@ -35,26 +51,32 @@ public class QueenMoveCondition extends MoveCondition {
                 == Math.abs(piece.getRow() - target.getRow());
     }
 
-    private boolean isNotExistObstacleOnCrossPath(Board board, Piece piece, Position target) {
+    private boolean isNotExistObstacleOnHorizontalPath(final Board board, final Piece piece,
+                                                       final Position target) {
         int maxCol = Math.max(piece.getColumn(), target.getColumn());
         int minCol = Math.min(piece.getColumn(), target.getColumn());
+
+        return board.getPieces().stream()
+                .filter(pieceOnBoard -> !pieceOnBoard.equals(piece))
+                .noneMatch(pieceOnBoard ->
+                        (pieceOnBoard.getRow() == piece.getRow()) &&
+                                (minCol < pieceOnBoard.getColumn()) &&
+                                (pieceOnBoard.getColumn() < maxCol)
+                );
+    }
+
+    private boolean isNotExistObstacleOnVerticalPath(final Board board, final Piece piece,
+                                                     final Position target) {
         int maxRow = Math.max(piece.getRow(), target.getRow());
         int minRow = Math.min(piece.getRow(), target.getRow());
 
         return board.getPieces().stream()
                 .filter(pieceOnBoard -> !pieceOnBoard.equals(piece))
                 .noneMatch(pieceOnBoard ->
-                        isExistObstacleOnCrossPath(maxCol, minCol, maxRow, minRow, pieceOnBoard)
+                        (pieceOnBoard.getColumn() == piece.getColumn()) &&
+                                (minRow < pieceOnBoard.getRow()) &&
+                                (pieceOnBoard.getRow() < maxRow)
                 );
-    }
-
-    private boolean isExistObstacleOnCrossPath(final int maxCol,
-                                               final int minCol,
-                                               final int maxRow,
-                                               final int minRow,
-                                               final Piece pieceOnBoard) {
-        return (minRow < pieceOnBoard.getRow()) && (pieceOnBoard.getRow() < maxRow) &&
-                (minCol < pieceOnBoard.getColumn() && pieceOnBoard.getColumn() < maxCol);
     }
 
     private boolean isNotExistObstacleOnXPath(Board board, Piece piece, Position target) {
@@ -71,14 +93,17 @@ public class QueenMoveCondition extends MoveCondition {
         int maxRow = Math.max(piece.getRow(), target.getRow());
         int minRow = Math.min(piece.getRow(), target.getRow());
 
-        return pieceOnBoard -> minCol < pieceOnBoard.getColumn() && pieceOnBoard.getColumn() < maxCol &&
+        return pieceOnBoard -> minCol < pieceOnBoard.getColumn()
+                && pieceOnBoard.getColumn() < maxCol &&
                 minRow < pieceOnBoard.getRow() && pieceOnBoard.getRow() < maxRow;
     }
 
-    private Predicate<Piece> hasSameGradientWithSourceAndTarget(final Piece piece, final Position target) {
+    private Predicate<Piece> hasSameGradientWithSourceAndTarget(final Piece piece,
+                                                                final Position target) {
         return selectedPiece ->
                 piece.getPosition().calculateGradient(target) ==
                         piece.getPosition().calculateGradient(selectedPiece.getPosition());
     }
+
 
 }

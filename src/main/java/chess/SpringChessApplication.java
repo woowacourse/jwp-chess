961278020e1cdb13bbd7e663cbdd41c9c937b1e5
Sehwan-Jang <whilter08@gmail.com>
2021-04-19 15:30:12 +0900

    refactor : 불필요 어노테이션 제거

--- src/main/java/chess/SpringChessApplication.java
+++ src/main/java/chess/SpringChessApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 public class SpringChessApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringChessApplication.class, args);
--- src/main/java/chess/service/SparkChessService.java
+++ src/main/java/chess/service/SparkChessService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.sql.SQLException;
 import java.util.List;
 
-@Service
 public class SparkChessService {
     private static final String END_TRUE = "true";
 

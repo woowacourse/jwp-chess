3ed197f936465f7d8621109d9d22bb2499068ed0
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-04-20 02:39:29 +0900

    reafactor: DDL 의 AUTO_INCREMENT 값 지정을 해제

--- src/main/resources/templates/schema.sql
+++ src/main/resources/templates/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE `grid` (
                         `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         `isStarted` tinyint NOT NULL DEFAULT '0',
                         PRIMARY KEY (`gridId`)
-) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
 
 CREATE TABLE `piece` (
                          `pieceId` int NOT NULL AUTO_INCREMENT,
@@ -15,11 +15,11 @@ CREATE TABLE `piece` (
                          `gridId` int NOT NULL,
                          `name` varchar(45) COLLATE utf8mb4_bin NOT NULL,
                          PRIMARY KEY (`pieceId`)
-) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
 
 CREATE TABLE `room` (
                         `roomId` int NOT NULL AUTO_INCREMENT,
                         `roomName` varchar(45) COLLATE utf8mb4_bin NOT NULL,
                         `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         PRIMARY KEY (`roomId`)
-) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin

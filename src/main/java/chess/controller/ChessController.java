1f3c36bfd013a8c160e6248b8cdff58e3cab9c63
Daeun Lee <leede0418@likelion.org>
2021-04-19 19:52:20 +0900

    refactor(controller): HTTP 상태코드(200) 반환

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -4,7 +4,9 @@ import chess.dto.request.MoveRequestDto;
 import chess.dto.request.TurnChangeRequestDto;
 import chess.dto.response.MoveResponseDto;
 import chess.service.ChessService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,8 @@ public class ChessController {
     }
 
     @PostMapping(value = "/turn", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void turn(@RequestBody TurnChangeRequestDto turnChangeRequestDto) {
+    public ResponseEntity<Void> turn(@RequestBody TurnChangeRequestDto turnChangeRequestDto) {
         chessService.changeTurn(turnChangeRequestDto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

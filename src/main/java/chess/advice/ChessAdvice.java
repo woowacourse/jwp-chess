0977e6de393ee0f8695bfe47957b2b542842a5a0
멍토 <daum7766@naver.com>
2021-04-19 17:48:57 +0900

    feat: exceptionhandler 추가

---
+++ src/main/java/chess/advice/ChessAdvice.java
@@ -0,0 +1,14 @@
+package chess.advice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ChessAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity IllegalArgumentExceptionHandler() {
+        return ResponseEntity.badRequest().build();
+    }
+}

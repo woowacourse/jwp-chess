b7e903c67a44ae42307f3584518bbdca5c8795b5
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-04-20 18:53:46 +0900

    test: SpringRoomDao에 관한 JdbcTest 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS chess_room;
+CREATE TABLE chess_room (
+    room_no int not null auto_increment,
+    room_name varchar(50) not null,
+    turn varchar(10) not null,
+    board varchar(1000) not null,
+    primary key (room_no)
+);

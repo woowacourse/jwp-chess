1407f1abb3c556ae5964561230f7ddeaa1cfb968
Kimun Kim <kkm97351@gmail.com>
2021-04-20 17:20:05 +0900

    refactor: MoveResult의 캐스팅 문 삭제

--- src/main/java/chess/domain/order/MoveResult.java
+++ src/main/java/chess/domain/order/MoveResult.java
@@ -1,7 +1,6 @@
 package chess.domain.order;
 
 import chess.domain.piece.Piece;
-import chess.domain.piece.RealPiece;
 import chess.exception.DomainException;
 
 public class MoveResult {
@@ -15,9 +14,9 @@ public class MoveResult {
         return capturedPiece.isNotBlank();
     }
 
-    public RealPiece getCapturedPiece() {
+    public Piece getCapturedPiece() {
         if (isCaptured()) {
-            return (RealPiece) capturedPiece;
+            return capturedPiece;
         }
         throw new DomainException("잡힌 말이 없습니다.");
     }

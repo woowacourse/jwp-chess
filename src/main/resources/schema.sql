ef238e4805615f0e44c236a171d28d5abd17a4be
김주원 <kjw11077@naver.com>
2021-04-20 23:44:17 +0900

    chore: MySQL 설정 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,4 @@
-create user 'joy'@'localhost' identified by 'password';
-
-grant all privileges on *.* to 'joy'@'localhost';
-
-flush privileges;
-
-
-DROP DATABASE chess_db;
-
-CREATE DATABASE chess_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-USE chess_db;
-
-
-DROP TABLE chess_game;
-
-CREATE TABLE chess_game
+CREATE TABLE IF NOT EXISTS chess_game
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     name       TEXT                   NOT NULL,
@@ -22,10 +6,7 @@ CREATE TABLE chess_game
     created_at DATETIME DEFAULT NOW() NOT NULL
 );
 
-
-DROP TABLE move_command;
-
-CREATE TABLE move_command
+CREATE TABLE IF NOT EXISTS move_command
 (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
     game_id         BIGINT                 NOT NULL,

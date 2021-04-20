bac5c5eab9ac164f566f405a823f6f717aaaf67c
Jinho <jh8579@gmail.com>
2021-04-20 17:51:05 +0900

    fix : data.sql 오류 발생하는 부분 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ CREATE TABLE `game` (
   `isEnd` tinyint DEFAULT NULL,
   `createdTime` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE `game_history` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -12,11 +12,11 @@ CREATE TABLE `game_history` (
   `command` varchar(45) DEFAULT NULL,
   `createdTime` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+);
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+);

b0d9845c3aa941f6609d1047d0558b0ab3b4cb42
pomo <bosl95@naver.com>
2021-04-22 01:18:32 +0900

    refactor : 와일드 카드 제거

--- src/main/java/chess/controller/SpringWebChessController.java
+++ src/main/java/chess/controller/SpringWebChessController.java
@@ -16,20 +16,16 @@ public class SpringWebChessController {
     }
 
     @GetMapping("/newgame")
-    public ResponseEntity<CommonDto<?>> newGame() { // ? extends ParentDto로 선언해볼까?
+    public ResponseEntity<CommonDto<NewGameDto>> newGame() {
         ChessGameManager chessGameManager = new ChessGameManager();
         chessGameManager.start();
         int gameId = gameDAO.saveGame(chessGameManager);
         return ResponseEntity.ok().body(
-                new CommonDto<>(
-                        "새로운 게임이 생성되었습니다.",
-                        NewGameResponse.from(chessGameManager, gameId)
-                )
-        );
+                new CommonDto<>("새로운 게임이 생성되었습니다.", NewGameDto.from(chessGameManager, gameId)));
     }
 
     @PostMapping("/move")
-    public ResponseEntity<CommonDto<?>> move(@RequestBody MoveRequest moveRequest) {
+    public ResponseEntity<CommonDto<RunningGameDto>> move(@RequestBody MoveRequest moveRequest) {
         ChessGameManager chessGameManager = gameDAO.loadGame(moveRequest.getGameId());
 
         int gameId = moveRequest.getGameId();
@@ -41,30 +37,19 @@ public class SpringWebChessController {
         gameDAO.updatePiecesByGameId(chessGameManager, gameId);
 
         return ResponseEntity.ok().body(
-                new CommonDto<>(
-                        "기물을 이동했습니다.",
-                        RunningGameResponse.from(chessGameManager))
-        );
+                new CommonDto<>("기물을 이동했습니다.", RunningGameDto.from(chessGameManager)));
     }
 
     @GetMapping("/load/games")
-    public ResponseEntity<CommonDto<?>> loadGames() {
+    public ResponseEntity<CommonDto<GameListDto>> loadGames() {
         return ResponseEntity.ok().body(
-                new CommonDto<>(
-                        "게임 목록을 불러왔습니다.",
-                        new GameListDto(gameDAO.loadGames())
-                )
-        );
+                new CommonDto<>("게임 목록을 불러왔습니다.", new GameListDto(gameDAO.loadGames())));
     }
 
     @GetMapping("/load/{id}")
-    public ResponseEntity<CommonDto<?>> loadGame(@PathVariable int id) {
+    public ResponseEntity<CommonDto<RunningGameDto>> loadGame(@PathVariable int id) {
         ChessGameManager chessGameManager = gameDAO.loadGame(id);
         return ResponseEntity.ok().body(
-                new CommonDto<>(
-                        "게임을 불러왔습니다",
-                        RunningGameResponse.from(chessGameManager)
-                )
-        );
+                new CommonDto<>("게임을 불러왔습니다", RunningGameDto.from(chessGameManager)));
     }
 }

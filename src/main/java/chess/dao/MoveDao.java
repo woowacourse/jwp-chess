c80821945639a19f23e7e8a719d1d4bec8735bf7
RinSabbit <celena1004@naver.com>
2021-04-23 00:55:22 +0900

    fix: 체스말이 한 번 이상 이동된 방은 삭제되지 않는 오류 수정

--- src/main/java/chess/dao/MoveDao.java
+++ src/main/java/chess/dao/MoveDao.java
@@ -18,17 +18,17 @@ public class MoveDao {
     }
 
     public void addMoveById(final Position from, final Position to, int id) {
-        String query = "insert into move2 (start, end, room_id) values (?,?,?)";
+        String query = "insert into move (start, end, room_id) values (?,?,?)";
         jdbcTemplate.update(query, from.toString(), to.toString(), id);
     }
 
     public List<MoveRequest> getMovesById(int id) {
-        String query = "select * from move2 where room_id= " + id;
+        String query = "select * from move where room_id= " + id;
         return jdbcTemplate.query(query, mapper);
     }
 
     public void deleteAllById(int id) {
-        String query = "delete from move2 where room_id=" + id;
+        String query = "delete from move where room_id=" + id;
         jdbcTemplate.update(query);
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,3 @@
-create table if not exists move
-(
-    id    int        not null auto_increment,
-    start varchar(2) not null,
-    end   varchar(2) not null,
-    primary key (id)
-);
-
 create table if not exists room
 (
     id int not null auto_increment,
@@ -13,12 +5,12 @@ create table if not exists room
     primary key (id)
 );
 
-create table if not exists move2
+create table if not exists move
 (
     id    int        not null auto_increment,
     room_id int,
     start varchar(2) not null,
     end   varchar(2) not null,
     primary key (id),
-    foreign key (room_id) references room (id)
+    foreign key (room_id) references room (id) on delete cascade
 );

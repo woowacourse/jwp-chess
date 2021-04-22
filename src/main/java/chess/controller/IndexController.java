df5ab8447533ac2a4ba1c78eb916778cdd68ed03
pkeugine <pkeugine@gmail.com>
2021-04-22 10:54:07 +0900

    refactor: 컨트롤러 역할 분리

---
+++ src/main/java/chess/controller/IndexController.java
@@ -0,0 +1,14 @@
+package chess.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String welcomeAsHTML() {
+        return "index.html";
+    }
+}

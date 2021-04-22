c32c2c475adc20fa3b16ab02821aaa5492906800
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-22 18:21:55 +0900

    refactor: 게임 방 정보 쿠키를 통해 전달

--- src/main/java/chess/web/controller/ChessGameController.java
+++ src/main/java/chess/web/controller/ChessGameController.java
@@ -7,7 +7,8 @@ import chess.web.controller.dto.response.ChessGameResponseDto;
 import chess.web.controller.dto.response.CreateGameResponseDto;
 import chess.web.controller.dto.response.GameStatusResponseDto;
 import chess.web.service.ChessGameService;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -56,9 +57,33 @@ public class ChessGameController {
     }
 
     @GetMapping("/games/{gameId}")
-    public String chessBoard(@PathVariable Long gameId, Model model) {
+    public String chessBoard(@PathVariable Long gameId, Model model, HttpServletResponse response) {
         GameStatusResponseDto gameStatusDto = chessGameService.getGameStatus(gameId);
+        addCookiesAboutGameToResponse(gameStatusDto, response);
         model.addAttribute("gameStatusDto", gameStatusDto);
         return "chess-game";
     }
+
+    private void addCookiesAboutGameToResponse(GameStatusResponseDto gameStatusResponseDto, HttpServletResponse response) {
+        List<Cookie> cookies = getCookiesAboutGame(gameStatusResponseDto);
+
+        for (Cookie cookie : cookies) {
+            response.addCookie(cookie);
+        }
+    }
+
+    private List<Cookie> getCookiesAboutGame(GameStatusResponseDto gameStatusResponseDto) {
+        return new ArrayList<>(Arrays.asList(
+            new Cookie("gameId", String.valueOf(gameStatusResponseDto.getGameId())),
+            new Cookie("title", gameStatusResponseDto.getTitle()),
+            new Cookie("beforeTurnTeamColorName", gameStatusResponseDto.getBeforeTurnTeamColorName()),
+            new Cookie("currentTurnTeamColorName", gameStatusResponseDto.getCurrentTurnTeamColorName()),
+            new Cookie("boardStatus", gameStatusResponseDto.getBoardStatus()),
+            new Cookie("whitePlayerScore", String.valueOf(gameStatusResponseDto.getWhitePlayerScore())),
+            new Cookie("blackPlayerScore", String.valueOf(gameStatusResponseDto.getBlackPlayerScore())),
+            new Cookie("isKingDead", String.valueOf(gameStatusResponseDto.isKingDead()))
+        ));
+    }
+
+
 }

f1434ab9adbe7011fa92bd2275e26e1a1fb60cd6
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-22 16:22:12 +0900

    refactor: Controller 통합

--- src/main/java/chess/web/controller/ChessGamePlayController.java
+++ src/main/java/chess/web/controller/ChessGamePlayController.java
@@ -6,18 +6,22 @@ import static chess.web.controller.ChessGameController.ENCRYPTED_PASSWORD;
 import chess.web.controller.dto.request.MoveRequestDto;
 import chess.web.controller.dto.response.MoveResponseDto;
 import chess.web.service.ChessGameService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class PieceMoveController {
+public class ChessGamePlayController {
 
     private final ChessGameService chessGameService;
 
-    public PieceMoveController(ChessGameService chessGameService) {
+    public ChessGamePlayController(ChessGameService chessGameService) {
         this.chessGameService = chessGameService;
     }
 
@@ -29,5 +33,11 @@ public class PieceMoveController {
         moveRequestDto.setEncryptedPassword(encryptedPassword);
         chessGameService.movePiece(moveRequestDto);
         return new MoveResponseDto(false);
+    }
+
+    @DeleteMapping("/games/{gameId}")
+    public HttpEntity<HttpStatus> deleteGame(@PathVariable Long gameId) {
+        chessGameService.endGame(gameId);
+        return new HttpEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/chess/web/controller/advice/PieceMoveControllerAdvice.java
+++ src/main/java/chess/web/controller/advice/PieceMoveControllerAdvice.java
@@ -1,11 +1,11 @@
 package chess.web.controller.advice;
 
-import chess.web.controller.PieceMoveController;
+import chess.web.controller.ChessGamePlayController;
 import chess.web.controller.dto.response.MoveResponseDto;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackageClasses = PieceMoveController.class)
+@RestControllerAdvice(basePackageClasses = ChessGamePlayController.class)
 public class PieceMoveControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)

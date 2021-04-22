ccb7d8a440d093dfdad2a9d0524de466d568e009
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-22 11:03:18 +0900

    refactor: ControllerAdvice 클래스 이름 더 명시적으로 수정

--- src/main/java/chess/web/controller/advice/PieceMoveControllerAdvice.java
+++ src/main/java/chess/web/controller/advice/PieceMoveControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = PieceMoveController.class)
-public class PieceMoveAdvice {
+public class PieceMoveControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public MoveResponseDto pieceMoveException(IllegalArgumentException e) {

55aa1304bceff605fbd151f8d86ac2bb784f0944
dwl5 <ssop6403@gmail.com>
2021-04-22 16:16:23 +0900

    refactor: Room 생성 Transaction 처리

--- src/main/java/chess/controller/ChessRoomController.java
+++ src/main/java/chess/controller/ChessRoomController.java
@@ -6,6 +6,7 @@ import dto.RoomDto;
 import dto.RoomRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +29,7 @@ public class ChessRoomController {
         return ResponseEntity.ok().body(chessRoomService.rooms());
     }
 
+    @Transactional
     @PostMapping("/room")
     public void create(@Valid @ModelAttribute RoomRequestDto roomRequestDto, BindingResult bindingResult) {
         chessRoomService.create(roomRequestDto);

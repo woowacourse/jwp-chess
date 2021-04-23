9cf1cb11f31ff3f964d8033f28120e8854ce25ca
ecsimsw <ecsimsw@gmail.com>
2021-04-24 02:56:45 +0900

    todo :: controllerAdvice 테스트 코드 작성

--- src/main/java/chess/controller/ExceptionController.java
+++ src/main/java/chess/controller/ExceptionController.java
@@ -7,15 +7,32 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.sql.SQLException;
+
 @ControllerAdvice
 public class ExceptionController {
 
     public ExceptionController() {
     }
 
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handlingException(final Exception e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        final String message = "Unhandled exception : "+ e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalArgumentException(final Exception e) {
+        final String message = "IllegalArgumentException exception : "+ e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleSQLException(final Exception e) {
+        final String message = "SQL exception : "+ e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/chess/controller/RoomRestController.java
+++ src/main/java/chess/controller/RoomRestController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/room")

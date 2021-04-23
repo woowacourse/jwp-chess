61a3b3927334c2357a169cdf959745d80a66f576
younghoonkwon <younghoon.kwon91@gmail.com>
2021-04-23 17:14:01 +0900

    refactor: Controller 리팩토링

--- src/main/java/chess/controller/HomeController.java
+++ src/main/java/chess/controller/HomeController.java
@@ -1,6 +1,5 @@
 package chess.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/chess/controller/api/LobbyController.java
+++ src/main/java/chess/controller/api/LobbyController.java
@@ -1,24 +1,18 @@
 package chess.controller.api;
 
-import chess.domain.game.ChessGame;
-import chess.domain.piece.Color;
-import chess.dto.DuplicateDto;
-import chess.dto.FinishDto;
+import chess.dto.ChessGameDto;
+import chess.dto.ChessGamesDto;
 import chess.dto.RoomIdDto;
-import chess.dto.RoomListDto;
-import chess.dto.ScoreDto;
 import chess.dto.TitleDto;
 import chess.service.LobbyService;
-import javax.validation.Valid;
-
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 public class LobbyController {
@@ -29,38 +23,21 @@ public class LobbyController {
         this.lobbyService = lobbyService;
     }
 
-
-    @PostMapping("/lobby/new")
+    @PostMapping("/game")
     public ResponseEntity newGame(@RequestBody @Valid TitleDto titleDTO) {
+        if (lobbyService.findGame(titleDTO.getTitle()).isPresent()) {
+            throw new IllegalArgumentException("같은 이름으로 등록된 방이 있습니다.");
+        }
         return ResponseEntity.ok(new RoomIdDto(lobbyService.newGame(titleDTO.getTitle())));
     }
 
-    @PostMapping("/findRoomId")
-    public ResponseEntity findRoomId(@RequestBody @Valid TitleDto titleDTO) {
-        String title = titleDTO.getTitle().replaceAll("%20", " ");
-        return ResponseEntity.ok(new RoomIdDto(lobbyService.findRoomId(title)
-            .orElseThrow(() -> new EmptyResultDataAccessException(0))));
-    }
-
-    @PostMapping("/isDuplicate")
-    public ResponseEntity<DuplicateDto> isDuplicate(@RequestBody TitleDto titleDTO) {
-        return ResponseEntity.ok(new DuplicateDto(lobbyService.isDuplicate(titleDTO.getTitle())));
-    }
-
-    @GetMapping("/rooms")
-    public ResponseEntity<RoomListDto> findAllRooms() {
-        return ResponseEntity.ok(new RoomListDto(lobbyService.findAllRooms()));
-    }
-
-    @GetMapping("/finishByName/{roomName}")
-    public ResponseEntity<FinishDto> isFinished(@PathVariable String roomName) {
-        return ResponseEntity.ok(new FinishDto(lobbyService.isFinished(roomName)));
-    }
-
-    @GetMapping("/scoreByName/{roomName}")
-    public ResponseEntity<ScoreDto> score(@PathVariable String roomName) {
-        ChessGame chessGame = lobbyService.loadGame(roomName);
-        return ResponseEntity
-            .ok(new ScoreDto(chessGame.getScore(Color.BLACK), chessGame.getScore(Color.WHITE)));
+    @GetMapping("/games")
+    public ResponseEntity<ChessGamesDto> findAllRooms() {
+        return ResponseEntity.ok(new ChessGamesDto(
+                lobbyService.findAllGames()
+                        .stream()
+                        .map(ChessGameDto::new)
+                        .collect(Collectors.toList())
+        ));
     }
 }

437dc5eb84e88597221e2ffb698c0ef35ad0402e
Seed <hsik0225@gmail.com>
2021-04-23 14:18:20 +0900

    refactor(html-controller):프론트 뷰 반환을 MvcCongiurer로 하도록 수정

---
+++ src/main/java/chess/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package chess.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/pages/main.html");
+        registry.addViewController("/chess").setViewName("/pages/chess.html");
+    }
+}

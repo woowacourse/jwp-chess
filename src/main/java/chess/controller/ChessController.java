78dee695059b9aa303c6db52c289699d60c33921
TaewanKimmmm <ktww88@gmail.com>
2021-04-25 01:43:11 +0900

    style: 코드 포매팅

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -6,7 +6,8 @@ import chess.service.ChessService;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ChessController {
--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -144,7 +144,7 @@ function request_move_post(first_click, second_click) {
     moveXhr.send(JSON.stringify({
         source: start_position,
         target: destination,
-        roomId :window.location.href.split('rooms/')[1]
+        roomId: window.location.href.split('rooms/')[1]
     }));
 
     start_position = null;
@@ -175,7 +175,7 @@ function request_move_post(first_click, second_click) {
         turnXhr.send(JSON.stringify({
             currentTurn: current_turn,
             nextTurn: next_turn,
-            roomId :window.location.href.split('rooms/')[1]
+            roomId: window.location.href.split('rooms/')[1]
         }));
         window.location.href = "/rooms/" + window.location.href.split('rooms/')[1];
     };

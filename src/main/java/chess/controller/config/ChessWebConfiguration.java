7a0c7d2da42f65df30394cc57f82e662243ed5e8
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-04-24 23:30:34 +0900

    refactor: url 변경 및 Configuration 추가

---
+++ src/main/java/chess/controller/config/ChessWebConfiguration.java
@@ -0,0 +1,16 @@
+package chess.controller.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ChessWebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+
+}

01c7866df84a5be0630c55d53202cac3de8a449c
Seed <hsik0225@gmail.com>
2021-04-24 17:51:49 +0900

    feat(room-controller):체스 방 입장 기능 구현

--- src/main/java/chess/controller/RoomController.java
+++ src/main/java/chess/controller/RoomController.java
@@ -8,9 +8,12 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import chess.domain.chess.Chess;
+import chess.domain.chess.ChessDto;
 import chess.domain.room.Room;
 import chess.service.RoomService;
 
@@ -25,22 +28,39 @@ public class RoomController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Room>> getRooms() {
+    public ResponseEntity<List<Room>> listRooms() {
         return ResponseEntity.ok(roomService.findAll());
     }
 
+    @GetMapping("/{roomId}/chess")
+    public ResponseEntity<ChessDto> enterRoom(@PathVariable long roomId) {
+        long chessId = roomService.findChessIdById(roomId);
+        Chess chess = roomService.findChessById(chessId);
+        ChessDto chessDto = new ChessDto(chess);
+
+        HttpHeaders httpHeaders = createHeadersWithCookieOf(chessId);
+        return ResponseEntity.status(200)
+                             .headers(httpHeaders)
+                             .body(chessDto);
+    }
+
     @PostMapping
-    public ResponseEntity<Void> insert(Room room) {
+    public ResponseEntity<Void> create(Room room) {
         final long chessId = roomService.insert(room);
-        ResponseCookie cookie = ResponseCookie.from("chessId", String.valueOf(chessId))
-                                              .path("/")
-                                              .build();
 
         URI location = URI.create("/chess");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.SET_COOKIE, cookie.toString());
+        HttpHeaders httpHeaders = createHeadersWithCookieOf(chessId);
         return ResponseEntity.created(location)
                              .headers(httpHeaders)
                              .build();
+    }
+
+    private HttpHeaders createHeadersWithCookieOf(long chessId) {
+        HttpHeaders headers = new HttpHeaders();
+        ResponseCookie cookie = ResponseCookie.from("chessId", String.valueOf(chessId))
+                                              .path("/")
+                                              .build();
+        headers.add(HttpHeaders.SET_COOKIE, cookie.toString());
+        return headers;
     }
 }

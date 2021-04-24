233f57b4c6db15217cc3f0c6cfc0887cc93cd4d1
pomo <bosl95@naver.com>
2021-04-24 14:55:43 +0900

    feat: "/room"을 관리하는 RoomController 생성

---
+++ src/main/java/chess/controller/spring/RoomController.java
@@ -0,0 +1,32 @@
+package chess.controller.spring;
+
+import chess.dto.CommonDto;
+import chess.dto.GameListDto;
+import chess.dto.RoomDto;
+import chess.service.ChessService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/room")
+public class RoomController {
+    private final ChessService service;
+
+    public RoomController(ChessService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<CommonDto<GameListDto>> loadGames() {
+        return ResponseEntity.ok().body(service.loadGameList());
+    }
+
+    @PostMapping("/name")
+    public ResponseEntity<CommonDto<RoomDto>> saveRoomName(@RequestBody String roomName) {
+        return ResponseEntity.ok().body(service.saveRoom(roomName));
+    }
+}

e8b1bf45f72f2b7aaf1309ead53b941698f3949a
jaeseongDev <qkrwotjd1445@naver.com>
2021-04-24 23:30:14 +0900

    test : /room GET에 대한 테스트 코드 추가

--- src/test/java/chess/controller/spring/RoomControllerTest.java
+++ src/test/java/chess/controller/spring/RoomControllerTest.java
@@ -1,9 +1,7 @@
 package chess.controller.spring;
 
-import chess.dto.requestdto.StartRequestDto;
-import chess.dto.responsedto.GridAndPiecesResponseDto;
-import chess.service.ChessService;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.is;
@@ -21,7 +20,7 @@ import static org.hamcrest.Matchers.is;
 public class RoomControllerTest {
 
     @Autowired
-    private ChessService chessService;
+    private JdbcTemplate jdbcTemplate;
 
     @LocalServerPort
     int port;
@@ -31,26 +30,61 @@ public class RoomControllerTest {
         RestAssured.port = port;
     }
 
+    @BeforeEach
+    public void setup() {
+        jdbcTemplate.execute("TRUNCATE TABLE piece");
+        jdbcTemplate.execute("TRUNCATE TABLE grid");
+        jdbcTemplate.execute("TRUNCATE TABLE room");
+    }
+
     @Test
     @DisplayName("/room/{roomId}/restart POST")
     void restart() {
         String roomName = "test";
-        StartRequestDto startRequestDto = new StartRequestDto(roomName);
-        GridAndPiecesResponseDto gridAndPiecesResponseDto = chessService.getGridAndPieces(startRequestDto);
-        Long roomId = gridAndPiecesResponseDto.getGridDto().getRoomId();
-
+        Response response = createGridAndRoom(roomName);
+        int roomId = response.path("data.gridDto.roomId");
         RestAssured
                 .given()
-                .log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(roomName)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(roomName)
                 .when()
-                .post("/room/" + roomId + "/restart")
+                    .post("/room/" + roomId + "/restart")
+                .then()
+                    .log().all()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("code", is(200))
+                    .body("data.piecesResponseDto.size()", is(64))
+                    .body("data.gridDto.roomId", is(roomId));
+    }
+
+    @Test
+    @DisplayName("/room GET")
+    void getRooms() {
+        String roomName = "test";
+        createGridAndRoom(roomName);
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/room")
                 .then()
                 .log().all()
                 .statusCode(HttpStatus.OK.value())
                 .body("code", is(200))
-                .body("data.piecesResponseDto.size()", is(64))
-                .body("data.gridDto.roomId", is(roomId.intValue()));
+                .body("data.rooms.size()", is(1))
+                .body("data.rooms[0].roomName", is(roomName));
+    }
+
+    private Response createGridAndRoom(String roomName) {
+        return RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .pathParam("roomName", roomName)
+                .when()
+                .get("/grid/{roomName}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .response();
     }
 }

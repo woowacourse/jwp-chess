d08f670160d580eff63d2edf883ba083882b0df4
binghe819 <binghe819@naver.com>
2021-04-24 17:14:10 +0900

    refactor: REST API 디자인 가이드에 따라 HTTP 요청 메서드 수정

--- src/main/java/chess/controller/chess/RoomController.java
+++ src/main/java/chess/controller/chess/RoomController.java
@@ -2,26 +2,25 @@ package chess.controller.chess;
 
 import chess.controller.ApiError;
 import chess.controller.ApiResult;
-import chess.domain.dto.ResponseDto;
 import chess.domain.dto.RoomDto;
 import chess.domain.dto.move.MoveRequestDto;
 import chess.domain.dto.move.MoveResponseDto;
 import chess.exception.BusinessException;
 import chess.exception.ErrorCode;
 import chess.serivce.chess.ChessService;
-import java.util.Map;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/room")
+@RequestMapping("/rooms")
 public class RoomController {
     private final ChessService service;
 
@@ -39,9 +38,9 @@ public class RoomController {
         return ResponseEntity.badRequest().body(ApiError.of(ErrorCode.DB_COMMON, e.getMessage()));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<ApiResult> createRoom(@RequestBody Map<String, String> body) {
-        RoomDto result = service.createRoom(body.get("name"));
+    @PostMapping("/{name}")
+    public ResponseEntity<ApiResult> createRoom(@PathVariable("name") String roomName) {
+        RoomDto result = service.createRoom(roomName);
         return ResponseEntity.ok().body(ApiResult.of(result));
     }
 
@@ -51,19 +50,19 @@ public class RoomController {
         return ResponseEntity.ok().body(ApiResult.of(result));
     }
 
-    @GetMapping("/{name}/start")
+    @PutMapping("/{name}/start")
     public ResponseEntity<ApiResult> startRoom(@PathVariable("name") String roomName) {
         MoveResponseDto result = service.start(roomName);
         return ResponseEntity.ok().body(ApiResult.of(result));
     }
 
-    @GetMapping("/{name}/end")
+    @PutMapping("/{name}/end")
     public ResponseEntity<ApiResult> endRoom(@PathVariable("name") String roomName) {
         MoveResponseDto result = service.end(roomName);
         return ResponseEntity.ok().body(ApiResult.of(result));
     }
 
-    @PostMapping("/{name}/move")
+    @PutMapping("/{name}/move")
     public ResponseEntity<ApiResult> move(@PathVariable("name") String roomName, @RequestBody MoveRequestDto moveRequestDto) {
         MoveResponseDto result = service.move(roomName, moveRequestDto.getSource(),
             moveRequestDto.getTarget());

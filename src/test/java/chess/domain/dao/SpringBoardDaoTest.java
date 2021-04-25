1fe98c3c3e14a29c369cd4790f25ccdc7d72b1a8
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-25 16:35:03 +0900

    feat(SpringBoardDaoTest): SpringBoardDao 테스트 구현

---
+++ src/test/java/chess/domain/dao/SpringBoardDaoTest.java
@@ -0,0 +1,56 @@
+package chess.domain.dao;
+
+import chess.dao.SpringBoardDao;
+import chess.domain.Side;
+import chess.domain.board.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@JdbcTest
+@Transactional
+public class SpringBoardDaoTest {
+    private static final String existRoomName = "testRoomName";
+
+    private SpringBoardDao springBoardDao;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        springBoardDao = new SpringBoardDao(jdbcTemplate.getDataSource());
+        jdbcTemplate.execute("set mode MySQL");
+    }
+
+    @Test
+    void initBoardTest() {
+        assertThat(springBoardDao.initBoard(existRoomName))
+                .isEqualTo(Board.getGamingBoard());
+    }
+
+    @Test
+    void newBoardTest() {
+        assertThat(springBoardDao.newBoard("newRoomName"))
+                .isEqualTo(Board.getGamingBoard().getBoard());
+    }
+
+    @Test
+    void findBoardTest() {
+        assertThat(springBoardDao.findBoard(existRoomName).get())
+                .isEqualTo(Board.getGamingBoard());
+    }
+
+    @Test
+    void findTurnTest() {
+        assertThat(springBoardDao.findTurn(existRoomName).get())
+                .isEqualTo(Side.WHITE);
+    }
+}

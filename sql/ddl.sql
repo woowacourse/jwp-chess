d4e282fb1e018c987c97acee6cdf7d181a41198e
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-04-25 17:07:31 +0900

    refactor(ddl.sql): 테이블 구조 변경

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,25 +4,25 @@ CREATE DATABASE chess_db;
 USE chess_db;
 
 DROP TABLE IF EXISTS game_room_info CASCADE;
-DROP TABLE IF EXISTS chess_game CASCADE;
+DROP TABLE IF EXISTS chess_game_info CASCADE;
 DROP TABLE IF EXISTS team_info CASCADE;
 
 CREATE TABLE game_room_info (
-    game_id int NOT NULL AUTO_INCREMENT,
+    room_id int NOT NULL AUTO_INCREMENT,
     room_name VARCHAR(50),
-    PRIMARY KEY (game_id)
+    PRIMARY KEY (room_id)
 );
 
-CREATE TABLE chess_game (
-    game_id int NOT NULL,
+CREATE TABLE chess_game_info (
+    room_id int NOT NULL,
     current_turn_team VARCHAR(5) NOT NULL,
     is_playing boolean NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES game_room_info(game_id)
+    FOREIGN KEY (room_id) REFERENCES game_room_info(room_id)
 );
 
 CREATE TABLE team_info (
-    game_id int NOT NULL,
+    room_id int NOT NULL,
     team VARCHAR(5) NOT NULL,
     piece_info VARCHAR(400) NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES game_room_info(game_id)
+    FOREIGN KEY (room_id) REFERENCES game_room_info(room_id)
 );

66e1bdad517445823a035bb281cd7e5cd8caafb6
xrabcde <xrabcde@gmail.com>
2021-04-25 22:23:27 +0900

    refactor(BoardDto): gameOverFlag 접근제어자 수정

--- src/main/java/chess/domain/dto/BoardDto.java
+++ src/main/java/chess/domain/dto/BoardDto.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class BoardDto {
-    public boolean gameOverFlag = false;
+    private boolean gameOverFlag = false;
     private Map<String, String> boardInfo = new HashMap<>();
 
     public BoardDto() {

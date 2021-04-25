d74731ee72f4e6520c766f194c3655c8c0744b0b
cjy17 <cjy1705@naver.com>
2021-04-25 21:35:21 +0900

    refactor : 이어하기에서 방번호 미입력시 이동하지 않도록 수정

--- src/main/resources/static/js/Home.js
+++ src/main/resources/static/js/Home.js
@@ -13,8 +13,15 @@ window.onload = function () {
 }
 
 async function startNewGame(e) {
+    const roomName = prompt("방 제목을 입력하세요");
     const whiteUserName = prompt("흰색 유저 이름을 입력하세요.");
     const blackUserName = prompt("검정색 유저 이름을 입력하세요.");
+
+    try{
+        await validateRoomName(roomName);
+    } catch (e) {
+        alert("중복된 이름이거나, 올바르지 않은 입력입니다.")
+    }
 
     try {
         validateName(whiteUserName, blackUserName);
@@ -30,7 +37,7 @@ async function startNewGame(e) {
             alert("존재하지 않는 유저가 있습니다.");
             return;
         }
-        await createGame(host["id"], guest["id"]);
+        await createGame(roomName, host["id"], guest["id"]);
     } catch (e) {
         alert(e);
     }
@@ -42,6 +49,22 @@ async function getExistentUser(userName) {
     }
     return await getData(`${url}/users`, params);
 }
+
+async function validateRoomName(roomName) {
+    if (roomName.length === 0) {
+        throw Error("방 제목을 입력하세요");
+    }
+
+    const params = {
+        name : roomName
+    }
+    const room = await getData('${url}/games/rooms/check', params);
+    if(room != null) {
+        throw Error("존재하는 방이름 입니다.");
+    }
+
+}
+
 
 function validateName(whiteName, blackName) {
     if (whiteName.length === 0 || blackName.length === 0) {
@@ -66,9 +89,9 @@ function isEmptyObject(object) {
 }
 
 
-async function createGame(hostId, guestId) {
+async function createGame(name, hostId, guestId) {
     const body = {
-        name: "임시 방이름",
+        name: name,
         hostId: hostId,
         guestId: guestId
     };
@@ -78,5 +101,7 @@ async function createGame(hostId, guestId) {
 async function loadGame() {
     const roomNumbers = await getData(`${url}/games/room`);
     const gameId = prompt("이동할 방번호를 입력하세요. \n" + "저장되어 있는 방 번호입니다 : " + roomNumbers["roomNumbers"]);
-    window.location.href = `${url}/games/${gameId}`
+    if (gameId != null) {
+        window.location.href = `${url}/games/${gameId}`;
+    }
 }

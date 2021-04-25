897bf85bf5d20d1cf613f9b2b707d1b7af7143fe
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-25 15:35:50 +0900

    feat: 테스트 DisplayName 추가

--- src/test/java/chess/controller/SpringChessControllerTest.java
+++ src/test/java/chess/controller/SpringChessControllerTest.java
@@ -3,6 +3,7 @@ package chess.controller;
 import chess.dto.PositionDTO;
 import chess.dto.RoomNameDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -30,6 +31,7 @@ public class SpringChessControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("현재 방 이름을 잘 반환하는지 테스트")
     void currentRoomTest() throws Exception{
         mockMvc.perform(get("/currentRoom")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -40,6 +42,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("재시작 테스트")
     void restartTest() throws Exception {
         mockMvc.perform(post("/restart")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -48,6 +51,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("이동 성공 테스트")
     void succeedMoveTest() throws Exception {
         String content = objectMapper.writeValueAsString(new PositionDTO("a2", "a4"));
 
@@ -61,6 +65,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("이동 실패 테스트")
     void failMoveTest() throws Exception {
         String content = objectMapper.writeValueAsString(new PositionDTO("a1", "a4"));
 
@@ -72,6 +77,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("현재 보드 정보 테스트")
     void currentBoardTest() throws Exception {
         mockMvc.perform(get("/currentBoard")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -80,6 +86,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("현재 플레이어 턴 반환 테스트")
     void currentTurnTest() throws Exception {
         mockMvc.perform(post("/currentTurn")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -89,6 +96,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("현재 점수 반환 테스트")
     void scoreTest() throws Exception {
         mockMvc.perform(post("/score")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -99,6 +107,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("생성되어있는 방 리스트 테스트")
     void roomsTest() throws Exception {
         mockMvc.perform(get("/rooms")
             .contentType(MediaType.APPLICATION_JSON)
@@ -107,6 +116,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("중복되지 않은 방을 입력했을 때 테스트")
     void succeedCheckRoomNameTest() throws Exception {
         String content = objectMapper.writeValueAsString(new RoomNameDTO("newRoomName"));
 
@@ -119,6 +129,7 @@ public class SpringChessControllerTest {
     }
 
     @Test
+    @DisplayName("중복된 방을 입력했을 때 테스트")
     void duplicatedCheckRoomNameTest() throws Exception {
         String content = objectMapper.writeValueAsString(new RoomNameDTO("testRoomName"));
 

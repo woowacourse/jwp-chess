b6895a8219f226e0f7c85111d0f5e979c0d1f5cd
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-25 21:56:34 +0900

    js 인덴트 1로 변경

--- src/main/resources/static/JS/main.js
+++ src/main/resources/static/JS/main.js
@@ -6,6 +6,12 @@ roomTable.addEventListener("click", clickRoomName);
 deleteButton.addEventListener("click", deleteRoom);
 roomTable.addEventListener("dblclick", joinRoom);
 
+const MIN_ROOM_NAME_LENGTH = 1;
+const MAX_ROOM_NAME_LENGTH = 10;
+
+const SUCCEED_HTTP_CODE = 200;
+const FAIL_HTTP_CODE = 400;
+
 window.onload = function () {
     renderRooms();
 }
@@ -20,20 +26,25 @@ function joinRoom(event) {
 async function deleteRoom() {
     const clickedRoom = getClickedRoom();
     if (clickedRoom) {
-        if (!confirm(clickedRoom.textContent + " 방을 삭제하시겠습니까?")) {
-            return;
-        }
-        let data = {
-            roomName: clickedRoom.textContent
-        }
-        await fetch('/deleteRoom/', {
-            method: 'delete',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        await ansDeleteRoom(clickedRoom);
     }
+}
+
+async function ansDeleteRoom(clickedRoom) {
+    if (!confirm(clickedRoom.textContent + " 방을 삭제하시겠습니까?")) {
+        return;
+    }
+
+    let data = {
+        roomName: clickedRoom.textContent
+    }
+    await fetch('/deleteRoom/', {
+        method: 'delete',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     renderRooms();
 }
 
@@ -42,17 +53,22 @@ function clickRoomName(event) {
         const clickRoom = event.target.closest("td");
         const clickedRoom = getClickedRoom();
 
-        if (clickedRoom) {
-            clickedRoom.classList.remove("clickedRoomName");
-            clickedRoom.classList.toggle("clicked");
-        }
+        toggleClicked(clickedRoom);
         clickRoom.classList.toggle("clicked");
         clickRoom.classList.add("clickedRoomName");
     }
 }
 
+function toggleClicked(clickedRoom) {
+    if (clickedRoom) {
+        clickedRoom.classList.remove("clickedRoomName");
+        clickedRoom.classList.toggle("clicked");
+    }
+}
+
 function getClickedRoom() {
     const room = document.getElementsByTagName("td");
+
     for (let i = 0; i < room.length; i++) {
         if (room[i].classList.contains("clicked")) {
             return room[i];
@@ -64,36 +80,52 @@ function getClickedRoom() {
 async function addRoom(event) {
     const roomName = event.target.value;
     if (event.key === "Enter" && roomName !== "") {
-        if (0 > roomName.length && 10 < roomName.length) {
-            alert("방 이름은 한 글자 이상 열 글자 이하여야 합니다.")
-            return;
-        }
-        let data = {
-            roomName: roomName
-        }
-        let response = await fetch('/checkRoomName', {
-            method: 'post',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const status = response.status;
-
-        if (status === 200) {
-            response = await response.json();
-            createRoom(roomName);
-            alert(response.message);
-        }
-
-        if (status === 400) {
-            response = await response.text();
-            alert(response);
-        }
-        event.target.value = '';
-        renderRooms();
+        await executeAddRoom(roomName, event);
     }
+}
+
+async function requestAddRoom(roomName) {
+    let data = {
+        roomName: roomName
+    }
+    let response = await fetch('/checkRoomName', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return response;
+}
+
+async function executeAddRoom(roomName, event) {
+    if (validateRoomNameLength(roomName)) {
+        alert("방 이름은 한 글자 이상 열 글자 이하여야 합니다.")
+        return;
+    }
+    let response = await requestAddRoom(roomName);
+    await executeByStatus(response, roomName);
+    event.target.value = '';
+    renderRooms();
+}
+
+async function executeByStatus(response, roomName) {
+    const status = response.status;
+
+    if (status === SUCCEED_HTTP_CODE) {
+        response = await response.json();
+        createRoom(roomName);
+        alert(response.message);
+    }
+
+    if (status === FAIL_HTTP_CODE) {
+        response = await response.text();
+        alert(response);
+    }
+}
+
+function validateRoomNameLength(roomName) {
+    return MIN_ROOM_NAME_LENGTH >= roomName.length && MAX_ROOM_NAME_LENGTH < roomName.length;
 }
 
 async function renderRooms() {

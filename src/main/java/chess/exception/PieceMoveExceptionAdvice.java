4bae7d1f89db6eba4edfb68f86917ab468f6ebb7
jujubebat <bbwwpark@naver.com>
2021-04-26 22:40:21 +0900

    refactor: 에러 발생시 적절한 http 상태코드를 리턴하도록 수정

--- src/main/java/chess/exception/PieceMoveExceptionAdvice.java
+++ src/main/java/chess/exception/PieceMoveExceptionAdvice.java
@@ -1,5 +1,6 @@
 package chess.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,17 +10,17 @@ public class PieceMoveExceptionAdvice {
 
     @ExceptionHandler(PieceMoveException.class)
     public ResponseEntity<String> pieceMoveExceptionHandle(PieceMoveException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(NotExistDataException.class)
     public ResponseEntity<String> notExistDataExceptionHandle(NotExistDataException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(DataBaseException.class)
     public ResponseEntity<String> dataBaseExceptionHandle(DataBaseException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
 }

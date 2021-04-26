96f2b276f0ba5531c04042ef07e48fd90d41fe49
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-27 00:10:37 +0900

    feat : 클라이언트 브라우저에서 방 이름과 비밀번호를 입력해 방을 생성하는 기능 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,4 @@
-import {getFetch} from "./promise/fetches.js"
+import {getFetch, postFetch} from "./promise/fetches.js"
 
 const $startBtn = document.querySelector('#startBtn');
 const $games = document.querySelector('#games');
@@ -9,21 +9,33 @@ $startBtn.addEventListener("click", onClickStartBtn);
 $games.addEventListener("click", onClickGameRoom);
 
 async function onClickStartBtn(e) {
-    await getFetch("/game/start", 0);
+    const roomName = prompt("방이름 입력해달라");
+    isEmpty(roomName);
+    const password = prompt("비밀번호를 입력해달라");
+    isEmpty(password);
+
+    await postFetch("/room", {roomName: roomName, password: password});
     findGames();
+}
+
+function isEmpty(obj) {
+    if (!obj) {
+        alert("무언가 입력하라");
+        throw new Error("유저 입력 비어있음");
+    }
 }
 
 function onClickGameRoom(e) {
     if (e.target && e.target.classList.contains('room')) {
-        location.href = `/game/${e.target.closest('.text').id}`;
+        location.href = `/room/${e.target.closest('.text').id}`;
     }
 }
 
 async function findGames() {
     const responseData = await getFetch("/room");
-    const games = responseData.runningGames;
+    const games = responseData.activeRooms;
 
     $games.innerHTML = "<div class='text rainbow'>{ </div>";
-    Object.keys(games).forEach(ele => $games.insertAdjacentHTML("beforeend", `<div id="${ele}" class="text rainbow">&nbsp;&nbsp;<span class="room">{"gameID":"${ele}" , "next_turn":"${games[ele]}"},</span><img class="imgg" src="../img/tenor.gif"/></div>`));
+    Object.keys(games).forEach(ele => $games.insertAdjacentHTML("beforeend", `<div id="${ele}" class="text rainbow">&nbsp;&nbsp;<span class="room">{"roomId":"${ele}" , "roomName":"${games[ele]}"},</span><img class="imgg" src="../img/tenor.gif"/></div>`));
     $games.insertAdjacentHTML("beforeend", "<div class='text rainbow'>} </div>");
 }

06eb0a34d87e98539f55af7d7bfc14950dbc9afb
멍토 <daum7766@naver.com>
2021-04-26 22:19:38 +0900

    refactor: 에러상황 추가

--- src/main/java/chess/advice/ChessAdvice.java
+++ src/main/java/chess/advice/ChessAdvice.java
@@ -1,5 +1,6 @@
 package chess.advice;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ChessAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
+    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class, EmptyResultDataAccessException.class})
     public ResponseEntity IllegalArgumentExceptionHandler() {
         return ResponseEntity.badRequest().build();
     }

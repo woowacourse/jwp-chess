468a2a107301dc262f675878edf898e27523f486
Sehwan-Jang <whilter08@gmail.com>
2021-04-26 14:05:57 +0900

    style : schema.sql 가독성 개선

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,44 +1,16 @@
 CREATE TABLE IF NOT EXISTS chessroom
 (
-    room_id
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    room_name
-    VARCHAR
-(
-    255
-) NOT NULL,
-    PRIMARY KEY
-(
-    room_id
-)
-    );
+    room_id    INT          NOT NULL    AUTO_INCREMENT,
+    room_name  VARCHAR(255) NOT NULL,
+    PRIMARY KEY(room_id)
+);
 
 CREATE TABLE IF NOT EXISTS chessgame
 (
-    command_log
-    INT
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    room_id
-    INT
-    NOT
-    NULL,
-    target
-    VARCHAR
-(
-    12
-) NOT NULL,
-    destination VARCHAR
-(
-    12
-) NOT NULL,
-    command_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    command_log
-)
-    );
+    command_log     INT         NOT NULL     AUTO_INCREMENT,
+    room_id         INT         NOT NULL,
+    target          VARCHAR(12) NOT NULL,
+    destination     VARCHAR(12) NOT NULL,
+    command_date    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(command_log)
+);

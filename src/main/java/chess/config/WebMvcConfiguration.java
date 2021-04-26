bc39ebb9012649423f669482065c8f1a1b2af5d9
jujubebat <bbwwpark@naver.com>
2021-04-26 16:54:10 +0900

    feat: http 요청, 응답 body와 파라미터 로깅하는 기능 구현

---
+++ src/main/java/chess/config/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package chess.config;
+
+import chess.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor).addPathPatterns("/api/**")
+            .excludePathPatterns("/vendor/**", "/css/*", "/img/*");
+    }
+
+}
+

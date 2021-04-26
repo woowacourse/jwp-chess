6de8c8622652fa02ccb58e12caf4fb839e7a3748
hyeon9mak <jinha3507@gmail.com>
2021-04-26 12:43:20 +0900

    refactor: fix database table columns

--- src/initializeDB.sql
+++ src/initializeDB.sql
@@ -6,27 +6,19 @@ USE
 chess;
 
 -- 초기 테이블 생성
-CREATE TABLE user
-(
-    id       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nickname VARCHAR(64) NOT NULL
-);
-
-CREATE TABLE result
-(
-    id      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    game_id INT NOT NULL, -- room_id와 연결
-    winner  INT NOT NULL, -- user.id와 연결
-    loser   INT NOT NULL  -- user.id와 연결
-);
-
 CREATE TABLE room
 (
     id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title      VARCHAR(64) NOT NULL,
-    black_user INT         NOT NULL, -- user.id와 연결
-    white_user INT         NOT NULL, -- user.id와 연결
-    status     TINYINT(4) NOT NULL   -- 0 -> 종료된 게임을 뜻함 / 1 -> 진행중인 게임을 뜻함
+    status     VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE user
+(
+    id       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    room_id  INT         NOT NULL,
+    nickname VARCHAR(64) NOT NULL UNIQUE,
+    password VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE log
@@ -38,9 +30,12 @@ CREATE TABLE log
     register_date  timestamp DEFAULT NOW()
 );
 
+CREATE TABLE result
+(
+    id      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL, -- room_id와 연결
+    winner  INT NOT NULL, -- user.id와 연결
+    loser   INT NOT NULL  -- user.id와 연결
+);
+
 -- 초기 데이터 생성
-INSERT INTO user (nickname)
-VALUES ('blackAir'),
-       ('whiteAir');
-
-

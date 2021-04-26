d4800ef128deeafaaeb19054a7bad01c165cef5d
멍토 <daum7766@naver.com>
2021-04-26 23:40:18 +0900

    refactor: 상황에 맞게 에러 출력되도록 변경

--- src/main/java/chess/advice/ChessAdvice.java
+++ src/main/java/chess/advice/ChessAdvice.java
@@ -8,8 +8,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ChessAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class, EmptyResultDataAccessException.class})
-    public ResponseEntity IllegalArgumentExceptionHandler() {
-        return ResponseEntity.badRequest().build();
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity illegalArgumentExceptionHandler() {
+        ExceptionState exceptionState = ExceptionState.ILLEGAL_ARGUMENT;
+        return ResponseEntity.status(exceptionState.getStatus()).body(exceptionState.getErrorMessage());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity nullPointerExceptionHandler() {
+        ExceptionState exceptionState = ExceptionState.NULL_POINTER;
+        return ResponseEntity.status(exceptionState.getStatus()).body(exceptionState.getErrorMessage());
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity emptyResultDataAccessExceptionHandler() {
+        ExceptionState exceptionState = ExceptionState.EMPTY_RESULT_DATA_ACCESS;
+        return ResponseEntity.status(exceptionState.getStatus()).body(exceptionState.getErrorMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity exceptionHandler() {
+        ExceptionState exceptionState = ExceptionState.EXCEPTION;
+        return ResponseEntity.status(exceptionState.getStatus()).body(exceptionState.getErrorMessage());
     }
 }

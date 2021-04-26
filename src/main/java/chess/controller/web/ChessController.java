4dbccded06867f9f670c63a55f908268ed2302e7
NewWisdom <wlgp2500@gmail.com>
2021-04-26 21:29:22 +0900

    refactor(): response-entity 사용하도록 변경

--- src/main/java/chess/controller/web/ChessController.java
+++ src/main/java/chess/controller/web/ChessController.java
@@ -4,6 +4,7 @@ import chess.controller.web.dto.*;
 import chess.domain.manager.ChessGameManager;
 import chess.domain.manager.ChessGameManagerBundle;
 import chess.service.ChessService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -17,38 +18,38 @@ public class ChessController {
     }
 
     @GetMapping("/games")
-    public RunningGameResponseDto getGames() {
+    public ResponseEntity<RunningGameResponseDto> getGames() {
         ChessGameManagerBundle runningGames = chessService.findRunningGames();
-        return new RunningGameResponseDto(runningGames.getIdAndNextTurn());
+        return ResponseEntity.ok(new RunningGameResponseDto(runningGames.getIdAndNextTurn()));
     }
 
     @PostMapping("/game/start")
-    public ChessGameResponseDto gameStart(@RequestBody Map<String, String> param) {
+    public ResponseEntity<ChessGameResponseDto> gameStart(@RequestBody Map<String, String> param) {
         String title = param.get("title");
-        return new ChessGameResponseDto(chessService.start(title));
+        return ResponseEntity.ok(new ChessGameResponseDto(chessService.start(title)));
     }
 
     @GetMapping("/games/{id:[\\d]+}/score")
-    public ScoreResponseDto getScore(@PathVariable long id) {
-        return new ScoreResponseDto(chessService.getStatistics(id));
+    public ResponseEntity<ScoreResponseDto> getScore(@PathVariable long id) {
+        return ResponseEntity.ok(new ScoreResponseDto(chessService.getStatistics(id)));
     }
 
     @GetMapping("/games/{id:[\\d]+}/load")
-    public ChessGameResponseDto loadGame(@PathVariable long id) {
+    public ResponseEntity<ChessGameResponseDto> loadGame(@PathVariable long id) {
         ChessGameManager load = chessService.findById(id);
-        return new ChessGameResponseDto(load);
+        return ResponseEntity.ok(new ChessGameResponseDto(load));
     }
 
     @GetMapping("/games/{id:[\\d]+}/reset")
-    public ChessGameResponseDto resetGame(@PathVariable long id){
+    public ResponseEntity<ChessGameResponseDto> resetGame(@PathVariable long id){
         ChessGameManager resetGame = chessService.reset(id);
-        return new ChessGameResponseDto(resetGame);
+        return ResponseEntity.ok(new ChessGameResponseDto(resetGame));
     }
 
 
     @PostMapping("/games/{id:[\\d]+}/move")
-    public MoveResponseDto movePiece(@PathVariable long id, @RequestBody MoveRequestDto moveMessage) {
+    public ResponseEntity<MoveResponseDto> movePiece(@PathVariable long id, @RequestBody MoveRequestDto moveMessage) {
         chessService.move(id, moveMessage);
-        return new MoveResponseDto(chessService.isEnd(id), chessService.nextColor(id));
+        return ResponseEntity.ok(new MoveResponseDto(chessService.isEnd(id), chessService.nextColor(id)));
     }
 }

1dbcdf9710cf67364766ba097bbf40f73aa9ba76
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-26 23:49:36 +0900

    feat : roomId로 게임을 찾는 기능 추가

--- src/main/java/chess/controller/web/RoomController.java
+++ src/main/java/chess/controller/web/RoomController.java
@@ -1,5 +1,7 @@
 package chess.controller.web;
 
+import chess.chessgame.domain.room.game.ChessGameManager;
+import chess.service.RoomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/room")
 @Controller
 public class RoomController {
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
 
     @GetMapping("{roomId:[\\d]+}")
     public String enterRoom(@PathVariable long roomId, Model model) {
-        model.addAttribute("roomId", roomId);
+        ChessGameManager game = roomService.findGameBy(roomId);
+        model.addAttribute("gameId", game.getId());
         return "chess";
     }
 }

36706d80f4d51793a7604a2dc0f72f7f02ecb045
멍토 <daum7766@naver.com>
2021-04-26 16:51:36 +0900

    refactor: 안쓰는 반환타입 수정

--- src/main/java/chess/dao/RoomDao.java
+++ src/main/java/chess/dao/RoomDao.java
@@ -4,11 +4,8 @@ package chess.dao;
 import chess.domain.Room;
 import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.Objects;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,18 +16,15 @@ public class RoomDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public long create(Room room) {
+    public void create(Room room) {
         String sql = "insert into room (name, pw, game_id) values (?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(con -> {
-            PreparedStatement preparedStatement = con.prepareStatement(sql, new String[]{"game_id"});
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
             preparedStatement.setString(1, room.getName());
             preparedStatement.setString(2, room.getPw());
             preparedStatement.setLong(3, room.getGameId());
             return preparedStatement;
-        }, keyHolder);
-
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+        });
     }
 
     public Room load(long roomId) {

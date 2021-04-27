b9e8606273bbdaf37201e79ba1fc1b92236cb05a
dwl5 <ssop6403@gmail.com>
2021-04-27 12:02:24 +0900

    feat: User 생성 기능 추가

--- src/main/java/chess/controller/ChessUserController.java
+++ src/main/java/chess/controller/ChessUserController.java
@@ -1,17 +1,15 @@
 package chess.controller;
 
-import chess.domain.User;
 import chess.service.ChessUserService;
 import dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class ChessUserController {
 
     private final ChessUserService chessUserService;
@@ -21,14 +19,8 @@ public class ChessUserController {
         this.chessUserService = chessUserService;
     }
 
-    @PostMapping()
+    @PostMapping("/user")
     public void create(@RequestBody UserDto userDto) {
         chessUserService.create(userDto.getName(), userDto.getPw());
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<UserDto> login(@RequestBody UserDto userDto) {
-        UserDto response = chessUserService.login(userDto.getName(), userDto.getPw());
-        return ResponseEntity.ok(response);
     }
 }

aca978008a1695fb1f55b9674ea9be870b5ffdb2
binghe819 <binghe819@naver.com>
2021-04-27 15:48:37 +0900

    refactor: 예외 응답 코드 상황별 알맞게 수정

--- src/main/java/chess/controller/chess/ChessExceptionHandler.java
+++ src/main/java/chess/controller/chess/ChessExceptionHandler.java
@@ -3,7 +3,9 @@ package chess.controller.chess;
 import chess.controller.ApiError;
 import chess.exception.BusinessException;
 import chess.exception.ErrorCode;
+import org.eclipse.jetty.http.HttpStatus;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,11 +20,22 @@ public class ChessExceptionHandler {
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ApiError> dbExceptionHandler(DataAccessException e) {
-        return ResponseEntity.badRequest().body(ApiError.of(ErrorCode.DB_COMMON, e.getMessage()));
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR_500)
+            .body(ApiError.of(ErrorCode.DB_COMMON, e.getMessage()));
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ApiError> dbSelectExceptionHandler(EmptyResultDataAccessException e) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND_404)
+            .body(ApiError.of(ErrorCode.NOT_FOUND, e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiError> etcExceptionHandler(RuntimeException e) {
-        return ResponseEntity.badRequest().body(ApiError.of(ErrorCode.ETC, e.getMessage()));
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR_500)
+            .body(ApiError.of(ErrorCode.ETC, e.getMessage()));
     }
 }

850ce370a52555361208eead6aa649af478804e9
Kimun Kim <kkm97351@gmail.com>
2021-04-27 17:43:36 +0900

    test: ChessRestController MockMvc를 이용해 테스트 코드 작성

--- src/test/java/chess/controller/ChessRestControllerTest.java
+++ src/test/java/chess/controller/ChessRestControllerTest.java
@@ -1,102 +1,69 @@
 package chess.controller;
 
-import chess.domain.ChessGameManager;
 import chess.dto.CreateGameRequest;
-import chess.repository.GameRepository;
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import chess.dto.MoveRequest;
+import chess.service.ChessGameService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class ChessRestControllerTest {
-    @LocalServerPort
-    int port;
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ChessGameService chessGameService;
 
     @Autowired
-    GameRepository gameRepository;
+    ObjectMapper objectMapper;
 
-    private ChessGameManager chessGameManager;
-    long gameId;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-        this.chessGameManager = new ChessGameManager();
-        this.chessGameManager.start();
-        this.gameId = gameRepository.save(chessGameManager, "test title");
-    }
-
-    @AfterEach
-    void flush() {
-        gameRepository.delete(this.gameId);
-    }
-
-    @DisplayName("게임 목록을 불러온다")
+    @DisplayName("게임 목록 불러오기 [GET] /games의 정상 작동")
     @Test
-    void requestGamesTest() {
-        RestAssured.given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/games")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .body("message", is("게임 목록을 불러왔습니다."))
-                .body("item.games.gameId", hasItem((int)this.gameId))
-                .body("item.games.title", hasItem("test title"));
+    void requestGamesTest() throws Exception {
+        this.mockMvc.perform(get("/games"))
+                .andExpect(status().isOk());
     }
 
-    @DisplayName("게임을 생성한다.")
+    @DisplayName("게임 생성 요청 ")
     @Test
-    void createNewGameTest() {
-        CreateGameRequest createGameRequest = new CreateGameRequest("test title");
+    void createNewGameTest() throws Exception {
+        String content = objectMapper.writeValueAsString(new CreateGameRequest("test title"));
 
-        RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(createGameRequest)
-                .when().post("/games")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .body("item.gameId", is((int)this.gameId + 1));
+        this.mockMvc.perform(post("/games")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isCreated());
+    }
 
-        this.gameRepository.delete(this.gameId + 1);
+    @DisplayName("기물을 이동한다.")
+    @Test
+    void movePieceTest() throws Exception {
+        String content = objectMapper.writeValueAsString(new MoveRequest(1, "a2", "a4"));
+
+        this.mockMvc.perform(put("/games/1/pieces")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isOk());
     }
 
 //    @DisplayName("게임을 불러온다.")
 //    @Test
-//    void loadGameTest() {
-//        RestAssured.given().log().all()
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .when().get("/games/{gameId}", this.gameId)
-//                .then().log().all()
-//                .statusCode(HttpStatus.OK.value());
-
-//    }
-
-//    @DisplayName("기물을 이동한다.")
-//    @Test
-//    void movePieceTest() {
-//        MoveRequest moveRequest = new MoveRequest(this.gameId, "a2", "a4");
-//
-//        RestAssured.given().log().all()
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .body(moveRequest)
-//                .when().put("/games/{gameId}/pieces", this.gameId)
-//                .then().log().all()
-//                .statusCode(HttpStatus.OK.value())
-//                .body("item.chessBoard", hasItem("a4"));
+//    void loadGameTest() throws Exception {
+//        this.mockMvc.perform(get("/games/1"))
+//                .andExpect(status().isOk());
 //    }
 
 }

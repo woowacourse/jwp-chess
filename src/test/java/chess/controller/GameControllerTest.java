120720469e2bc39867b18c2e55766cc3b890dc20
Sol ji Choi <bssj9307@gmail.com>
2021-04-27 15:39:50 +0900

    test: GameControllerTest 단위 테스트로 변경 - service를 주입받아 테스트

--- src/test/java/chess/controller/GameControllerTest.java
+++ src/test/java/chess/controller/GameControllerTest.java
@@ -1,23 +1,29 @@
 package chess.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import chess.controller.api.GameController;
+import chess.domain.board.ChessBoard;
+import chess.domain.game.ChessGame;
+import chess.domain.piece.Color;
 import chess.dto.MoveDto;
-import chess.dto.TitleDto;
+import chess.service.GameService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -25,53 +31,65 @@ import org.springframework.transaction.annotation.Transactional;
 @AutoConfigureMockMvc
 public class GameControllerTest {
 
+    @Mock
+    private GameService gameService;
+    @InjectMocks
+    private GameController gameController;
+
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ObjectMapper objectMapper;
 
-    private String id;
 
     @BeforeEach
-    void setUp() throws Exception {
-        String content = objectMapper.writeValueAsString(new TitleDto("test-room"));
-
-        MvcResult mvcResult = mockMvc.perform(post("/game")
-            .content(content)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
-        id = mvcResult.getResponse().getContentAsString();
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(gameController).build();
     }
 
     @Test
     void loadGame() throws Exception {
-        mockMvc.perform(get("/room/" + id + "/game-info"))
+        when(gameService.loadGame(1L))
+            .thenReturn(new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-room"));
+
+        mockMvc.perform(get("/room/1/game-info"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("id").value(id));
+            .andExpect(jsonPath("id").value(1));
     }
 
     @Test
     void move() throws Exception {
         String content = objectMapper.writeValueAsString(new MoveDto("b2", "b3"));
 
-        mockMvc.perform(put("/room/" + id + "/move")
+        when(gameService.loadGame(1L))
+            .thenReturn(new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-room"));
+
+        mockMvc.perform(put("/room/1/move")
             .content(content)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("id").value(id));
+            .andExpect(jsonPath("id").value(1));
     }
 
     @Test
     void finish() throws Exception {
-        mockMvc.perform(post("/room/" + id + "/finish"))
+        when(gameService.loadGame(1L))
+            .thenReturn(new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-room"));
+
+        mockMvc.perform(post("/room/1/finish"))
             .andExpect(status().isOk());
     }
 
     @Test
     void restart() throws Exception {
-        mockMvc.perform(post("/room/" + id + "/restart"))
+        when(gameService.restart(1L))
+            .thenReturn(new ChessGame(1L, Color.BLACK, false, new ChessBoard(), "test-room"));
+
+        mockMvc.perform(post("/room/1/restart"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("id").value(id))
-            .andExpect(jsonPath("finished").value(false));
+            .andExpect(jsonPath("id").value(1))
+            .andExpect(jsonPath("finished").value(false))
+            .andExpect(jsonPath("turn").value("BLACK"));
     }
 }

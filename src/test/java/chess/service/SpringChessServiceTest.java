9adfcf05070fb02a7fdbc471dfbd4b8015747a13
naeunkim <knae11@naver.com>
2021-04-27 21:27:30 +0900

    test(service): move 테스트

--- src/test/java/chess/service/SpringChessServiceTest.java
+++ src/test/java/chess/service/SpringChessServiceTest.java
@@ -1,9 +1,16 @@
 package chess.service;
 
 import chess.domain.ChessGame;
+import chess.domain.Position;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Rook;
+import chess.domain.team.PiecePositions;
 import chess.domain.team.Team;
 import chess.webdao.MysqlChessDao;
+import chess.webdto.dao.BoardInfosDto;
+import chess.webdto.dao.TurnDto;
 import chess.webdto.view.ChessGameDto;
+import chess.webdto.view.MoveRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +20,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class SpringChessServiceTest {
@@ -23,7 +33,7 @@ class SpringChessServiceTest {
     @Mock
     MysqlChessDao mysqlChessDao;
 
-    static ObjectMapper objectMapper;
+    private static ObjectMapper objectMapper;
 
     @BeforeAll
     static void beforeAll() {
@@ -33,12 +43,46 @@ class SpringChessServiceTest {
     @Test
     @DisplayName("새로운 게임 시작하기")
     void startNewGame() throws Exception {
-        ChessGameDto original = new ChessGameDto(new ChessGame(Team.blackTeam(), Team.whiteTeam()));
+        ChessGameDto expected = new ChessGameDto(new ChessGame(Team.blackTeam(), Team.whiteTeam()));
 
         ChessGameDto result = springChessService.startNewGame();
 
-        assertThat(objectMapper.writeValueAsString(result)).isEqualTo(objectMapper.writeValueAsString(original));
+        assertThat(objectMapper.writeValueAsString(result)).isEqualTo(objectMapper.writeValueAsString(expected));
     }
 
+    @Test
+    @DisplayName("움직인 경우 테스트 - Rook a2 -> a3")
+    void move() throws Exception {
+        // given
+        MoveRequestDto moveRequestDto = new MoveRequestDto();
+        moveRequestDto.setStart("a2");
+        moveRequestDto.setDestination("a3");
 
+        TurnDto turnDto = new TurnDto();
+        turnDto.setTurn("white");
+        turnDto.setIsPlaying(true);
+        given(mysqlChessDao.selectTurnByRoomId(1L)).willReturn(turnDto);
+
+        List<BoardInfosDto> boards = new ArrayList<>();
+        BoardInfosDto boardInfosDto = new BoardInfosDto();
+        boardInfosDto.setIsFirstMoved(true);
+        boardInfosDto.setPiece("Rook");
+        boardInfosDto.setPosition("a2");
+        boardInfosDto.setTeam("white");
+        boards.add(boardInfosDto);
+        given(mysqlChessDao.selectBoardInfosByRoomId(1L)).willReturn(boards);
+
+        Map<Position, Piece> setting = new HashMap<>();
+        setting.put(Position.of("a2"), new Rook());
+        Team whiteTeam = new Team(new PiecePositions(setting));
+        ChessGame chessGame = new ChessGame(new Team(new PiecePositions(Collections.emptyMap())),whiteTeam ,whiteTeam , true);
+        chessGame.move(Position.of("a2"), Position.of("a3"));
+        ChessGameDto expected = new ChessGameDto(chessGame);
+
+        // when
+        ChessGameDto result = springChessService.move(moveRequestDto);
+
+        // then
+        assertThat(objectMapper.writeValueAsString(result)).isEqualTo(objectMapper.writeValueAsString(expected));
+    }
 }

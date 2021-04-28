ea2ca1fe8c4794a8c138092d5c19d2a94c18c0f7
Wannte <T.wannte@gmail.com>
2021-04-28 13:39:49 +0900

    refactor(GameDao): insert 부분, GeneratedKeyHolder 로직으로 수정하여 한번의 쿼링
    NullPointerException 이 발생할 수 있는데, 이 부분에 대해서 방어 방법이 있을까?

--- src/main/java/chess/dao/GameDao.java
+++ src/main/java/chess/dao/GameDao.java
@@ -2,9 +2,15 @@ package chess.dao;
 
 import chess.controller.dto.GameDto;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -22,10 +28,14 @@ public class GameDao {
 
     public long insert(String gameName) {
         String insertGame = "INSERT INTO game (game_name) VALUES (?)";
-        String findLastId = "SELECT last_insert_id();";
 
-        jdbcTemplate.update(insertGame, gameName);
-        return jdbcTemplate.queryForObject(findLastId, Long.class);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update((Connection con)-> {
+            PreparedStatement pstmt = con.prepareStatement(insertGame, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, gameName);
+            return pstmt;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
     public List<GameDto> selectAll() {

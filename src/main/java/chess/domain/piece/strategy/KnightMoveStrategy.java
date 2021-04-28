b108303d786eec409aa351ca0a4bdbbb83dc2079
Kimun Kim <kkm97351@gmail.com>
2021-04-28 18:03:09 +0900

    fix: 나이트의 이동 거리 제한이 없는 버그 픽스
    - 방향만 일정하다면(북북동, 북북서, 남남동, 남남서, 동동북, 동동남, 서서북, 서서남), 1번을 초과한 행마로만 갈 수 있는 행마를 할 수 있었던 문제를 해결했습니다.

--- src/main/java/chess/domain/piece/strategy/KnightMoveStrategy.java
+++ src/main/java/chess/domain/piece/strategy/KnightMoveStrategy.java
@@ -1,9 +1,19 @@
 package chess.domain.piece.strategy;
 
+import chess.domain.exception.InvalidMoveStrategyException;
+import chess.domain.order.MoveRoute;
 import chess.domain.position.Direction;
 
 public class KnightMoveStrategy extends DefaultMoveStrategy {
     public KnightMoveStrategy() {
         super(Direction.knightDirection());
+    }
+
+    @Override
+    public boolean canMove(MoveRoute moveRoute) {
+        if (moveRoute.length() > 2) {
+            throw new InvalidMoveStrategyException("나이트가 움직일 수 있는 범위를 넘어섰습니다.");
+        }
+        return super.canMove(moveRoute);
     }
 }

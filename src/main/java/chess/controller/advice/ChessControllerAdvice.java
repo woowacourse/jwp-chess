9fe015c9e1b417264216ff308465bae1330b1d80
Kimun Kim <kkm97351@gmail.com>
2021-04-28 11:46:30 +0900

    refactor: ChessControllerAdvice 리턴 코드 ResponseEntity의 정적 팩토리 메소드를 이용하도록 수정

--- src/main/java/chess/controller/advice/ChessControllerAdvice.java
+++ src/main/java/chess/controller/advice/ChessControllerAdvice.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ChessControllerAdvice {
     @ExceptionHandler(HandledException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(HandledException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
-        return new ResponseEntity<>(
-                new ErrorResponse("예상치 못한 서버 에러가 발생했습니다.\n에러 메세지 : " + e.getMessage()),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("예상치 못한 서버 에러가 발생했습니다.\n에러 메세지 : " + e.getMessage()));
     }
 }

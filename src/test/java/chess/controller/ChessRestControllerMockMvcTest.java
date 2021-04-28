729e19bd38017cc029e28cddf97168a206df2a3e
Kimun Kim <kkm97351@gmail.com>
2021-04-28 17:37:08 +0900

    test: 빈 제목의 게임 생성 요청시 4xx 응답 요청 테스트 코드 작성

--- src/test/java/chess/controller/ChessRestControllerMockMvcTest.java
+++ src/test/java/chess/controller/ChessRestControllerMockMvcTest.java
@@ -1,5 +1,6 @@
 package chess.controller;
 
+import chess.dto.CreateGameRequest;
 import chess.dto.MoveRequest;
 import chess.service.ChessGameService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,8 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +29,17 @@ class ChessRestControllerMockMvcTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @DisplayName("게임 생성 시 빈 제목의 게임을 생성 요청할 경우 400 코드를 응답한다.")
+    @Test
+    void receiveBadRequestOnRequestEmptyTitleGame() throws Exception {
+        String content = objectMapper.writeValueAsString(new CreateGameRequest(""));
+
+        this.mockMvc.perform(post("/games")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isBadRequest());
+    }
 
     @DisplayName("게임 목록 불러오기 [GET] /games의 정상 작동")
     @Test

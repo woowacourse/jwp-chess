5fbac8446cd86047d5c359a350eebc57c442b15c
younghoonkwon <younghoon.kwon91@gmail.com>
2021-04-28 22:25:29 +0900

    feat: HomeControllerTest 추가

---
+++ src/test/java/chess/controller/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package chess.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+class HomeControllerTest {
+
+    @Test
+    void lobby() throws Exception {
+        standaloneSetup(new HomeController()).build()
+                .perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void initBoard() throws Exception {
+        standaloneSetup(new HomeController()).build()
+                .perform(get("/game/{id}", "1"))
+                .andExpect(status().isOk());
+    }
+}

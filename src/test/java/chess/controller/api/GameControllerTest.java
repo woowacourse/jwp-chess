4c2e21d97ebf5767908d84edae32540b76f739b4
younghoonkwon <younghoon.kwon91@gmail.com>
2021-04-30 11:15:26 +0900

    refactor: BDD 스타일로 테스트 수정

--- src/test/java/chess/controller/api/GameControllerTest.java
+++ src/test/java/chess/controller/api/GameControllerTest.java
@@ -25,8 +25,7 @@ import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -46,14 +45,14 @@ class GameControllerTest {
     void loadGame() throws Exception {
         ChessGame chessGame = new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-game");
         ChessGameDto chessGameDto = new ChessGameDto(chessGame);
-        when(gameService.loadGame(1L)).thenReturn(chessGame);
+        given(gameService.loadGame(1L)).willReturn(chessGame);
 
         mockMvc.perform(get("/game/1/game-info"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(chessGameDto)));
 
-        verify(gameService, times(1)).loadGame(1L);
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).loadGame(1L);
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
@@ -62,9 +61,8 @@ class GameControllerTest {
         MoveDto moveDto = new MoveDto("a2", "a3");
         ChessGame chessGame = new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-game");
         chessGame.move(Position.of(moveDto.getSource()), Position.of(moveDto.getTarget()));
-        when(gameService.loadGame(1L))
-                .thenReturn(chessGame);
-        doNothing().when(gameService).move(1L, moveDto);
+        given(gameService.loadGame(1L)).willReturn(chessGame);
+        willDoNothing().given(gameService).move(1L, moveDto);
 
         mockMvc.perform(put("/game/1/move")
                 .content(objectMapper.writeValueAsString(moveDto))
@@ -72,35 +70,35 @@ class GameControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(new ChessGameDto(chessGame))));
 
-        verify(gameService, times(1)).move(any(), any());
-        verify(gameService, times(1)).loadGame(1L);
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).move(any(), any());
+        then(gameService).should(times(1)).loadGame(1L);
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
     @DisplayName("움직일 수 없는 경우 404")
     void moveFailed() throws Exception {
-        doThrow(new IllegalArgumentException()).when(gameService).move(eq(1L), any());
+        willThrow(new IllegalArgumentException()).given(gameService).move(eq(1L), any());
 
         mockMvc.perform(put("/game/1/move")
                 .content(objectMapper.writeValueAsString(new MoveDto("b1", "b2")))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
-        verify(gameService, times(1)).move(any(), any());
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).move(any(), any());
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
     @DisplayName("게임 종료")
     void terminateGame() throws Exception {
-        doNothing().when(gameService).terminateGame(1L);
+        willDoNothing().given(gameService).terminateGame(1L);
 
         mockMvc.perform(post("/game/1/terminate"))
                 .andExpect(status().isOk());
 
-        verify(gameService, times(1)).terminateGame(1L);
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).terminateGame(1L);
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
@@ -108,22 +106,22 @@ class GameControllerTest {
     void restart() throws Exception {
         ChessGame chessGame = new ChessGame(1L, Color.WHITE, false, new ChessBoard(), "test-game");
         ChessGameDto chessGameDto = new ChessGameDto(chessGame);
-        when(gameService.restart(1L)).thenReturn(chessGame);
+        given(gameService.restart(1L)).willReturn(chessGame);
 
         mockMvc.perform(post("/game/1/restart"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(chessGameDto)));
 
-        verify(gameService, times(1)).restart(1L);
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).restart(1L);
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
     @DisplayName("게임 생성 성공")
     void newGameSuccess() throws Exception {
         TitleDto newGameTitle = new TitleDto("test-room");
-        when(gameService.newGame(newGameTitle.getTitle())).thenReturn(1L);
-        doNothing().when(gameService).verifyDuplicateTitleInGames(newGameTitle.getTitle());
+        given(gameService.newGame(newGameTitle.getTitle())).willReturn(1L);
+        willDoNothing().given(gameService).verifyDuplicateTitleInGames(newGameTitle.getTitle());
 
         mockMvc.perform(post("/game")
                 .content(objectMapper.writeValueAsString(newGameTitle))
@@ -131,25 +129,25 @@ class GameControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("1"));
 
-        verify(gameService, times(1)).newGame(newGameTitle.getTitle());
-        verify(gameService, times(1)).verifyDuplicateTitleInGames(newGameTitle.getTitle());
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).newGame(newGameTitle.getTitle());
+        then(gameService).should(times(1)).verifyDuplicateTitleInGames(newGameTitle.getTitle());
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
     @DisplayName("게임 생성 실패")
     void newGameFailed() throws Exception {
         TitleDto newGameTitle = new TitleDto("test-room");
-        when(gameService.newGame(newGameTitle.getTitle())).thenReturn(1L);
-        doThrow(new IllegalArgumentException()).when(gameService).verifyDuplicateTitleInGames(newGameTitle.getTitle());
+        given(gameService.newGame(newGameTitle.getTitle())).willReturn(1L);
+        willThrow(new IllegalArgumentException()).given(gameService).verifyDuplicateTitleInGames(newGameTitle.getTitle());
 
         mockMvc.perform(post("/game")
                 .content(objectMapper.writeValueAsString(newGameTitle))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
-        verify(gameService, times(1)).verifyDuplicateTitleInGames(newGameTitle.getTitle());
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(1)).verifyDuplicateTitleInGames(newGameTitle.getTitle());
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 
     @Test
@@ -160,7 +158,7 @@ class GameControllerTest {
                 new ChessGame(),
                 new ChessGame()
         );
-        when(gameService.findAllGames()).thenReturn(games);
+        given(gameService.findAllGames()).willReturn(games);
         ChessGamesDto chessGamesDto = new ChessGamesDto(
                 gameService.findAllGames()
                         .stream()
@@ -174,7 +172,7 @@ class GameControllerTest {
                 .andDo(print())
                 .andExpect(content().json(objectMapper.writeValueAsString(chessGamesDto)));
 
-        verify(gameService, times(2)).findAllGames();
-        verifyNoMoreInteractions(gameService);
+        then(gameService).should(times(2)).findAllGames();
+        then(gameService).shouldHaveNoMoreInteractions();
     }
 }
--- src/test/java/chess/service/GameServiceTest.java
+++ src/test/java/chess/service/GameServiceTest.java
@@ -1,9 +1,13 @@
 package chess.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GameServiceTest {
 
     @Test

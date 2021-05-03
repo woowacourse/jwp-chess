12fa7b59f5d3739436223054d71aa66de3f78a1b
dwl5 <ssop6403@gmail.com>
2021-05-03 17:21:36 +0900

    refactor: RoomCreateRequest final로 변경

--- src/main/java/chess/dto/request/RoomCreateRequest.java
+++ src/main/java/chess/dto/request/RoomCreateRequest.java
@@ -4,10 +4,10 @@ import javax.validation.constraints.Size;
 
 public class RoomCreateRequest {
     @Size(min = 2, max = 8, message = "방 이름은 2글자 이상 8글자 이하 입니다.")
-    private String roomName;
+    private final String roomName;
     @Size(min = 2, max = 8, message = "방 비밀번호는 2글자 이상 8글자 이하 입니다.")
-    private String roomPw;
-    private String userName;
+    private final String roomPw;
+    private final String userName;
 
     public RoomCreateRequest(final String roomName, final String roomPw, final String userName) {
         this.roomName = roomName;

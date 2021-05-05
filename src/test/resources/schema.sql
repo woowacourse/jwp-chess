e90671cb681508c39c439b16f0e8f48085799ce8
TaewanKimmmm <ktww88@gmail.com>
2021-05-06 00:06:32 +0900

    chore: 테스트 리소스들 테스트 패키지로 이동

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,4 +1,7 @@
-use chess;
+-- use chess;
+
+DROP TABLE IF EXISTS piece;
+DROP TABLE IF EXISTS room;
 
 CREATE TABLE IF NOT EXISTS room (
     room_id int(11) NOT NULL AUTO_INCREMENT,
@@ -13,3 +16,9 @@ CREATE TABLE IF NOT EXISTS piece (
     room_id int(11) NOT NULL,
     CONSTRAINT room_id FOREIGN KEY (room_id) REFERENCES room (room_id)
     ) DEFAULT CHARSET=utf8;
+
+INSERT INTO ROOM(room_id, current_turn, room_name) values(1, 'white', 'room1');
+INSERT INTO ROOM(room_id, current_turn, room_name) values(2, 'white', 'room2');
+INSERT INTO ROOM(room_id, current_turn, room_name) values(3, 'white', 'room3');
+
+INSERT INTO piece(piece_name, piece_position, room_id) values('p', 'e2', 1);

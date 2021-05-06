fe98ab749cd3d6837a9fc4baf0ef7830f59dad56
TaewanKimmmm <ktww88@gmail.com>
2021-05-07 00:32:59 +0900

    refactor: InMemoryRepository DB 종속성 제거

--- src/test/java/chess/repository/InMemoryRepository.java
+++ src/test/java/chess/repository/InMemoryRepository.java
@@ -1,87 +1,78 @@
 package chess.repository;
 
-import chess.dao.PieceDao;
-import chess.dao.RoomDao;
 import chess.domain.board.ChessBoardFactory;
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
-import chess.dto.PieceDto;
-import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-@Repository
 public class InMemoryRepository implements ChessRepository {
-    private final RoomDao roomDao;
-    private final PieceDao pieceDao;
+    private int counter = 0;
 
-    public InMemoryRepository(RoomDao roomDao, PieceDao pieceDao) {
-        this.roomDao = roomDao;
-        this.pieceDao = pieceDao;
-    }
+    private final Map<Integer, Map<String, String>> room = new HashMap<>();
+    private final Map<Integer, Map<String, String>> piece = new HashMap<>();
 
     @Override
     public int makeRoom(Map<String, String> board, String roomName) {
-        int newRoomId = roomDao.insertRoom(roomName);
-        initializePieceStatus(newRoomId, board);
-        return newRoomId;
+        room.put(++counter, new HashMap<String, String>() {{
+            put(roomName, "white");
+        }});
+        piece.put(counter, board);
+        return counter;
     }
 
     @Override
-    public void initializePieceStatus(int roomId, final Map<String, String> board) {
-        for (Map.Entry<String, String> boardStatus : board.entrySet()) {
-            pieceDao.insertInitialPieces(roomId, boardStatus.getValue(), boardStatus.getKey());
-        }
+    public void initializePieceStatus(int roomId, Map<String, String> board) {
+        piece.put(roomId, board);
     }
 
     @Override
     public Map<Position, Piece> getBoardByRoomId(int roomId) {
         Map<String, String> board = new LinkedHashMap<>();
-        List<PieceDto> pieces = pieceDao.getPieces(roomId);
-
-        for (PieceDto piece : pieces) {
-            board.put(piece.getPosition(), piece.getName());
+        for (Map.Entry<String, String> cell : piece.get(roomId).entrySet()) {
+            board.put(cell.getKey(), cell.getValue());
         }
         return ChessBoardFactory.createStoredBoard(board);
     }
 
     @Override
     public void moveSourcePieceToTargetPoint(String source, String target, int roomId) {
-        pieceDao.movePiece(source, target, roomId);
+        Map<String, String> pieceInformation = piece.get(roomId);
+        Object pieceName = pieceInformation.remove(source);
+        pieceInformation.put(target, pieceName.toString());
     }
 
     @Override
     public void changeTurn(String nextTurn, String currentTurn, int roomId) {
-        roomDao.changeTurn(nextTurn, currentTurn, roomId);
+        Map<String, String> roomInformation = room.get(roomId);
+        roomInformation.replace((String)roomInformation.keySet().toArray()[0], nextTurn);
     }
 
     @Override
     public String getCurrentTurnByRoomId(int roomId) {
-        return roomDao.selectTurnByRoomId(roomId);
+        Map<String, String> roomInformation = room.get(roomId);
+        return (String) roomInformation.values().toArray()[0];
     }
 
     @Override
     public void removeTargetPiece(String target, int roomId) {
-        pieceDao.removePiece(target, roomId);
+        piece.get(roomId).remove(target);
     }
 
     @Override
     public Map<Integer, String> getRoomNames() {
-        List<Map<String, Object>> resultList = roomDao.selectAllRoomNames();
-        Map<Integer, String> rooms = new HashMap<>();
-        for (Map<String, Object> result : resultList) {
-            int roomId = (int) result.get("room_id");
-            String roomName = (String) result.get("room_name");
-            rooms.put(roomId, roomName);
+        Map<Integer, String> roomNames = new HashMap<>();
+        for (Map.Entry<Integer, Map<String, String>> room : room.entrySet()) {
+            roomNames.put(room.getKey(), (String) room.getValue().keySet().toArray()[0]);
         }
-        return rooms;
+        return roomNames;
     }
 
     @Override
     public int getRoomId(String roomName) {
-        return roomDao.selectRoomId(roomName);
+        Optional<Map.Entry<Integer, Map<String, String>>> first = room.entrySet().stream()
+                .filter(entry -> entry.getValue().containsKey(roomName))
+                .findFirst();
+        return first.get().getKey();
     }
 }
--- src/test/java/chess/service/InMemoryChessServiceTest.java
+++ src/test/java/chess/service/InMemoryChessServiceTest.java
@@ -13,52 +13,32 @@ import chess.repository.InMemoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource("classpath:application-test.properties")
-@Sql("classpath:schema.sql")
-@JdbcTest
 public class InMemoryChessServiceTest {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
     private ChessService chessService;
 
     @BeforeEach
     void setUp() {
-        ChessRepository inMemoryRepository = new InMemoryRepository(
-                new RoomDao(this.jdbcTemplate, this.namedParameterJdbcTemplate),
-                new PieceDao(this.jdbcTemplate, this.namedParameterJdbcTemplate)
-        );
+        ChessRepository inMemoryRepository = new InMemoryRepository();
         chessService = new ChessService(inMemoryRepository);
     }
 
     @DisplayName("방 생성 테스트")
     @Test
     void makeRoomTest() {
-        assertEquals(4, chessService.makeRoom("room4"));
+        assertEquals(1, chessService.makeRoom("room1"));
     }
 
     @DisplayName("방 id를 통해 게임을 가져오는 테스트")
     @Test
     void getStoredRoundTest() {
-        chessService.makeRoom("room4");
-        Round round = chessService.getStoredRound(4);
+        chessService.makeRoom("room1");
+        Round round = chessService.getStoredRound(1);
         assertEquals(81, round.getBoard().size());
         assertEquals("white", round.getCurrentTurn());
         assertEquals(38, round.getPlayerScore("white"));
@@ -68,9 +48,9 @@ public class InMemoryChessServiceTest {
     @DisplayName("기물의 움직임 테스트")
     @Test
     void movePieceTest() {
-        chessService.makeRoom("room4");
-        chessService.movePiece("e2", "e4", 4);
-        Map<Position, Piece> board = chessService.getStoredRound(4).getBoard();
+        chessService.makeRoom("room1");
+        chessService.movePiece("e2", "e4", 1);
+        Map<Position, Piece> board = chessService.getStoredRound(1).getBoard();
         assertNull(board.get(Position.valueOf("2", "e")));
         assertEquals("p", board.get(Position.valueOf("4", "e")).getPiece());
     }
@@ -78,14 +58,17 @@ public class InMemoryChessServiceTest {
     @DisplayName("차례 바꾸기 테스트")
     @Test
     void changeTurnTest() {
-        chessService.makeRoom("room4");
-        chessService.changeTurn("black", "white", 4);
-        assertEquals("black", chessService.getStoredRound(4).getCurrentTurn());
+        chessService.makeRoom("room1");
+        chessService.changeTurn("black", "white", 1);
+        assertEquals("black", chessService.getStoredRound(1).getCurrentTurn());
     }
 
     @DisplayName("방 이름 목록 가져오기 테스트")
     @Test
     void getRoomNamesTest() {
+        chessService.makeRoom("room1");
+        chessService.makeRoom("room2");
+        chessService.makeRoom("room3");
         Map<Integer, String> roomNames = chessService.getRoomNames();
         assertEquals(3, roomNames.size());
         assertEquals("room1", roomNames.get(1));
@@ -94,10 +77,7 @@ public class InMemoryChessServiceTest {
     @DisplayName("방 이름으로 id 가져오기 테스트")
     @Test
     void getRoomIdTest() {
-        assertEquals(2, chessService.getRoomId("room2"));
+        chessService.makeRoom("room1");
+        assertEquals(1, chessService.getRoomId("room1"));
     }
-
-
-
-
 }

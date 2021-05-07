94fe353f9b6101f9d67023da876532c6dfd0e08b
Gomding <backchida@gmail.com>
2021-05-07 16:19:50 +0900

    feat(User.java) : User 엔티티 객체 구현

--- src/main/java/chess/domain/user/User.java
+++ src/main/java/chess/domain/user/User.java
@@ -1,4 +1,38 @@
 package chess.domain.user;
 
-public class User {
+import chess.domain.Entity;
+
+import java.util.Objects;
+
+public class User extends Entity<Long> {
+
+    private final String username;
+    private final String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    private void validateUser(String username, String password) {
+        validateNull(username, password);
+    }
+
+    private void validateNull(String username, String password) {
+        Objects.requireNonNull(username, "유저 이름은 null일 수 없습니다.");
+        Objects.requireNonNull(password, "유저 비밀번호는 null일 수 없습니다.");
+    }
+
+    private void validateEmpty(String username, String password) {
+        if (username.isEmpty()) {
+            throw new IllegalArgumentException("유저 이름은 공백일 수 없습니다.");
+        }
+        if (password.isEmpty()) {
+            throw new IllegalArgumentException("유저 비밀번호는 공백일 수 없습니다.");
+        }
+    }
+
+    public boolean checkPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
 }

b4f56f8fff30865a104539885e88d5b13cc9cb5b
Gomding <backchida@gmail.com>
2021-05-10 00:32:31 +0900

    refactor(Room.java) : 블랙 플레이어가 참가하지않아서 대기중인 방인지, 블랙 플레이어가 들어왔는지 확인하는 메서드 추가

--- src/main/java/chess/domain/room/Room.java
+++ src/main/java/chess/domain/room/Room.java
@@ -6,41 +6,44 @@ import java.util.Objects;
 
 public class Room extends Entity<Long> {
 
-    private final String roomName;
+    private final Long gameId;
+    private final String name;
     private final Long whiteUserId;
     private Long blackUserId;
 
-    public Room(String roomName, Long whiteUserId) {
-        this(roomName, whiteUserId, null);
+    public Room(Long gameId, String name, Long whiteUserId) {
+        this(null, gameId, name, whiteUserId, null);
     }
 
-    public Room(String roomName, Long whiteUserId, Long blackUserId) {
-        this.roomName = roomName;
+    public Room(Long id, Long gameId, String name, Long whiteUserId) {
+        this(id, gameId, name, whiteUserId, null);
+    }
+
+    public Room(Long gameId, String name, Long whiteUserId, Long blackUserId) {
+        this(null, gameId, name, whiteUserId, blackUserId);
+    }
+
+    public Room(Long id, Long gameId, String name, Long whiteUserId, Long blackUserId) {
+        super(id);
+        this.gameId = gameId;
+        this.name = name;
         this.whiteUserId = whiteUserId;
         this.blackUserId = blackUserId;
+        validateRoom(name, whiteUserId);
     }
 
-    public static Room of(final String roomName, final Long whiteUserId) {
-        validateRoom(roomName, whiteUserId);
-        return new Room(roomName, whiteUserId);
+    private static void validateRoom(final String name, final Long whiteUserId) {
+        validateNull(name, whiteUserId);
+        validateEmpty(name);
     }
 
-    public static Room of(final String roomName, final Long whiteUserId, final Long blackUserId) {
-        return new Room(roomName, whiteUserId, blackUserId);
-    }
-
-    private static void validateRoom(final String roomName, final Long whiteUserId) {
-        validateNull(roomName, whiteUserId);
-        validateEmpty(roomName, whiteUserId);
-    }
-
-    private static void validateNull(final String roomName, final Long whiteUserId) {
-        Objects.requireNonNull(roomName, "방 생성시 방 이름은 null 일 수 없습니다.");
+    private static void validateNull(final String name, final Long whiteUserId) {
+        Objects.requireNonNull(name, "방 생성시 방 이름은 null 일 수 없습니다.");
         Objects.requireNonNull(whiteUserId, "방 생성시 White User 는 null이 될 수 없습니다.");
     }
 
-    private static void validateEmpty(final String roomName, final Long whiteUserId) {
-        if (roomName.isEmpty()) {
+    private static void validateEmpty(final String name) {
+        if (name.isEmpty()) {
             throw new IllegalArgumentException("방 이름은 1글자 이상 작성해야합니다.");
         }
     }
@@ -51,11 +54,23 @@ public class Room extends Entity<Long> {
     }
 
     private void validateBlackUserId(Long blackUserId) {
-        Objects.requireNonNull(blackUserId, "Black User 입장시 ID는 null일 수 없습니다.");
+        Objects.requireNonNull(blackUserId, "Black User ID는 null일 수 없습니다.");
     }
 
-    public String roomName() {
-        return this.roomName;
+    public boolean isWaitingRoom() {
+        return blackUserId == null || blackUserId.equals(0L);
+    }
+
+    public boolean haveBlackUser() {
+        return blackUserId != null && blackUserId > 0L;
+    }
+
+    public Long gameId() {
+        return this.gameId;
+    }
+
+    public String name() {
+        return this.name;
     }
 
     public Long whiteUserId() {

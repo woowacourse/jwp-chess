f4581d56b1548bec48af5e48e72830bfe50ea6ef
Gomding <backchida@gmail.com>
2021-05-10 00:29:21 +0900

    refactor(schema.sql) : 데이터베이스 테이블 변경

--- src/main/resources/db/h2/schema.sql
+++ src/main/resources/db/h2/schema.sql
@@ -1,43 +1,41 @@
 DROP TABLE room IF EXISTS;
 DROP TABLE user IF EXISTS;
 DROP TABLE game IF EXISTS;
-DROP TABLE piece IF EXISTS;
+DROP TABLE square IF EXISTS;
 DROP TABLE history IF EXISTS;
 
 -- -----------------------------------------------------
--- Table `game`
+-- Table `room`
 -- -----------------------------------------------------
 CREATE TABLE room
 (
-    id             INTEGER IDENTITY PRIMARY KEY,
-    game_id        INTEGER,
-    room_name      VARCHAR(100) NOT NULL,
-    white_user_id   INTEGER NOT NULL,
-    black_user_id   INTEGER
+    id            INTEGER IDENTITY PRIMARY KEY,
+    game_id       INTEGER,
+    name     VARCHAR(10) NOT NULL,
+    white_user_id INTEGER      NOT NULL,
+    black_user_id INTEGER
 );
 CREATE
-INDEX game_id ON game (id);
-ALTER TABLE room
-    ADD CONSTRAINT fk_game_room FOREIGN KEY (game_id) REFERENCES game (id);
+    INDEX room_id ON room (id);
 
 -- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 CREATE TABLE user
 (
-    id             INTEGER IDENTITY PRIMARY KEY,
-    username VARCHAR(45)  NOT NULL,
-    password VARCHAR(15) NOT NULL,
+    id       INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    password VARCHAR(15) NOT NULL
 );
 CREATE
-INDEX user_id ON user (id);
+    INDEX user_id ON user (id);
 
 -- -----------------------------------------------------
 -- Table `game`
 -- -----------------------------------------------------
 CREATE TABLE game
 (
-    id           INTEGER IDENTITY PRIMARY KEY,
+    id       INTEGER IDENTITY PRIMARY KEY,
     turn_owner  VARCHAR(45) NOT NULL,
     turn_number INTEGER     NOT NULL,
     playing     BIT         NOT NULL,
@@ -45,14 +43,31 @@ CREATE TABLE game
     black_score DOUBLE  NOT NULL
 );
 CREATE
-INDEX history_id ON history (id);
+    INDEX game_id ON game (id);
+
+-- -----------------------------------------------------
+-- Table `history`
+-- -----------------------------------------------------
+CREATE TABLE history
+(
+    id          INTEGER IDENTITY PRIMARY KEY,
+    game_id     INTEGER     NOT NULL,
+    source      VARCHAR(45) NOT NULL,
+    target      VARCHAR(45) NOT NULL,
+    turn_owner  VARCHAR(45) NOT NULL,
+    turn_number VARCHAR(45) NOT NULL,
+    playing     BIT         NOT NULL
+);
+CREATE
+    INDEX history_id ON history (id);
 ALTER TABLE history
     ADD CONSTRAINT fk_game_history FOREIGN KEY (game_id) REFERENCES game (id);
 
+
 -- -----------------------------------------------------
--- Table `piece`
+-- Table `square`
 -- -----------------------------------------------------
-CREATE TABLE piece
+CREATE TABLE square
 (
     id       INTEGER IDENTITY PRIMARY KEY,
     game_id  INTEGER     NOT NULL,
@@ -60,24 +75,6 @@ CREATE TABLE piece
     symbol   VARCHAR(45) NOT NULL
 );
 CREATE
-INDEX piece_id ON piece (id);
-ALTER TABLE piece
-    ADD CONSTRAINT fk_game_piece FOREIGN KEY (game_id) REFERENCES game (id);
-
-
--- -----------------------------------------------------
--- Table `history`
--- -----------------------------------------------------
-CREATE TABLE history
-(
-    id           INTEGER IDENTITY PRIMARY KEY,
-    game_id      INTEGER     NOT NULL,
-    move_command VARCHAR(45) NOT NULL,
-    turn_owner   VARCHAR(45) NOT NULL,
-    turn_number  VARCHAR(45) NOT NULL,
-    playing      BIT         NOT NULL
-);
-CREATE
-INDEX history_id ON history (id);
-ALTER TABLE history
-    ADD CONSTRAINT fk_game_history FOREIGN KEY (game_id) REFERENCES game (id);
+    INDEX square_id ON square (id);
+ALTER TABLE square
+    ADD CONSTRAINT fk_game_square FOREIGN KEY (game_id) REFERENCES game (id);

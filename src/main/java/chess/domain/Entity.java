0cebd460f881da786bc737231e155f46b2d4818b
Gomding <backchida@gmail.com>
2021-05-10 00:35:28 +0900

    refactor(Entity.java) : 생성자 추가

--- src/main/java/chess/domain/Entity.java
+++ src/main/java/chess/domain/Entity.java
@@ -3,6 +3,13 @@ package chess.domain;
 public class Entity<E> {
     protected E id;
 
+    public Entity() {
+    }
+
+    public Entity(E id) {
+        this.id = id;
+    }
+
     public E getId() {
         return this.id;
     };

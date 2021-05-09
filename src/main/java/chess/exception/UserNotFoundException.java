593a264bfa4b6e3c2ba8849ebd6be13cd780cb40
Gomding <backchida@gmail.com>
2021-05-10 00:38:48 +0900

    feat(Exception) : 커스텀 예외 추가

---
+++ src/main/java/chess/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package chess.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

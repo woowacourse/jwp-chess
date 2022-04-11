558247d10115c38d9399fd8002bd7c0c7ed404ef
brorae <jhr3239@gmail.com>
2022-04-12 03:49:09 +0900

    chore: 4,5단계 체스 미션 받아오기

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -1,22 +1,21 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.staticFileLocation;
 
-import java.util.HashMap;
-import java.util.Map;
+import chess.controller.WebController;
 
-import static spark.Spark.get;
 
 public class SparkChessApplication {
-    public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.hbs");
-        });
-    }
+//    public static void main(String[] args) {
+//        get("/", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            return render(model, "index.hbs");
+//        });
+//    }
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    public static void main(String[] args) {
+        staticFileLocation("/static");
+        WebController webController = new WebController();
+        webController.run();
     }
 }

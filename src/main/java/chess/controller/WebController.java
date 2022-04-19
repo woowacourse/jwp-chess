2928ee93c4cf1bdf16a687d0767747adc6e43947
Ohzzi <clover7even@naver.com>
2022-04-19 20:52:00 +0900

    feat: 게임 방 번호를 입력하면 게임으로 입장되도록 URL 매핑 구현

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -2,6 +2,8 @@ package chess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebController {
@@ -9,5 +11,15 @@ public class WebController {
     @GetMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @GetMapping("/game")
+    public RedirectView redirectGame(@RequestParam("gameId") String gameId) {
+        return new RedirectView("/game/" + gameId);
+    }
+
+    @GetMapping("/game/{gameId}")
+    public String joinGame() {
+        return "/game.html";
     }
 }

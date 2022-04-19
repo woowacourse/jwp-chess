59005cfb794f76fedc682192946bf5947c3cf4b2
giron <ggyucheol0@gmail.com>
2022-04-19 14:56:22 +0900

    feat: 체스 초기 상태 설정

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -1,22 +1,20 @@
 package chess;
 
+import chess.web.controller.ChessWebController;
+import chess.web.service.ChessService;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class SparkChessApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.hbs");
-        });
-    }
-
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+        port(8080);
+        staticFileLocation("static");
+        ChessWebController chessController = new ChessWebController(new ChessService());
+        chessController.run();
     }
 }

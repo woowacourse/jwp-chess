222e608f0a6a626a5138efd60790e3e28287bd97
Kim Seunglae <obey1342@gmail.com>
2022-04-20 15:48:05 +0900

    feat: ExceptionHandler 구현

--- src/main/java/chess/web/controller/ChessController.java
+++ src/main/java/chess/web/controller/ChessController.java
@@ -5,14 +5,11 @@ import chess.service.dto.BoardDto;
 import chess.service.dto.GameResultDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
 
 @Controller
 public class ChessController {
@@ -77,5 +74,11 @@ public class ChessController {
     public String requestEndGame(@PathVariable int gameId) {
         chessService.endGame(gameId);
         return "redirect:../";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleError(Exception ex, Model model) {
+        model.addAttribute("exception", ex.getMessage());
+        return "exception";
     }
 }

a79cb82bafa21444cd46d711d40cceb6c892f209
woong7 <driedseas@gmail.com>
2022-04-20 21:34:03 +0900

    feat: move 버튼을 클릭하면 기물을 이동하는 기능

--- src/main/java/chess/IngameController.java
+++ src/main/java/chess/IngameController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class IngameController {
     private final DBService DBService;
@@ -25,5 +28,31 @@ public class IngameController {
         model.addAttribute("msg", "누가 이기나 보자구~!");
         model.addAttribute("gameID", gameID);
         return "ingame";
+    }
+
+    @PostMapping("/ingame/{gameID}")
+    public String movePiece(@PathVariable String gameID, @RequestBody String movement, Model model) {
+        ChessGame chessGame = DBService.loadSavedChessGame(gameID, DBService.getTurn(gameID));
+        List<String> movements = Arrays.asList(movement.split("&"));
+
+        String source = getPosition(movements.get(0));
+        String target = getPosition(movements.get(1));
+
+        try {
+            chessGame.move(new Square(source), new Square(target));
+            DBService.movePiece(gameID, source, target);
+            DBService.updateTurn(gameID, chessGame);
+            return runGame(gameID, model);
+        } catch (IllegalArgumentException e) {
+            model.addAllAttributes(chessGame.getEmojis());
+            model.addAttribute("msg", e.getMessage());
+        }
+        model.addAttribute("gameID", gameID);
+
+        return "ingame";
+    }
+
+    private String getPosition(String input) {
+        return input.split("=")[1];
     }
 }
--- src/test/java/chess/IngameControllerTest.java
+++ src/test/java/chess/IngameControllerTest.java
@@ -7,18 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.Is.is;
 
 @SpringBootTest
 class IngameControllerTest {
 
+    @DisplayName("gameID에 따라 게임을 로드한다.")
     @Test
-    void contextLoads() {
-    }
-
-    @DisplayName("Parameter Header - Params")
-    @Test
-    void messageForParam() {
+    void loadGameByGameID() {
         RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/ingame?gameID=panda")
@@ -27,4 +24,16 @@ class IngameControllerTest {
                 .body(is("panda"));
     }
 
+    @DisplayName("move를 클릭하면 기물을 이동한다.")
+    @Test
+    void movePiece() {
+        String movement = "source=a2&target=a4";
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(movement)
+                .when().post("/ingame/panda")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body(containsString("/ingame/panda"));
+    }
 }

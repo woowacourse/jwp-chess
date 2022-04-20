9a38917c54415b3c8a0482aec5e081185327f70a
Ohzzi <clover7even@naver.com>
2022-04-20 20:52:21 +0900

    refactor: GameDao가 JdbcTemplate을 사용하도록 변경

--- src/test/java/chess/dao/PieceDaoTest.java
+++ src/test/java/chess/dao/PieceDaoTest.java
@@ -1,5 +1,6 @@
 package chess.dao;
 
+<<<<<<< HEAD
 import static chess.domain.CachedPosition.a2;
 import static chess.domain.CachedPosition.a3;
 import static chess.domain.CachedPosition.b2;
@@ -99,4 +100,86 @@ public class PieceDaoTest {
 
         assertThat(pieces).isEmpty();
     }
+=======
+public class PieceDaoTest {
+
+//    private static final long testGameId = 1;
+//
+//    private final GameDao gameDao = new GameDaoImpl(new TestConnectionSetup());
+//    private final PieceDao pieceDao = new PieceDaoImpl(new TestConnectionSetup());
+//
+//    @BeforeEach
+//    void createGame() {
+//        gameDao.save(testGameId);
+//    }
+//
+//    @AfterEach
+//    void clear() {
+//        gameDao.delete(testGameId);
+//    }
+//
+//    @DisplayName("기물 저장 테스트")
+//    @Test
+//    void save() {
+//        pieceDao.save(testGameId, a2, new Pawn(Color.WHITE));
+//    }
+//
+//    @DisplayName("게임의 전체 기물 조회 테스트")
+//    @Test
+//    void findAll() {
+//        pieceDao.save(testGameId, a2, new Pawn(Color.WHITE));
+//        pieceDao.save(testGameId, b2, new Pawn(Color.WHITE));
+//
+//        List<PieceResponse> pieces = pieceDao.findAll(testGameId);
+//
+//        assertThat(pieces.size()).isEqualTo(2);
+//    }
+//
+//    @DisplayName("위치에 맞는 기물 조회 테스트")
+//    @Test
+//    void find() {
+//        pieceDao.save(testGameId, a2, new Pawn(Color.WHITE));
+//
+//        Optional<Piece> maybePiece = pieceDao.find(testGameId, a2);
+//        Piece actual = maybePiece.orElseThrow(() -> new AssertionFailedException("해당하는 기물이 없습니다."));
+//
+//        assertThat(actual).isEqualTo(new Pawn(Color.WHITE));
+//    }
+//
+//    @DisplayName("기물의 위치 변경 테스트")
+//    @Test
+//    void update() {
+//        pieceDao.save(testGameId, a2, new Pawn(Color.WHITE));
+//
+//        pieceDao.updatePosition(testGameId, a2, a3);
+//        Optional<Piece> shouldEmpty = pieceDao.find(testGameId, a2);
+//        Piece actual = pieceDao.find(testGameId, a3).orElseThrow(() -> new AssertionFailedException("해당하는 기물이 없습니다."));
+//
+//        Assertions.assertAll(
+//                () -> assertThat(shouldEmpty.isPresent()).isFalse(),
+//                () -> assertThat(actual).isEqualTo(new Pawn(Color.WHITE))
+//        );
+//    }
+//
+//    @DisplayName("위치에 맞는 기물 삭제 테스트")
+//    @Test
+//    void delete() {
+//        pieceDao.save(testGameId, a2, new Pawn(Color.WHITE));
+//
+//        pieceDao.delete(testGameId, a2);
+//        Optional<Piece> maybePiece = pieceDao.find(1, a2);
+//
+//        assertThat(maybePiece.isPresent()).isFalse();
+//    }
+//
+//    @DisplayName("게임의 전체 기물 삭제 테스트")
+//    @Test
+//    void deleteAll() {
+//        pieceDao.deleteAll(testGameId);
+//
+//        List<PieceResponse> pieces = pieceDao.findAll(1);
+//
+//        assertThat(pieces).isEmpty();
+//    }
+>>>>>>> refactor: GameDao가 JdbcTemplate을 사용하도록 변경
 }
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 CREATE TABLE game (
       id INT AUTO_INCREMENT NOT NULL,
       name VARCHAR(20) NOT NULL,
@@ -14,4 +15,21 @@ CREATE TABLE piece (
        game_id INT NOT NULL,
        PRIMARY KEY (id),
        FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
+=======
+CREATE TABLE game
+(
+    game_id int         NOT NULL PRIMARY KEY,
+    state   varchar(20) NOT NULL
+);
+
+CREATE TABLE piece
+(
+    piece_id   int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id    INT         NOT NULL,
+    position   VARCHAR(10) NOT NULL,
+    piece_type VARCHAR(10) NOT NULL,
+    color      VARCHAR(10) NOT NULL,
+    FOREIGN KEY (game_id)
+        REFERENCES game (game_id) ON DELETE CASCADE
+>>>>>>> refactor: GameDao가 JdbcTemplate을 사용하도록 변경
 );

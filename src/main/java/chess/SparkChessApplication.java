0092b4c581c4de098407ba812e8fcfdd57c44d17
Hongdonggeon <ghd700@naver.com>
2022-04-21 19:27:15 +0900

    refactor: Spark Application 로직 이동

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -1,22 +1,17 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.get;
+import chess.controller.ChessWebController;
+import chess.dao.JdbcBoardDao;
+import chess.dao.JdbcGameStatusDao;
+import chess.dao.JdbcTurnDao;
+import chess.domain.ChessGameService;
+import chess.domain.board.strategy.WebBasicBoardStrategy;
 
 public class SparkChessApplication {
-    public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.hbs");
-        });
-    }
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private static final ChessWebController controller = new ChessWebController(new ChessGameService(new JdbcBoardDao(), new JdbcTurnDao(), new JdbcGameStatusDao()));
+
+    public static void main(String[] args) {
+        controller.start(new WebBasicBoardStrategy());
     }
 }

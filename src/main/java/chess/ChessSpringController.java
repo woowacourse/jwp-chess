1d97d6a4c3c6607d32794a1ce7c065eff8e911a4
BeomsooKim <kbsat@naver.com>
2022-04-21 21:31:29 +0900

    feat: 스프링 체스 방 생성 및 입장 구현 (/room)

--- src/main/java/chess/ChessSpringController.java
+++ src/main/java/chess/ChessSpringController.java
@@ -1,7 +1,10 @@
 package chess;
 
+import chess.service.ChessService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChessSpringController {
@@ -9,5 +12,14 @@ public class ChessSpringController {
     @GetMapping("/")
     public String index() {
         return "roby";
+    }
+
+    @GetMapping("/room")
+    public String room(@RequestParam(value = "name") String name,
+                       Model model) {
+        ChessService chessService = new ChessService();
+        chessService.createRoom(name);
+        model.addAttribute("name", name);
+        return "room";
     }
 }

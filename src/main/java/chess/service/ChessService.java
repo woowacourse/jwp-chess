97ed4f4baddb45f7e153aad99d13b2ed4bac615b
is2js <tingstyle1@gmail.com>
2022-04-21 17:14:44 +0900

    refactor : 방 생성 구현 변경

--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -1,10 +1,17 @@
 package chess.service;
 
+import chess.domain.board.BoardFactory;
+import chess.domain.board.Position;
+import chess.domain.piece.Piece;
 import chess.dto.request.RoomRequestDto;
 import chess.dto.response.RoomResponseDto;
+import chess.entity.BoardEntity;
 import chess.entity.RoomEntity;
 import chess.repository.BoardRepository;
 import chess.repository.RoomRepository;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +28,16 @@ public class ChessService {
     public RoomResponseDto createRoom(final RoomRequestDto roomRequestDto) {
         final RoomEntity room = new RoomEntity(roomRequestDto.getName(), "white", false);
         final RoomEntity createdRoom = roomRepository.insert(room);
+        boardRepository.batchInsert(createBoards(createdRoom));
         return RoomResponseDto.of(createdRoom);
+    }
+
+    private List<BoardEntity> createBoards(final RoomEntity createdRoom) {
+        final Map<Position, Piece> board = BoardFactory.initialize();
+        return board.entrySet().stream()
+            .map(entry -> new BoardEntity(createdRoom.getId(),
+                entry.getKey().convertPositionToString(),
+                entry.getValue().convertPieceToString()))
+            .collect(Collectors.toList());
     }
 }
--- src/test/java/chess/service/ChessServiceTest.java
+++ src/test/java/chess/service/ChessServiceTest.java
@@ -3,6 +3,8 @@ package chess.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.dto.request.RoomRequestDto;
+import chess.dto.response.RoomResponseDto;
+import chess.repository.BoardRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +20,24 @@ class ChessServiceTest {
     @Autowired
     private ChessService chessService;
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @DisplayName("체스 초보만이라는 이름을 가진 방을 생성한다.")
     @Test
     void createRoom() {
         final RoomRequestDto roomRequestDto = new RoomRequestDto("체스 초보만");
-        final String name = chessService.createRoom(roomRequestDto).getName();
+        final RoomResponseDto room = chessService.createRoom(roomRequestDto);
 
-        assertThat(name).isEqualTo("체스 초보만");
+        assertThat(room.getName()).isEqualTo("체스 초보만");
+        assertThat(boardRepository.findBoardByRoomId(room.getId())).hasSize(64);
+    }
+
+    @DisplayName("")
+    @Test
+    void enterRoom() {
+        final RoomRequestDto roomRequestDto = new RoomRequestDto("체스 초보만");
+        final Long id = chessService.createRoom(roomRequestDto).getId();
+
     }
 }

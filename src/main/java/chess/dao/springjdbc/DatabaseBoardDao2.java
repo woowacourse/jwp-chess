c3749b43e92893f10d87641cfc97bc51e59ffc28
yangdongjue5510 <djwhy5510@gmail.com>
2022-04-22 16:56:08 +0900

    refactor: 보드 db 조회 및 보드 db 초기화 수정

--- src/main/java/chess/dao/springjdbc/DatabaseBoardDao2.java
+++ src/main/java/chess/dao/springjdbc/DatabaseBoardDao2.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,35 +20,28 @@ public class DatabaseBoardDao2 implements BoardDao {
     private static final String SQUARE = "square";
 
     private JdbcTemplate jdbcTemplate;
-
+    private final RowMapper<PieceWithSquareDto> pieceRowMapper = (resultSet, rowNum) ->
+            new PieceWithSquareDto(
+                    resultSet.getString("square"),
+                    resultSet.getString("piece_type"),
+                    resultSet.getString("piece_color")
+            );
     public DatabaseBoardDao2(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void initBoard(int gameId) {
-        String sql = "insert into board (game_id, piece_type, piece_color, square)\n"
-                + "select ?, init.piece_type, init.piece_color, init.square from init_board as init\n"
-                + "on duplicate key update piece_type = init.piece_type, piece_color = init.piece_color";
-        new StatementExecutor(JdbcUtil.getConnection(), sql)
-                .setInt(gameId)
-                .executeUpdate();
+        String sql = "INSERT INTO board (game_id, piece_type, piece_color, square)\n"
+                + "SELECT ?, init.piece_type, init.piece_color, init.square FROM init_board AS init\n"
+                + "ON duplicate KEY UPDATE piece_type = init.piece_type, piece_color = init.piece_color";
+        jdbcTemplate.update(sql, gameId);
     }
 
     @Override
     public BoardDto getBoardByGameId(int id) {
         String sql = "select piece_type, piece_color, square from board where game_id = ?";
-        List<PieceWithSquareDto> pieces = new StatementExecutor(JdbcUtil.getConnection(), sql)
-                .setInt(id)
-                .findAll(this::getPieceDto);
-        return new BoardDto(pieces);
-    }
-
-    private PieceWithSquareDto getPieceDto(ResultSet resultSet) throws SQLException {
-        String color = resultSet.getString(PIECE_COLOR);
-        String type = resultSet.getString(PIECE_TYPE);
-        String square = resultSet.getString(SQUARE);
-        return new PieceWithSquareDto(square, type, color);
+        return new BoardDto(jdbcTemplate.query(sql, pieceRowMapper, id));
     }
 
     @Override

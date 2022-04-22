5094d788576369ce9713d2d54c64ffaaa8a47c13
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-22 20:29:07 +0900

    feat: 체스 게임 생성 쿼리 추가

---
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -0,0 +1,31 @@
+package chess.dao;
+
+import chess.domain.state.Turn;
+import java.sql.PreparedStatement;
+import java.util.Objects;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ChessGameDao {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public ChessGameDao(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public long createChessGame(Turn turn) {
+		String sql = "insert into chess_game (turn) values (?)";
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(con -> {
+					PreparedStatement statement = con.prepareStatement(sql, new String[]{"id"});
+					statement.setString(1, turn.name());
+					return statement;
+				},
+				keyHolder);
+		return Objects.requireNonNull(keyHolder.getKey()).longValue();
+	}
+}
---
+++ src/test/java/chess/dao/ChessGameDaoTest.java
@@ -0,0 +1,33 @@
+package chess.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.state.Turn;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class ChessGameDaoTest {
+
+	private ChessGameDao chessGameDao;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@BeforeEach
+	void setup() {
+		chessGameDao = new ChessGameDao(jdbcTemplate);
+	}
+
+	@Test
+	@DisplayName("체스 게임 생성")
+	void createChessGame() {
+		Turn turn = Turn.WHITE_TURN;
+
+		assertThat(chessGameDao.createChessGame(turn)).isEqualTo(1L);
+	}
+}

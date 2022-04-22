e866198c7e634c1fec802aa1d7a1e56c035374f8
cjlee38 <cjlee38@github.com>
2022-04-22 17:42:25 +0900

    chore: gitignore 수정(sql 파일 추가)

---
+++ docker/db/mysql/init/chess.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TABLE game (
+    room_name VARCHAR(10) NOT NULL,
+    turn_color VARCHAR(10) NOT NULL,
+    state VARCHAR(10) NOT NULL,
+    PRIMARY KEY (room_name)
+);
+
+CREATE TABLE board (
+    piece_type VARCHAR(10) NOT NULL,
+    piece_color VARCHAR(10) NOT NULL,
+    vertical_index INT NOT NULL,
+    horizontal_index INT NOT NULL,
+    room_name VARCHAR(10) NOT NULL,
+    FOREIGN KEY(room_name) REFERENCES game (room_name)
+ );

cde917f80434b4c12650cc4c1318a92a51e594be
awesomeo184 <awesomeo184@gmail.com>
2022-04-22 23:07:42 +0900

    feat: ChessGameDAO에 JdbcTemplate 적용

--- src/main/java/chess/dao/ChessGameDAO.java
+++ src/main/java/chess/dao/ChessGameDAO.java
@@ -1,78 +1,66 @@
 package chess.dao;
 
-import chess.view.OutputView;
-import chess.utils.DBConnectionUtils;
 import chess.dto.ChessGameRoomInfoDTO;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
-public final class ChessGameDAO {
+@Repository
+public class ChessGameDAO {
 
-    public String addGame(final ChessGame chessGame) throws SQLException {
-        String query = "INSERT INTO CHESS_GAME (name) VALUES (?)";
-        Connection connection = DBConnectionUtils.getConnection();
-        String gameId = null;
-        try {
-            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, chessGame.getName());
-            statement.executeUpdate();
-            ResultSet rs = statement.getGeneratedKeys();
-            rs.next();
-            gameId = rs.getString(1);
-            DBConnectionUtils.closeConnection(connection);
-        } catch (SQLException e) {
-            OutputView.printErrorMessage(e.getMessage());
-        }
-        return gameId;
+    private final JdbcTemplate jdbcTemplate;
+
+    public ChessGameDAO(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<ChessGameRoomInfoDTO> findActiveGames() throws SQLException {
-        String query = "SELECT * FROM CHESS_GAME WHERE IS_END = false";
-        Connection connection = DBConnectionUtils.getConnection();
-        List<ChessGameRoomInfoDTO> chessGameDTOs = new ArrayList<>();
-        try {
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                ChessGameRoomInfoDTO chessGameDTO = new ChessGameRoomInfoDTO(rs.getString("id"), rs.getString("name"));
-                chessGameDTOs.add(chessGameDTO);
-            }
-            DBConnectionUtils.closeConnection(connection);
-        } catch (SQLException e) {
-            OutputView.printErrorMessage(e.getMessage());
-        }
-        return chessGameDTOs;
+    public String addGame(final ChessGame chessGame) {
+        String sql = "INSERT INTO CHESS_GAME (name) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, new String[]{"id"});
+            statement.setString(1, chessGame.getName());
+            statement.executeUpdate();
+            return statement;
+        }, keyHolder);
+        return String.valueOf(keyHolder.getKey().longValue());
+    }
+
+    public List<ChessGameRoomInfoDTO> findActiveGames() {
+        String sql = "SELECT id, name FROM CHESS_GAME WHERE IS_END = false";
+        List<ChessGameRoomInfoDTO> chessGameRoomInfoDTOs = jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> {
+                    ChessGameRoomInfoDTO chessGameRoomInfoDTO = new ChessGameRoomInfoDTO(
+                            rs.getString("id"),
+                            rs.getString("name")
+                    );
+                    return chessGameRoomInfoDTO;
+                }
+        );
+        return chessGameRoomInfoDTOs;
     }
 
     public ChessGameRoomInfoDTO findGameById(final String gameId) {
-        String query = "SELECT * FROM CHESS_GAME WHERE ID = ? AND IS_END = FALSE ORDER BY created_at";
-        Connection connection = DBConnectionUtils.getConnection();
+        String sql = "SELECT id, name FROM CHESS_GAME WHERE ID = ? AND IS_END = FALSE ORDER BY created_at";
 
-        try {
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, gameId);
-            ResultSet rs = statement.executeQuery();
-            rs.next();
-            return new ChessGameRoomInfoDTO(rs.getString("id"), rs.getString("name"));
-        } catch (SQLException e) {
-            OutputView.printErrorMessage(e.getMessage());
-        }
-        return null;
+        return jdbcTemplate.queryForObject(
+                sql,
+                (rs, rowNum) -> {
+                    ChessGameRoomInfoDTO chessGameRoomInfoDTO = new ChessGameRoomInfoDTO(
+                            rs.getString("id"),
+                            rs.getString("name")
+                    );
+                    return chessGameRoomInfoDTO;
+                }, gameId);
     }
 
-    public void updateGameEnd(final String gameId) throws SQLException {
-        String query = "UPDATE chess_game SET is_end = true WHERE id = ?";
-        final Connection connection = DBConnectionUtils.getConnection();
-        PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1, gameId);
-
-        try (connection; statement) {
-            statement.executeUpdate();
-        }
+    public void updateGameEnd(final String gameId)  {
+        String sql = "UPDATE chess_game SET is_end = true WHERE id = ?";
+        jdbcTemplate.update(sql, gameId);
     }
 }
--- src/test/java/chess/dao/ChessGameDAOTest.java
+++ src/test/java/chess/dao/ChessGameDAOTest.java
@@ -1,18 +1,24 @@
 package chess.dao;
 
 import chess.dto.ChessGameRoomInfoDTO;
-import java.sql.SQLException;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@SpringBootTest
 class ChessGameDAOTest {
+
+    @Autowired
+    private JdbcTemplate template;
 
     @Test
     @DisplayName("체스 게임방 생성")
-    void makeChessGameRoom() throws SQLException {
-        ChessGameDAO chessGameDAO = new ChessGameDAO();
+    void makeChessGameRoom() {
+        ChessGameDAO chessGameDAO = new ChessGameDAO(template);
         ChessGame chessGame = ChessGame.initChessGame();
         chessGame.setName("zero");
         String gameId = chessGameDAO.addGame(chessGame);
@@ -22,8 +28,8 @@ class ChessGameDAOTest {
 
     @Test
     @DisplayName("체스 게임방 가져오기")
-    void findChessGameRoom() throws SQLException {
-        ChessGameDAO chessGameDAO = new ChessGameDAO();
+    void findChessGameRoom() {
+        ChessGameDAO chessGameDAO = new ChessGameDAO(template);
         List<ChessGameRoomInfoDTO> activeGames = chessGameDAO.findActiveGames();
 
         Assertions.assertThat(activeGames.size()).isNotEqualTo(0);

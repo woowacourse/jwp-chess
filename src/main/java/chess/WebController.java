3080d588549e560c847406bc27e5a7db698f6174
awesomeo184 <awesomeo184@gmail.com>
2022-04-22 20:25:31 +0900

    refactor: HttpServletRequest 대신 RequestParam을 사용하도록 변경

--- src/main/java/chess/WebController.java
+++ src/main/java/chess/WebController.java
@@ -6,7 +6,6 @@ import chess.dto.BoardDTO;
 import chess.dto.ChessGameRoomInfoDTO;
 import chess.service.ChessService;
 import java.sql.SQLException;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -28,32 +28,32 @@ public class WebController {
     }
 
     @PostMapping("/chess/new")
-    public String createGame(HttpServletRequest request) throws SQLException {
-        String gameId = CHESS_SERVICE.addChessGame(request.getParameter("gameName"));
-        return "redirect:/chess/game/"+gameId;
+    public String createGame(@RequestParam String gameName) throws SQLException {
+        String gameId = CHESS_SERVICE.addChessGame(gameName);
+        return "redirect:/chess/game/" + gameId;
     }
 
     @GetMapping("/chess/game/{id}")
-    public String temp(@PathVariable int id, Model model){
-        ChessGameRoomInfoDTO chessGameRoomInfoDTO = CHESS_SERVICE.findGameById(String.valueOf(id));
+    public String temp(@PathVariable String id, Model model) {
+        ChessGameRoomInfoDTO chessGameRoomInfoDTO = CHESS_SERVICE.findGameById(id);
         model.addAttribute("chessGameRoom", chessGameRoomInfoDTO);
         return "game";
     }
 
     @GetMapping("/chess/game/{id}/board")
     @ResponseBody
-    public ResponseEntity<BoardDTO> createBoard(@PathVariable int id) throws SQLException {
-        ChessGame chessGame = CHESS_SERVICE.getChessGamePlayed(String.valueOf(id));
+    public ResponseEntity<BoardDTO> createBoard(@PathVariable String id) throws SQLException {
+        ChessGame chessGame = CHESS_SERVICE.getChessGamePlayed(id);
         return ResponseEntity.ok(new BoardDTO(chessGame));
     }
 
     @PostMapping("/chess/game/{id}/move")
     @ResponseBody
-    public ResponseEntity<BoardDTO> movePiece(HttpServletRequest request, @PathVariable int id) throws SQLException {
-        String source = request.getParameter("source");
-        String target = request.getParameter("target");
-        Team team = Team.valueOf(request.getParameter("team"));
-        ChessGame chessGame = CHESS_SERVICE.movePiece(String.valueOf(id), source, target, team);
+    public ResponseEntity<BoardDTO> movePiece(@RequestParam String source,
+                                              @RequestParam String target,
+                                              @RequestParam String team,
+                                              @PathVariable String id) throws SQLException {
+        ChessGame chessGame = CHESS_SERVICE.movePiece(id, source, target, Team.valueOf(team));
         return ResponseEntity.ok(new BoardDTO(chessGame));
     }
 

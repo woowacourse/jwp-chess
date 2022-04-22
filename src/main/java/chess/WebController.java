1bac9fbbab55833c06ef2dca62ae687571d7e2c6
awesomeo184 <awesomeo184@gmail.com>
2022-04-22 19:51:50 +0900

    refactor: spark template 의존성 제거

--- src/main/java/chess/WebController.java
+++ src/main/java/chess/WebController.java
@@ -1,17 +1,11 @@
 package chess;
 
-import static chess.utils.Render.renderHtml;
-
 import chess.dao.ChessGame;
 import chess.domain.piece.property.Team;
 import chess.dto.BoardDTO;
 import chess.dto.ChessGameRoomInfoDTO;
-import chess.dto.GameStatus;
 import chess.service.ChessService;
-import chess.utils.JsonConvertor;
-import chess.utils.Render;
 import java.sql.SQLException;
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -41,46 +35,26 @@ public class WebController {
 
     @GetMapping("/chess/game/{id}")
     public String temp(@PathVariable int id, Model model){
-        ChessGameRoomInfoDTO ChessGameRoomInfoDTO = CHESS_SERVICE.findGameById(String.valueOf(id));
-        model.addAttribute("gameId", ChessGameRoomInfoDTO.getId());
-        model.addAttribute("gameName", ChessGameRoomInfoDTO.getName());
+        ChessGameRoomInfoDTO chessGameRoomInfoDTO = CHESS_SERVICE.findGameById(String.valueOf(id));
+        model.addAttribute("chessGameRoom", chessGameRoomInfoDTO);
         return "game";
     }
 
-
     @GetMapping("/chess/game/{id}/board")
     @ResponseBody
-    public String createBoard(@PathVariable int id, Model model) throws SQLException {
+    public ResponseEntity<BoardDTO> createBoard(@PathVariable int id) throws SQLException {
         ChessGame chessGame = CHESS_SERVICE.getChessGamePlayed(String.valueOf(id));
-        Map<String, Object> boardDto = new BoardDTO(chessGame.getChessBoard()).getResult();
-        String boardHtml = renderHtml(boardDto, "/board.hbs");
-        model.addAttribute("board", boardHtml);
-        model.addAttribute("currentTurn", chessGame.getChessBoard().getCurrentTurn());
-        inputTeamScoresToModel(chessGame, model);
-        return JsonConvertor.toJson(model);
+        return ResponseEntity.ok(new BoardDTO(chessGame));
     }
 
     @PostMapping("/chess/game/{id}/move")
     @ResponseBody
-    public String movePiece(HttpServletRequest request, @PathVariable int id) throws SQLException {
+    public ResponseEntity<BoardDTO> movePiece(HttpServletRequest request, @PathVariable int id) throws SQLException {
         String source = request.getParameter("source");
         String target = request.getParameter("target");
         Team team = Team.valueOf(request.getParameter("team"));
         ChessGame chessGame = CHESS_SERVICE.movePiece(String.valueOf(id), source, target, team);
-        final Map<String, Object> model = Render.renderBoard(chessGame);
-
-        if (chessGame.isGameSet()) {
-            Map<String, Object> result = CHESS_SERVICE.getResult(chessGame);
-            model.putAll(result);
-        }
-        return JsonConvertor.toJson(model);
-    }
-
-    private static void inputTeamScoresToModel(ChessGame chessGame, Model model) {
-        double blackScore = GameStatus.calculateTeamScore(chessGame.getChessBoard().getBoard(), Team.BLACK);
-        double whiteScore = GameStatus.calculateTeamScore(chessGame.getChessBoard().getBoard(), Team.WHITE);
-        model.addAttribute("currentBlackScore", blackScore);
-        model.addAttribute("currentWhiteScore", whiteScore);
+        return ResponseEntity.ok(new BoardDTO(chessGame));
     }
 
     @ExceptionHandler(Exception.class)

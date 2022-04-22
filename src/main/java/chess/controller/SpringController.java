f5abc7d17d008167063b01b2f28affc790f3836b
Donggyu <a29661498@gmail.com>
2022-04-22 16:11:37 +0900

    feat: "/move" URI 컨트롤러 등록

--- src/main/java/chess/controller/SpringController.java
+++ src/main/java/chess/controller/SpringController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,5 +35,28 @@ public class SpringController {
         return modelAndView;
     }
 
+    @PostMapping("/move")
+    public ModelAndView move(@RequestParam("from") String from, @RequestParam("to") String to) {
+        List<String> chessBoard = chessService.getCurrentChessBoard();
 
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            String command = makeCommand(from, to);
+            chessBoard = chessService.move(command);
+            if (chessService.isEnd()) {
+                modelAndView.setViewName("redirect:/end");
+                return modelAndView;
+            }
+        } catch (IllegalArgumentException e) {
+            modelAndView.addObject("error", e.getMessage());
+        }
+
+        modelAndView.addObject("chessboard", chessBoard);
+        modelAndView.setViewName("chess");
+        return modelAndView;
+    }
+
+    private String makeCommand(String from, String to) {
+        return "move " + from + " " + to;
+    }
 }

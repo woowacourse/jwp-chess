65d8cc014cdddc6203907581a90e344e0dd956a4
kth990303 <kth990303@naver.com>
2022-04-22 17:44:38 +0900

    refactor: 불필요한 js 함수 삭제

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -2,23 +2,13 @@ let currentClickPosition = '';
 let currentPiece = '';
 let destinationClickPosition = '';
 
-const initChessBoard = () => {
-    initPieceRank();
-    initMapEvent();
-}
-
-const initPieceKind = (position) => {
-    const map = new Map([
-        ['a1', 'white_rook'], ['b1', 'white_knight'], ['c1', 'white_bishop'], ['d1', 'white_queen'],
-        ['e1', 'white_king'], ['f1', 'white_bishop'], ['g1', 'white_knight'], ['h1', 'white_rook'],
-        ['a2', 'white_pawn'], ['b2', 'white_pawn'], ['c2', 'white_pawn'], ['d2', 'white_pawn'],
-        ['e2', 'white_pawn'], ['f2', 'white_pawn'], ['g2', 'white_pawn'], ['h2', 'white_pawn'],
-        ['a7', 'black_pawn'], ['b7', 'black_pawn'], ['c7', 'black_pawn'], ['d7', 'black_pawn'],
-        ['e7', 'black_pawn'], ['f7', 'black_pawn'], ['g7', 'black_pawn'], ['h7', 'black_pawn'],
-        ['a8', 'black_rook'], ['b8', 'black_knight'], ['c8', 'black_bishop'], ['d8', 'black_queen'],
-        ['e8', 'black_king'], ['f8', 'black_bishop'], ['g8', 'black_knight'], ['h8', 'black_rook']
-    ]);
-    return map.get(position);
+const initMapEvent = () => {
+    for (let file = 0; file < 8; file++) {
+        for (let rank = 1; rank <= 8; rank++) {
+            const positionTag = document.getElementById(intToFile(file) + rank);
+            positionTag.addEventListener('click', clickToMove);
+        }
+    }
 }
 
 const intToFile = (value) => {
@@ -38,37 +28,11 @@ const toPieceFullName = (name) => {
     return map.get(name);
 }
 
-const initPieceRank = () => {
-    for (let rank = 1; rank <= 2; rank++) {
-        initPieceFile(rank.toString());
-    }
-    for (let rank = 7; rank <= 8; rank++) {
-        initPieceFile(rank.toString());
-    }
-}
-
-const initPieceFile = (rank) => {
-    for (let file = 0; file < 8; file++) {
-        const position = intToFile(file) + rank;
-        const positionTag = document.getElementById(intToFile(file) + rank);
-        markPiece(positionTag, initPieceKind(position));
-    }
-}
-
 const markPiece = (position, pieceKind) => {
     const piece = document.createElement("img");
     piece.className = 'chess-piece';
     piece.src = "images/" + pieceKind + ".png";
     position.appendChild(piece);
-}
-
-const initMapEvent = () => {
-    for (let file = 0; file < 8; file++) {
-        for (let rank = 1; rank <= 8; rank++) {
-            const positionTag = document.getElementById(intToFile(file) + rank);
-            positionTag.addEventListener('click', clickToMove);
-        }
-    }
 }
 
 const clickToMove = async (e) => {
@@ -165,9 +129,9 @@ const showResult = async () => {
 }
 
 const showError = async (message) => {
-    if(message){
-        document.getElementById('message-info').innerHTML=message;
+    if (message) {
+        document.getElementById('message-info').innerHTML = message;
         return;
     }
-    document.getElementById('message-info').innerHTML='';
+    document.getElementById('message-info').innerHTML = '';
 }

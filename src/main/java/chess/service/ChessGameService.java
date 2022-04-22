055d292fd13a938d84798cea9bc3e9c24dbbde40
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-22 20:45:13 +0900

    feat: 체스 게임 서비스 체스 게임 생성 기능 추가

---
+++ src/main/java/chess/service/ChessGameService.java
@@ -0,0 +1,25 @@
+package chess.service;
+
+import chess.dao.ChessGameDao;
+import chess.dao.PieceDao;
+import chess.domain.piece.PieceFactory;
+import chess.domain.state.Turn;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChessGameService {
+
+	private final PieceDao pieceDao;
+	private final ChessGameDao chessGameDao;
+
+	public ChessGameService(PieceDao pieceDao, ChessGameDao chessGameDao) {
+		this.pieceDao = pieceDao;
+		this.chessGameDao = chessGameDao;
+	}
+
+	public long createNewChessGame(){
+		long chessGameId = chessGameDao.createChessGame(Turn.WHITE_TURN);
+		pieceDao.savePieces(PieceFactory.createNewChessBoard(chessGameId));
+		return chessGameId;
+	}
+}

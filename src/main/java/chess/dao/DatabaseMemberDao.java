780cbfaaa8119218982382dec518214f3c52aa87
sinb57 <sinb57@naver.com>
2022-04-22 20:05:41 +0900

    refactor: spring-jdbc dao 리팩토링

--- src/main/java/chess/dao/DatabaseMemberDao.java
+++ src/main/java/chess/dao/DatabaseMemberDao.java
@@ -1,67 +1,61 @@
 package chess.dao;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import chess.domain.Member;
 
 @Repository
 public class DatabaseMemberDao implements MemberDao {
-    private final SqlExecutor executor = SqlExecutor.getInstance();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public Long save(Member member) {
         final String sql = "insert into Member (name) values (?)";
-        return executor.insertAndGetGeneratedKey(connection -> {
-            final PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, member.getName());
-            return statement;
-        });
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, member.getName());
+            return ps;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
     }
 
     @Override
     public Optional<Member> findById(Long id) {
         final String sql = "select id, name from Member where id = ?";
-        return executor.select(connection -> {
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setLong(1, id);
-            return statement;
-        }, this::serializeMember);
-    }
-
-    private Optional<Member> serializeMember(ResultSet resultSet) throws SQLException {
-        if (!resultSet.next()) {
-            return Optional.empty();
-        }
-        return Optional.of(new Member(resultSet.getLong("id"), resultSet.getString("name")));
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(sql,
+                        (resultSet, rowNum) -> new Member(
+                                resultSet.getLong("id"),
+                                resultSet.getString("name")
+                        ), id)
+        );
     }
 
     @Override
     public List<Member> findAll() {
         final String sql = "select id, name from Member";
-        return executor.select(connection -> connection.prepareStatement(sql), resultSet -> {
-            List<Member> members = new ArrayList<>();
-            while (resultSet.next()) {
-                members.add(new Member(resultSet.getLong("id"), resultSet.getString("name")));
-            }
-            return members;
-        });
+        return jdbcTemplate.query(
+                sql, (resultSet, rowNum) -> new Member(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name")
+                ));
     }
 
     @Override
     public void deleteById(Long id) {
         final String sql = "delete from Member where id = ?";
-        executor.delete(connection -> {
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setLong(1, id);
-            return statement;
-        });
+        jdbcTemplate.update(sql, id);
     }
 }

bb09113d55d79be9221f3581c4d2f5d85b6013a1
meatsby <meatsby@gmail.com>
2022-04-22 16:16:30 +0900

    feat: 방 만들기 및 방 조회 기능 구현

--- src/main/java/chess/SpringChessApplication.java
+++ src/main/java/chess/SpringChessApplication.java
@@ -1,11 +1,17 @@
 package chess;
 
 import chess.domain.game.ChessController;
+import chess.dto.RequestDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 @Controller
@@ -21,5 +27,18 @@ public class SpringChessApplication {
     public String index(Model model) {
         model.addAttribute("boards", controller.getRooms());
         return "home";
+    }
+
+    @PostMapping("/room")
+    public String createRoom(@ModelAttribute RequestDto requestDto) {
+        final int roomId = controller.startGame(requestDto);
+        return "redirect:/room/" + roomId;
+    }
+
+    @GetMapping("/room/{roomId}")
+    public String showRoom(@PathVariable("roomId") int id, Model model) {
+        model.addAttribute("roomId", id);
+        model.addAttribute("board", controller.getBoard(id));
+        return "index";
     }
 }

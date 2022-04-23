7d9fcf9b365cbf8ff8e6269f24af9bcf0cb7bb3a
Richard JEON <ztzy1907@gmail.com>
2022-04-24 03:16:05 +0900

    feat(color): 턴을 의미하는 Color enum에 String을 받는 from 생성자 추가

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -4,17 +4,31 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum Color {
-    BLACK,
-    WHITE,
-    EMPTY;
+    BLACK("black"),
+    WHITE("white"),
+    EMPTY("empty");
 
     private static final String NO_MATCHED_COLOR_FOUND_BY_PIECE = "해당 기물의 색상을 확인하는 데 실패하였습니다";
+    private static final String NO_MATCHED_COLOR_FOUND_BY_STRING = "일치하는 색상을 찾지 못했습니다";
+
+    private final String colorName;
+
+    Color(String colorName) {
+        this.colorName = colorName;
+    }
 
     public static Color from(Piece piece) {
         return Arrays.stream(values())
                 .filter(piece::isSameColor)
                 .findAny()
                 .orElseThrow(() -> new NoSuchElementException(NO_MATCHED_COLOR_FOUND_BY_PIECE));
+    }
+
+    public static Color from(String colorInput) {
+        return Arrays.stream(values())
+                .filter(color -> color.colorName.equalsIgnoreCase(colorInput))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException(NO_MATCHED_COLOR_FOUND_BY_STRING));
     }
 
     public Color opposite() {

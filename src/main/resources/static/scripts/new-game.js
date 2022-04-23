af6c545fdb01d0afdc7e27b97106e1f8cbbd3a1b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-23 23:59:11 +0900

    feat(web): setup html, css for /game page
    - change init game logic at client side
    - move to /game on clicking init game button

--- src/main/resources/static/scripts/new-game.js
+++ src/main/resources/static/scripts/new-game.js
@@ -1,12 +1,30 @@
-const initGameAndGetId = async () => {
-    const response = await fetch("/game", {method: "post"});
+const buildBody = (form) => {
+    const data = {};
+    for (const pair of new FormData(form)) {
+        data[pair[0]] = pair[1];
+    }
+    return data;
+}
+
+const initGameAndGetId = async (e, form) => {
+    e.preventDefault();
+    const response = await fetch("/game", {
+        headers: {'Content-Type': 'application/json'}, // 디폴트: text/plain;charset=UTF-8
+        method: "post",
+        body: JSON.stringify(buildBody(form))
+    });
+    if (!response.ok) {
+        return alert(await response.text());
+    }
     const {id} = await response.json();
     window.location.replace(`/game/${id}`);
 }
 
 const init = () => {
-    const newGameBtn = document.querySelector("button#init_game");
-    newGameBtn.addEventListener("click", initGameAndGetId);
+    const form = document.querySelector("form");
+    form.addEventListener("submit",
+        (e) => initGameAndGetId(e, form)
+    );
 }
 
 init();

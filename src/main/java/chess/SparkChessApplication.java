6866904a1f7e4e0af4745ab562a5bee44fbe95d8
Richard JEON <ztzy1907@gmail.com>
2022-04-24 03:17:34 +0900

    refactor(spark): 스파크 컨트롤러 및 view template 가져옴

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -1,18 +1,45 @@
 package chess;
 
-import chess.controller.WebChessController;
+import static spark.Spark.exception;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import chess.dao.ChessJDBCDao;
+import chess.dto.ExceptionDto;
+import chess.dto.MoveRequestDto;
+import chess.service.ChessService;
+import chess.service.DBChessServiceImpl;
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class SparkChessApplication {
+    private static final ChessService CHESS_SERVICE = new DBChessServiceImpl(new ChessJDBCDao());
+    private static final Gson GSON = new Gson();
+
     public static void main(String[] args) {
-        new WebChessController().run();
+        staticFiles.location("/static");
+
+        get("/", (req, res) -> render(new HashMap<>(), "index.html"));
+
+        get("/game/:gameId", (req, res) -> render(new HashMap<>(), "game.html"));
+
+        get("/board/:gameId", (req, res) -> GSON.toJson(CHESS_SERVICE.getBoardByGameId(req.params("gameId"))));
+
+        get("/score/:gameId", (req, res) -> GSON.toJson(CHESS_SERVICE.getScore(req.params("gameId"))));
+
+        get("/isFinished/:gameId", (req, res) -> GSON.toJson(CHESS_SERVICE.isFinished(req.params("gameId"))));
+
+        post("/move", (req, res) -> GSON.toJson(CHESS_SERVICE.move(GSON.fromJson(req.body(), MoveRequestDto.class))));
+
+        exception(Exception.class,
+                (exception, req, res) -> res.body(GSON.toJson(new ExceptionDto(exception.getMessage()))));
     }
-    //     get("/", (req, res) -> {
-    //         Map<String, Object> model = new HashMap<>();
-    //         return render(model, "index.hbs");
-    //     });
-    // }
-    //
-    // private static String render(Map<String, Object> model, String templatePath) {
-    //     return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-    // }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 }

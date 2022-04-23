49250c490267a14498e4ddc860f20e078dd45981
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-24 00:31:19 +0900

    feat(web): remove search page and cleanse ui

--- src/main/java/chess/controller/SearchController.java
+++ src/main/java/chess/controller/SearchController.java
@@ -1,35 +1,20 @@
 package chess.controller;
 
-import chess.dto.GameCountDto;
 import chess.dto.SearchResultDto;
 import chess.service.ChessService2;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/search")
 public class SearchController {
 
-    private static final String HTML_TEMPLATE_PATH = "search";
-    private static final String RESPONSE_MODEL_KEY = "response";
-
     private final ChessService2 chessService;
 
     public SearchController(ChessService2 chessService) {
         this.chessService = chessService;
-    }
-
-    @GetMapping
-    public ModelAndView renderSearch() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName(HTML_TEMPLATE_PATH);
-        GameCountDto gameCountDto = chessService.countGames();
-        modelAndView.addObject(RESPONSE_MODEL_KEY, gameCountDto);
-        return modelAndView;
     }
 
     @PostMapping

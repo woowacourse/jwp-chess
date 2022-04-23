c4fa1ed621a74639f012368a8319e70b0f5c6066
meatsby <meatsby@gmail.com>
2022-04-23 16:07:26 +0900

    test: 보드 Dao 테스트 추가

--- src/test/java/chess/dao/WebBoardDaoTest.java
+++ src/test/java/chess/dao/WebBoardDaoTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.domain.game.ChessBoard;
 import chess.domain.pieces.Color;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,11 +18,18 @@ class WebBoardDaoTest {
     @Autowired
     private WebChessBoardDao dao;
 
+    int boardId;
+
+    @BeforeEach
+    void setup() {
+        final ChessBoard board = dao.save(new ChessBoard("개초보만"));
+        this.boardId = board.getId();
+    }
+
     @Test
     void saveTest() {
         final ChessBoard board = dao.save(new ChessBoard("개초보만"));
         assertThat(board.getRoomTitle()).isEqualTo("개초보만");
-        dao.deleteById(board.getId());
     }
 
     @Test
@@ -42,7 +51,23 @@ class WebBoardDaoTest {
 
     @Test
     void findAllTest() {
-        assertThat(dao.findAll().size()).isEqualTo(2);
+        assertThat(dao.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    void deleteAll() {
+        dao.deleteAll();
+        List<ChessBoard> boards = dao.findAll();
+
+        assertThat(boards.size()).isEqualTo(0);
+    }
+
+    @Test
+    void updateTurn() {
+        dao.updateTurn(Color.BLACK, boardId);
+        ChessBoard chessBoard = dao.getById(boardId);
+
+        assertThat(chessBoard.getTurn()).isEqualTo(Color.BLACK);
     }
 
     @AfterEach

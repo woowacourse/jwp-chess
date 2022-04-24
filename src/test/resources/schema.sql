b13f8ba87caa35d607f86d9468396532032964ee
Seongwon97 <osw970325@naver.com>
2022-04-25 00:30:02 +0900

    chore: 테스트 db 설정 추가

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,21 @@
+drop table chess_game if exists;
+drop table piece if exists;
+
+create table chess_game
+(
+    id   int         not null auto_increment primary key,
+    name varchar(10) not null,
+    turn varchar(5)  not null
+);
+
+create table piece
+(
+    id            int        not null auto_increment primary key,
+    position      varchar(2) not null,
+    name          varchar(6) not null,
+    team          varchar(5) not null,
+    chess_game_id int        not null,
+    foreign key (chess_game_id) references chess_game (id)
+);
+
+insert into chess_game (name, turn) values ('rex_game', '화이트');

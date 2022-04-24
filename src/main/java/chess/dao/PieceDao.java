6e3e751052c96ac7a4b491d51a8812f43f6df76f
Donggyu <a29661498@gmail.com>
2022-04-24 17:26:38 +0900

    feat: delete 'jdbcTemplate' 전환

--- src/main/java/chess/dao/PieceDao.java
+++ src/main/java/chess/dao/PieceDao.java
@@ -1,14 +1,9 @@
 package chess.dao;
 
-import static chess.dao.DBConnector.getConnection;
-
 import chess.dto.ChessBoardDto;
 import chess.dto.ChessGameDto;
 import chess.dto.PieceDto;
 import chess.dto.PositionDto;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -49,16 +44,7 @@ public class PieceDao {
 
     public void delete(ChessGameDto chessGameDto) {
         String sql = "delete from piece where game_name = ?";
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            String gameName = chessGameDto.getGameName();
-
-            statement.setString(1, gameName);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new IllegalStateException(e.getMessage());
-        }
+        jdbcTemplate.update(sql, chessGameDto.getGameName());
     }
 
     public void update(ChessGameDto chessGameDto) {

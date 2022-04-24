a1dcf4653560cf460b0c50fd4de2f102f51353e7
Seongwon97 <osw970325@naver.com>
2022-04-25 00:30:33 +0900

    test: dao 테스트 코드 추가

---
+++ src/test/java/chess/dao/ChessGameDaoTest.java
@@ -0,0 +1,87 @@
+package chess.dao;
+
+import chess.domain.player.Team;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ChessGameDaoTest {
+
+    @Autowired
+    private ChessGameDao chessGameDao;
+
+    private final String gameName = "rex_game";
+    private final int gameId = 1;
+
+    @Test
+    @DisplayName("게임의 아이디를 올바르게 찾아온다.")
+    void findChessGameIdByName() {
+        int id = chessGameDao.findChessGameIdByName(gameName);
+
+        assertThat(id).isEqualTo(gameId);
+    }
+
+    @Test
+    @DisplayName("중복된 게임이 존재하는 경우 true를 반환한다.")
+    void isDuplicateGameName() {
+        boolean result = chessGameDao.isDuplicateGameName(gameName);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("중복된 게임이 존재하는 경우 false를 반환한다.")
+    void isNotDuplicateGameName() {
+        boolean result = chessGameDao.isDuplicateGameName("newGame");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("게임을 올바르게 저장한다.")
+    void saveChessGame() {
+        String newGameName = "new Game";
+        Team turn = Team.WHITE;
+
+        chessGameDao.saveChessGame(newGameName, turn);
+        int id = chessGameDao.findChessGameIdByName(newGameName);
+
+        assertThat(id).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게임의 아이디를 통해 현재 턴을 찾아온다.")
+    void findCurrentTurn() {
+        String currentTurn = chessGameDao.findCurrentTurn(gameId);
+        String expected = Team.WHITE.getName();
+
+        assertThat(currentTurn).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("턴 정보를 업데이트 한다.")
+    void updateGameTurn() {
+        Team nextTurn = Team.BLACK;
+        chessGameDao.updateGameTurn(gameId, nextTurn);
+
+        String currentTurn = chessGameDao.findCurrentTurn(gameId);
+
+        assertThat(currentTurn).isEqualTo(nextTurn.getName());
+    }
+
+    @Test
+    @DisplayName("게임을 삭제한다.")
+    void deleteChessGame() {
+        chessGameDao.deleteChessGame(gameId);
+
+        boolean result = chessGameDao.isDuplicateGameName(gameName);
+
+        assertThat(result).isFalse();
+    }
+}
---
+++ src/test/java/chess/dao/PieceDaoTest.java
@@ -0,0 +1,98 @@
+package chess.dao;
+
+import chess.domain.generator.BlackGenerator;
+import chess.domain.generator.WhiteGenerator;
+import chess.domain.player.Player;
+import chess.domain.player.Team;
+import chess.dto.PieceDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class PieceDaoTest {
+
+    @Autowired
+    private PieceDao pieceDao;
+
+    private final Player whitePlayer = new Player(new WhiteGenerator(), Team.WHITE);
+    private final Player blackPlayer = new Player(new BlackGenerator(), Team.BLACK);
+    private final String teamWhite = Team.WHITE.getName();
+    private final int gameId = 1;
+
+    @BeforeEach
+    void beforeEach() {
+        pieceDao.savePieces(whitePlayer, gameId);
+    }
+
+    @Test
+    @DisplayName("게임 id와 팀 이름을 통해 말들을 조회한다.")
+    void findChessGameIdByName() {
+        List<PieceDto> whitePieces = pieceDao.findAllPieceByIdAndTeam(gameId, teamWhite);
+
+        assertThat(whitePieces.size()).isEqualTo(16);
+    }
+
+    @Test
+    @DisplayName("말들을 올바르게 저장한다.")
+    void savePieces() {
+        pieceDao.savePieces(blackPlayer, gameId);
+        List<PieceDto> blackPieces = pieceDao.findAllPieceByIdAndTeam(gameId, Team.BLACK.getName());
+
+        assertThat(blackPieces.size()).isEqualTo(16);
+    }
+
+    @Test
+    @DisplayName("말들을 올바르게 삭제한다.")
+    void deletePieces() {
+        pieceDao.deletePieces(gameId);
+        List<PieceDto> whitePieces = pieceDao.findAllPieceByIdAndTeam(gameId, teamWhite);
+
+        assertThat(whitePieces.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("특정 위치의 말을 삭제한다.")
+    void deletePieceByGameIdAndPositionAndTeam() {
+        String deletePosition = "a2";
+        pieceDao.deletePieceByGameIdAndPositionAndTeam(gameId, deletePosition, teamWhite);
+        List<PieceDto> whitePieces = pieceDao.findAllPieceByIdAndTeam(gameId, teamWhite);
+
+        Optional<PieceDto> result = whitePieces.stream()
+                .filter(pieceDto -> pieceDto.getPosition().equals(deletePosition))
+                .findFirst();
+
+        assertThat(result.isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("말들의 위치를 올바르게 변경한다.")
+    void updatePiecePositionByGameId() {
+        String currentPosition = "a2";
+        String destinationPosition = "a4";
+        pieceDao.updatePiecePositionByGameId(gameId, currentPosition, destinationPosition, teamWhite);
+        List<PieceDto> whitePieces = pieceDao.findAllPieceByIdAndTeam(gameId, teamWhite);
+
+        Optional<PieceDto> result = whitePieces.stream()
+                .filter(pieceDto -> pieceDto.getPosition().equals(destinationPosition))
+                .findFirst();
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+
+    @AfterEach
+    void afterEach() {
+        pieceDao.deletePieces(gameId);
+    }
+}

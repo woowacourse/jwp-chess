092a918738d5815910b3b684599bf0f94bc6226d
pup-paw <wldusdl0310@gmail.com>
2022-04-24 18:19:06 +0900

    refactor: import 순서 수정, 변수명을 명시적인 이름으로 변경

--- src/main/java/chess/controller/ResultController.java
+++ src/main/java/chess/controller/ResultController.java
@@ -1,25 +1,26 @@
 package chess.controller;
 
-import chess.domain.GameResult;
-import chess.domain.piece.Color;
-import chess.service.ChessService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import chess.domain.GameResult;
+import chess.domain.piece.Color;
+import chess.service.ChessService;
+
 @Controller
 public class ResultController {
-    private final ChessService DBService;
+    private final ChessService chessService;
 
     public ResultController(final ChessService chessService) {
-        this.DBService = chessService;
+        this.chessService = chessService;
     }
 
     @GetMapping("/status")
     public String showResult(@RequestParam String gameID, Model model) {
 
-        GameResult gameResult = DBService.getGameResult(gameID);
+        GameResult gameResult = chessService.getGameResult(gameID);
         model.addAttribute("whiteScore", gameResult.calculateScore(Color.WHITE));
         model.addAttribute("blackScore", gameResult.calculateScore(Color.BLACK));
         return "status";

1818cf31150092ec98993e449533930028230f37
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-04-24 21:14:02 +0900

    refactor(MoveCommandDto):gameId 필드 삭제

--- src/main/java/chess/dto/MoveCommandDto.java
+++ src/main/java/chess/dto/MoveCommandDto.java
@@ -5,7 +5,6 @@ import chess.domain.command.MoveCommand;
 public class MoveCommandDto {
     private String source;
     private String target;
-    private String gameId;
 
     public MoveCommandDto(){
     }
@@ -13,7 +12,6 @@ public class MoveCommandDto {
     public MoveCommandDto(String source, String target, String gameId) {
         this.source = source;
         this.target = target;
-        this.gameId = gameId;
     }
 
     public String getSource() {
@@ -22,10 +20,6 @@ public class MoveCommandDto {
 
     public String getTarget() {
         return target;
-    }
-
-    public String getGameId() {
-        return gameId;
     }
 
     public MoveCommand toEntity() {

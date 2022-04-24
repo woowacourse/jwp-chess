bbf3c03a1620d4b40bc799d9a1fe65e6325a3a40
devHudi <devhudi@gmail.com>
2022-04-24 16:05:12 +0900

    refactor: RowMapper 추출

--- src/main/java/chess/dao/BoardDaoImpl.java
+++ src/main/java/chess/dao/BoardDaoImpl.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -20,6 +21,18 @@ public class BoardDaoImpl implements BoardDao {
     private static final String TABLE_NAME = "board";
 
     private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Position> positionMapper = (resultSet, rowNum) -> {
+        XAxis xAxis = XAxis.getByValue(resultSet.getString("x_axis"));
+        YAxis yAxis = YAxis.getByValue(resultSet.getString("y_axis"));
+        return Position.of(xAxis, yAxis);
+    };
+
+    private final RowMapper<Piece> pieceRowMapper = (resultSet, rowNum) -> {
+        PieceType pieceType = PieceType.valueOf(resultSet.getString("piece_type"));
+        PieceColor pieceColor = PieceColor.valueOf(resultSet.getString("piece_color"));
+        return new Piece(pieceType, pieceColor);
+    };
 
     @Autowired
     public BoardDaoImpl(JdbcTemplate jdbcTemplate) {
@@ -29,17 +42,12 @@ public class BoardDaoImpl implements BoardDao {
     @Override
     public Board getBoard(GameId gameId) {
         Map<Position, Piece> boardValue = new HashMap<>();
+        String query = String.format(
+                "SELECT piece_type, piece_color FROM %s WHERE game_id = ? AND x_axis = ? AND y_axis = ?", TABLE_NAME);
 
         for (Position position : getPositionsByGameId(gameId)) {
-            String query = String.format(
-                    "SELECT piece_type, piece_color FROM %s WHERE game_id = ? AND x_axis = ? AND y_axis = ?",
-                    TABLE_NAME);
-            Piece piece = jdbcTemplate.queryForObject(query, (resultSet, rowNum) -> {
-                PieceType pieceType = PieceType.valueOf(resultSet.getString("piece_type"));
-                PieceColor pieceColor = PieceColor.valueOf(resultSet.getString("piece_color"));
-                return new Piece(pieceType, pieceColor);
-            }, gameId.getGameId(), position.getXAxis().getValueAsString(), position.getYAxis().getValueAsString());
-
+            Piece piece = jdbcTemplate.queryForObject(query, pieceRowMapper, gameId.getGameId(),
+                    position.getXAxis().getValueAsString(), position.getYAxis().getValueAsString());
             boardValue.put(position, piece);
         }
 
@@ -47,22 +55,14 @@ public class BoardDaoImpl implements BoardDao {
     }
 
     private List<Position> getPositionsByGameId(GameId gameId) {
-        String query = String.format("SELECT x_axis, y_axis FROM %s WHERE game_id = ?",
-                TABLE_NAME);
-        List<Position> positions = jdbcTemplate.query(query, (resultSet, rowNum) -> {
-            XAxis xAxis = XAxis.getByValue(resultSet.getString("x_axis"));
-            YAxis yAxis = YAxis.getByValue(resultSet.getString("y_axis"));
-            Position position = Position.of(xAxis, yAxis);
-            return position;
-        }, gameId.getGameId());
-        return positions;
+        String query = String.format("SELECT x_axis, y_axis FROM %s WHERE game_id = ?", TABLE_NAME);
+        return jdbcTemplate.query(query, positionMapper, gameId.getGameId());
     }
 
     @Override
     public void createPiece(GameId gameId, Position position, Piece piece) {
         String query = String.format(
-                "INSERT INTO %s(game_id, x_axis, y_axis, piece_type, piece_color) VALUES(?, ?, ?, ?, ?)",
-                TABLE_NAME);
+                "INSERT INTO %s(game_id, x_axis, y_axis, piece_type, piece_color) VALUES(?, ?, ?, ?, ?)", TABLE_NAME);
         jdbcTemplate.update(query, gameId.getGameId(), position.getXAxis().getValueAsString(),
                 position.getYAxis().getValueAsString(), piece.getPieceType().name(), piece.getPieceColor().name());
     }

690a3d23c5298bf1c59acc3a7fa7e8ac5f1622a9
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-25 16:55:08 +0900

    refactor: Exception관리를 ControllerAdvice에서 하도록 수정

---
+++ src/main/java/chess/exception/ExceptionAdvice.java
@@ -0,0 +1,14 @@
+package chess.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler()
+    public ResponseEntity<String> getException(final Exception exception) {
+        return ResponseEntity.internalServerError().body(exception.getMessage());
+    }
+}

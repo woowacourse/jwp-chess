fb2365be0079f44a6df67faf1415fc1bb03feca6
her0807 <her0807@naver.com>
2022-04-25 14:38:54 +0900

    refactor: create table 부분 schema 설정 파일로 추출

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE board IF EXISTS;
+
+create table board
+(
+    position varchar(10) not null,
+    piece    varchar(20) not null,
+    primary key (position)
+);
+
+DROP TABLE game_status IF EXISTS;
+
+create table game_status
+(
+    status varchar(10) not null,
+    primary key (status)
+);
+
+DROP TABLE turn IF EXISTS;
+
+create table turn
+(
+    team varchar(10) not null,
+    primary key (team)
+);
+

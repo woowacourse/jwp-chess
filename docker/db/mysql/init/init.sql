681c89418253ffbb39863cfea40bb874aa203b46
nailseong <nnis9857@gmail.com>
2022-04-25 15:12:17 +0900

    feat: room 테이블에 room_id, password 컬럼 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -2,19 +2,21 @@ USE chess;
 
 CREATE TABLE room
 (
+    room_id      INT         NOT NULL UNIQUE AUTO_INCREMENT,
     name         VARCHAR(10) NOT NULL UNIQUE,
     game_status  VARCHAR(10) NOT NULL,
     current_turn VARCHAR(10) NOT NULL,
+    password     VARCHAR(10) NOT NULL,
     PRIMARY KEY (name)
 );
 
 CREATE TABLE chess_piece
 (
     chess_piece_id INT         NOT NULL UNIQUE AUTO_INCREMENT,
-    room_name      VARCHAR(10) NOT NULL,
+    room_id        INT         NOT NULL,
     position       VARCHAR(10) NOT NULL,
     chess_piece    VARCHAR(10) NOT NULL,
     color          VARCHAR(10) NOT NULL,
     PRIMARY KEY (chess_piece_id),
-    FOREIGN KEY (room_name) REFERENCES room (name) ON DELETE CASCADE
+    FOREIGN KEY (room_id) REFERENCES room (room_id) ON DELETE CASCADE
 );

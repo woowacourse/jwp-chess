5eb557b9698247a098e538fd7000f1663f88aff6
nailseong <nnis9857@gmail.com>
2022-04-25 17:16:12 +0900

    feat: 프론트 코드에 비밀번호 기능 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,6 @@
 async function start() {
     roomName = document.getElementById("roomNameInput").value;
+    password = document.getElementById("passwordInput").value;
     if (/^\s*$/.test(roomName)) {
         const error = document.getElementById("error");
         error.innerText = "방 이름을 입력하세요!";
@@ -8,8 +9,23 @@ async function start() {
         return;
     }
 
-    await fetch(`/rooms/${roomName}`, {method: "POST"});
-    await fetch(`/rooms/${roomName}/pieces`, {method: "POST"});
+    if (/^\s*$/.test(password)) {
+        const error = document.getElementById("error");
+        error.innerText = "비밀번호를 입력하세요!";
 
-    window.location.href = `/rooms/${roomName}`;
+        document.getElementById("password").value = "";
+        return;
+    }
+
+    const res = await fetch("/rooms", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({roomName: `${roomName}`, password: `${password}`})
+    });
+    if (res.status === 201) {
+        await fetch(`/rooms/${roomName}/pieces`, {method: "POST"});
+        window.location.href = res.headers.get("location");
+    }
 }

ccd33795f76736a6a3754a0f3d45ac47269c3fdd
jojogreen91 <iawbg13@gmail.com>
2022-04-26 03:11:15 +0900

    test: Repository 테스트 수정

--- src/test/java/chess/repository/BoardDaoTest.java
+++ src/test/java/chess/repository/BoardDaoTest.java
@@ -1,98 +1,85 @@
-//package chess.repository;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//
-//import chess.repository.BoardDao;
-//import chess.repository.entity.BoardEntity;
-//import java.util.List;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-//import org.springframework.test.context.jdbc.Sql;
-//
-//@JdbcTest
-//@Sql("/settingForTest.sql")
-//public class BoardDaoTest {
-//
-//    private static final String SAVED_NAME = "test";
-//
-//    @Autowired
-//    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-//
-//    private BoardDao boardDao;
-//
-//    @BeforeEach
-//    void setUp() {
-//        boardDao = new BoardDao(namedParameterJdbcTemplate);
-//    }
-//
-//    @Test
-//    @DisplayName("BoardEntity 로 board table 에 저장한다.")
-//    void save() {
-//        BoardEntity newBoardEntity = new BoardEntity(SAVED_NAME, "c", 4, "ROOK", "BLACK");
-//
-//        boardDao.save(List.of(newBoardEntity));
-//
-//        List<BoardEntity> boardEntities = boardDao.load(SAVED_NAME);
-//        long count = boardEntities.stream()
-//                .filter(boardEntity -> boardEntity.getName().equals(SAVED_NAME))
-//                .count();
-//        assertThat(count).isEqualTo(3);
-//    }
-//
-//    @Test
-//    @DisplayName("name 을 이용해서 board 를 삭제한다")
-//    void delete() {
-//        BoardEntity newBoardEntity = new BoardEntity("other", "c", 4, "ROOK", "BLACK");
-//        boardDao.save(List.of(newBoardEntity));
-//
-//        boardDao.delete(SAVED_NAME);
-//
-//        List<BoardEntity> boardEntities = boardDao.load("other");
-//        long count = boardEntities.stream()
-//                .filter(boardEntity -> boardEntity.getName().equals("other"))
-//                .count();
-//        assertThat(count).isEqualTo(1);
-//    }
-//
-//    @Test
-//    @DisplayName("조회할 board 가 존재하지 않을 때 예외를 발생시킨다.")
-//    void validateBoardExist() {
-//        boardDao.delete(SAVED_NAME);
-//
-//        assertThatThrownBy(() -> boardDao.load(SAVED_NAME))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("[ERROR] Board 가 존재하지 않습니다.");
-//    }
-//
-//    @Test
-//    @DisplayName("name 으로 board 를 조회한다.")
-//    void load() {
-//        List<BoardEntity> boardEntities = boardDao.load(SAVED_NAME);
-//
-//        long count = boardEntities.stream()
-//                .filter(boardEntity -> boardEntity.getName().equals(SAVED_NAME))
-//                .count();
-//        assertThat(count).isEqualTo(2);
-//    }
-//
-//    @Test
-//    @DisplayName("name, position_column_value, position_row_value 를 이용해서 board 를 업데이트 한다")
-//    void updatePiece() {
-//        BoardEntity boardEntity = new BoardEntity(SAVED_NAME, "a", 2, "PAWN", "WHITE");
-//
-//        boardDao.updatePiece(boardEntity);
-//
-//        List<BoardEntity> boardEntities = boardDao.load(SAVED_NAME);
-//        long count = boardEntities.stream()
-//                .filter(e -> e.getPositionColumnValue().equals("a") && e.getPositionRowValue() == 2)
-//                .filter(e -> e.getPieceName().equals("PAWN") && e.getPieceTeamValue().equals("WHITE"))
-//                .count();
-//        assertThat(count).isEqualTo(1);
-//    }
-//}
+package chess.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.repository.entity.BoardEntity;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+
+@JdbcTest
+@Sql("/settingForTest.sql")
+public class BoardDaoTest {
+
+    private static final String SAVED_NAME = "test";
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private BoardDao boardDao;
+    private ChessGameDao chessGameDao;
+
+    @BeforeEach
+    void setUp() {
+        boardDao = new BoardDao(namedParameterJdbcTemplate);
+        chessGameDao = new ChessGameDao(namedParameterJdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("BoardEntity 로 board table 에 저장한다.")
+    void save() {
+        int chessGameId = chessGameDao.findChessGameIdByName(SAVED_NAME);
+        BoardEntity newBoardEntity = new BoardEntity(chessGameId, "c", 4, "ROOK", "BLACK");
+
+        boardDao.save(List.of(newBoardEntity));
+
+        List<BoardEntity> boardEntities = boardDao.load(chessGameId);
+        long count = boardEntities.stream()
+                .filter(boardEntity -> boardEntity.getChessGameId() == chessGameId)
+                .count();
+        assertThat(count).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("조회할 board 가 존재하지 않을 때 예외를 발생시킨다.")
+    void validateBoardExist() {
+        assertThatThrownBy(() -> boardDao.load(2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] Board 가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("chess_game_id 으로 board 를 조회한다.")
+    void load() {
+        int chessGameId = chessGameDao.findChessGameIdByName(SAVED_NAME);
+
+        List<BoardEntity> boardEntities = boardDao.load(chessGameId);
+
+        long count = boardEntities.stream()
+                .filter(boardEntity -> boardEntity.getChessGameId() == chessGameId)
+                .count();
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("name, position_column_value, position_row_value 를 이용해서 board 를 업데이트 한다")
+    void updatePiece() {
+        int chessGameId = chessGameDao.findChessGameIdByName(SAVED_NAME);
+        BoardEntity boardEntity = new BoardEntity(chessGameId, "a", 2, "PAWN", "WHITE");
+
+        boardDao.updatePiece(boardEntity);
+
+        List<BoardEntity> boardEntities = boardDao.load(chessGameId);
+        long count = boardEntities.stream()
+                .filter(e -> e.getPositionColumnValue().equals("a") && e.getPositionRowValue() == 2)
+                .filter(e -> e.getPieceName().equals("PAWN") && e.getPieceTeamValue().equals("WHITE"))
+                .count();
+        assertThat(count).isEqualTo(1);
+    }
+}
--- src/test/resources/settingForTest.sql
+++ src/test/resources/settingForTest.sql
@@ -1,8 +1,31 @@
+drop table board if exists;
+
+drop table chess_game if exists;
+
+create table chess_game
+(
+    chess_game_id      int primary key auto_increment,
+    name               varchar(20) not null,
+    is_on              bool        not null,
+    team_value_of_turn varchar(20) not null
+);
+
+
+create table board
+(
+    chess_game_id         int         not null,
+    position_column_value varchar(1)  not null,
+    position_row_value    int         not null,
+    piece_name            varchar(20) not null,
+    piece_team_value      varchar(20) not null,
+    foreign key (chess_game_id) references chess_game (chess_game_id) on delete cascade
+);
+
 insert into chess_game (name, is_on, team_value_of_turn)
 values ('test', true, 'BLACK');
 
-insert into board(name, position_column_value, position_row_value, piece_name, piece_team_value)
-values ('test', 'a', 2, 'KING', 'BLACK');
+insert into board(chess_game_id, position_column_value, position_row_value, piece_name, piece_team_value)
+values (1, 'a', 2, 'KING', 'BLACK');
 
-insert into board(name, position_column_value, position_row_value, piece_name, piece_team_value)
-values ('test', 'b', 3, 'QUEEN', 'WHITE');
+insert into board(chess_game_id, position_column_value, position_row_value, piece_name, piece_team_value)
+values (1, 'b', 3, 'QUEEN', 'WHITE');

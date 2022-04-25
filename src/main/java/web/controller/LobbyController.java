5e8689dd2b69cc9854b082f6c9127ad90b4628b9
정진혁 <wilgur513@naver.com>
2022-04-25 20:58:41 +0900

    [Spring 체스 - 1단계] 베루스(정진혁) 미션 제출합니다. (#390)

---
+++ src/main/java/web/controller/LobbyController.java
@@ -0,0 +1,36 @@
+package web.controller;
+
+import chess.Score;
+import chess.piece.Color;
+import java.math.BigDecimal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.ChessGameDao;
+import web.dto.GameStatus;
+
+
+@Controller
+public class LobbyController {
+
+    private final ChessGameDao chessGameDao;
+
+    public LobbyController(ChessGameDao chessGameDao) {
+        this.chessGameDao = chessGameDao;
+    }
+
+    @GetMapping("/")
+    public String lobby(Model model) {
+        model.addAttribute("chess-games", chessGameDao.findAll());
+        return "index";
+    }
+
+    @PostMapping("/create-chess-game")
+    public String createChessGame(@RequestParam String name) {
+        Score initialScore = new Score(new BigDecimal("38.0"));
+        chessGameDao.saveChessGame(name, GameStatus.READY, Color.WHITE, initialScore, initialScore);
+        return "redirect:/";
+    }
+}

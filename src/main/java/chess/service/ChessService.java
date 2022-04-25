e487604b2f01fe2b2fc40cd87dd869a7f7617f42
nailseong <nnis9857@gmail.com>
2022-04-26 00:58:30 +0900

    refactor: ChessService 로직 개선

--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -20,7 +20,6 @@ import chess.dto.response.CurrentTurnDto;
 import chess.dto.response.RoomStatusDto;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
@@ -36,21 +35,36 @@ public class ChessService {
     }
 
     public List<ChessPieceDto> findAllPiece(final int roomId) {
-        checkRoomExist(roomId);
-        return chessPieceDao.findAllByRoomId(roomId);
+        final List<ChessPieceDto> pieceDtos = chessPieceDao.findAllByRoomId(roomId);
+        if (pieceDtos.isEmpty()) {
+            throw new IllegalArgumentException("기물이 존재하지 않습니다.");
+        }
+        return pieceDtos;
     }
 
     public void initPiece(final int roomId) {
-        checkRoomExist(roomId);
-        final ChessGame chessGame = findGameByRoomId(roomId);
+        final boolean roomExist = roomDao.isExistId(roomId);
+        if (roomExist) {
+            throw new IllegalArgumentException("기물이 이미 생성 되었습니다.");
+        }
+        final Map<Position, ChessPiece> pieceByPosition = ChessBoardFactory.createInitPieceByPosition();
+        final ChessGame chessGame = new ChessGame(new ChessBoard(pieceByPosition));
 
         final StartResult startResult = chessGame.start();
         updateChessPiece(roomId, startResult.getPieceByPosition());
         updateRoomStatusTo(roomId, GameStatus.PLAYING);
     }
 
+    private void updateChessPiece(final int roomId, final Map<Position, ChessPiece> pieceByPosition) {
+        chessPieceDao.deleteAllByRoomId(roomId);
+        chessPieceDao.saveAll(roomId, pieceByPosition);
+    }
+
+    private void updateRoomStatusTo(final int roomId, final GameStatus gameStatus) {
+        roomDao.updateStatusById(roomId, gameStatus);
+    }
+
     public MoveResult move(final int roomId, MoveRequestDto requestDto) {
-        checkRoomExist(roomId);
         final ChessGame chessGame = findGameByRoomId(roomId);
         final Position from = requestDto.getFrom();
         final Position to = requestDto.getTo();
@@ -67,15 +81,17 @@ public class ChessService {
         chessPieceDao.updateByRoomIdAndPosition(roomId, from, to);
     }
 
+    private void updateRoom(final int roomId, final GameStatus gameStatus, final Color currentTurn) {
+        roomDao.updateById(roomId, gameStatus, currentTurn);
+    }
+
     public Score findScore(final int roomId) {
-        checkRoomExist(roomId);
         final ChessGame chessGame = findGameByRoomId(roomId);
 
         return chessGame.calculateScore();
     }
 
     public EndResult result(final int roomId) {
-        checkRoomExist(roomId);
         final ChessGame chessGame = findGameByRoomId(roomId);
 
         final EndResult result = chessGame.end();
@@ -84,24 +100,18 @@ public class ChessService {
         return result;
     }
 
-    private void checkRoomExist(final int roomId) {
-        if (!roomDao.isExistId(roomId)) {
-            throw new IllegalArgumentException("존재하지 않는 방 입니다.");
-        }
-    }
-
     private ChessGame findGameByRoomId(final int roomId) {
-        Map<Position, ChessPiece> pieceByPosition = initAllPiece(roomId);
-        Color currentTurn = initCurrentTurn(roomId);
-        GameStatus gameStatus = initGameStatus(roomId);
+        Map<Position, ChessPiece> pieceByPosition = findPieceByPosition(roomId);
+        Color currentTurn = findCurrentTurn(roomId);
+        GameStatus gameStatus = findGameStatus(roomId);
 
         return new ChessGame(new ChessBoard(pieceByPosition, currentTurn), gameStatus);
     }
 
-    private Map<Position, ChessPiece> initAllPiece(final int roomId) {
+    private Map<Position, ChessPiece> findPieceByPosition(final int roomId) {
         final List<ChessPieceDto> dtos = chessPieceDao.findAllByRoomId(roomId);
         if (dtos.isEmpty()) {
-            return ChessBoardFactory.createInitPieceByPosition();
+            throw new IllegalArgumentException("방이 존재하지 않습니다.");
         }
 
         return dtos.stream()
@@ -112,32 +122,13 @@ public class ChessService {
                 ));
     }
 
-    private Color initCurrentTurn(final int roomId) {
+    private Color findCurrentTurn(final int roomId) {
         final CurrentTurnDto dto = roomDao.findCurrentTurnById(roomId);
-        if (Objects.isNull(dto)) {
-            return Color.WHITE;
-        }
         return dto.getCurrentTurn();
     }
 
-    private GameStatus initGameStatus(final int roomId) {
+    private GameStatus findGameStatus(final int roomId) {
         final RoomStatusDto dto = roomDao.findStatusById(roomId);
-        if (Objects.isNull(dto)) {
-            return GameStatus.READY;
-        }
         return dto.getGameStatus();
-    }
-
-    private void updateChessPiece(final int roomId, final Map<Position, ChessPiece> pieceByPosition) {
-        chessPieceDao.deleteAllByRoomId(roomId);
-        chessPieceDao.saveAll(roomId, pieceByPosition);
-    }
-
-    private void updateRoom(final int roomId, final GameStatus gameStatus, final Color currentTurn) {
-        roomDao.updateById(roomId, gameStatus, currentTurn);
-    }
-
-    private void updateRoomStatusTo(final int roomId, final GameStatus gameStatus) {
-        roomDao.updateStatusById(roomId, gameStatus);
     }
 }

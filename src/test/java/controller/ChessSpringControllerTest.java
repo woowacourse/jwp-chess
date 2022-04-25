b59ef08b7a22a29b192f4b1c5eb3e1ebbcd3f653
jaejae-yoo <wotj102@gmail.com>
2022-04-25 15:26:09 +0900

    test: 종료 요청 테스트 추가

--- src/test/java/controller/ChessSpringControllerTest.java
+++ src/test/java/controller/ChessSpringControllerTest.java
@@ -4,9 +4,7 @@ package controller;
 import chess.SpringChessApplication;
 import chess.dto.MoveDto;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -17,6 +15,7 @@ import io.restassured.RestAssured;
 import static org.hamcrest.core.Is.is;
 
 @SpringBootTest(classes = SpringChessApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ChessSpringControllerTest {
 
     @LocalServerPort
@@ -29,6 +28,7 @@ public class ChessSpringControllerTest {
 
     @DisplayName("Board - GET")
     @Test
+    @Order(1)
     void getBoard() {
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -40,6 +40,7 @@ public class ChessSpringControllerTest {
 
     @DisplayName("move - POST")
     @Test
+    @Order(2)
     void move() {
         MoveDto moveDto = new MoveDto("f2", "f3");
         RestAssured.given().log().all()
@@ -53,6 +54,7 @@ public class ChessSpringControllerTest {
 
     @DisplayName("status - GET")
     @Test
+    @Order(3)
     void status() {
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -64,10 +66,23 @@ public class ChessSpringControllerTest {
 
     @DisplayName("reset - POST")
     @Test
+    @Order(4)
     void reset() {
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/reset")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(2));
+    }
+
+    @DisplayName("end - POST")
+    @Test
+    @Order(5)
+    void end() {
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/end")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
                 .body("size()", is(2));

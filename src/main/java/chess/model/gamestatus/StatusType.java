68ceac68849c3499b47151cf51b9faada0055be8
yangdongjue5510 <djwhy5510@gmail.com>
2022-04-25 14:12:36 +0900

    refactor: 상태 찾는 로직 리팩토링

--- src/main/java/chess/model/gamestatus/StatusType.java
+++ src/main/java/chess/model/gamestatus/StatusType.java
@@ -17,7 +17,7 @@ public enum StatusType {
 
     public static Status createStatus(String statusName, Board board) {
         return Arrays.stream(values())
-                .filter(type -> type.name().equals(statusName.toUpperCase()))
+                .filter(type -> type.hasSameName(statusName))
                 .findFirst()
                 .map(type -> type.statusFunction.apply(board))
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 상태입니다."));
@@ -25,13 +25,12 @@ public enum StatusType {
 
     public static StatusType findByStatus(Status status) {
         return Arrays.stream(values())
-                .filter(type -> type.hasSameClassName(status))
+                .filter(type -> type.hasSameName(status.getClass().getSimpleName()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("맞는 상태 타입을 찾지 못했습니다."));
     }
 
-    private boolean hasSameClassName(Status status) {
-        String className = status.getClass().getSimpleName().toUpperCase();
-        return this.name().equals(className);
+    private boolean hasSameName(String compareName) {
+        return this.name().equals(compareName.toUpperCase());
     }
 }

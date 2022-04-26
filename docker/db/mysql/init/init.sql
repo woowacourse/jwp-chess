d9b90dabac8ca883836ab6817bf4b818708df31e
kth990303 <kth990303@naver.com>
2022-04-26 15:43:09 +0900

    feat: 체스룸 구현에 따른 sql문 추가 구현

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,11 +1,27 @@
-CREATE TABLE turn (
-    team varchar(5) not null primary key
+CREATE TABLE room
+(
+    id       long        not null auto_increment primary key,
+    name     varchar(30) not null,
+    password varchar(20) not null
 );
 
-insert into turn (team) values ('WHITE');
+CREATE TABLE turn
+(
+    id     long       not null auto_increment primary key,
+    roomId long       not null,
+    team   varchar(5) not null,
+    constraint fk_roomId foreign key (roomId) references room (id) on delete cascade
+);
 
-CREATE TABLE piece (
-    position varchar(3) not null primary key,
-    name varchar(2) not null,
-    team varchar(5) not null
+insert into turn (team)
+values ('WHITE');
+
+CREATE TABLE piece
+(
+    id       long       not null auto_increment primary key,
+    roomId   long       not null,
+    position varchar(3) not null,
+    name     varchar(2) not null,
+    team     varchar(5) not null,
+    constraint fk_roomId foreign key (roomId) references room (id) on delete cascade
 );

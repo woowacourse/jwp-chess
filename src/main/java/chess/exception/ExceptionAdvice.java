3ec9654aed7408ebcbe458a24c73fad4bd7d3663
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-27 00:38:04 +0900

    refactor: reset 기능 오류 수정

--- src/main/java/chess/exception/ExceptionAdvice.java
+++ src/main/java/chess/exception/ExceptionAdvice.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler
-    public ResponseEntity<String> getException(final Exception exception) {
-        return ResponseEntity.internalServerError().body(exception.getMessage());
-    }
+//    @ExceptionHandler
+//    public ResponseEntity<String> getException(final Exception exception) {
+//        return ResponseEntity.internalServerError().body(exception.getMessage());
+//    }
 }

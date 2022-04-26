55095af8066a42165f8b4f9952a3715fa0a4a2c0
tonic523 <diddmlvkf@gmail.com>
2022-04-26 13:45:14 +0900

    feat: 체스판 상태를 관리하는 Service 계층 구현

--- src/main/java/chess/application/web/controller/ChessController.java
+++ src/main/java/chess/application/web/controller/ChessController.java
@@ -1,10 +1,9 @@
 package chess.application.web.controller;
 
-import chess.application.web.dao.CommandDao;
 import chess.application.web.dto.StateDto;
 import chess.application.web.dto.StatusDto;
+import chess.application.web.service.StateService;
 import chess.game.Player;
-import chess.state.Start;
 import chess.state.State;
 
 import org.springframework.stereotype.Controller;
@@ -17,10 +16,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class ChessController {
 
-    private final CommandDao commandDao;
+    private final StateService stateService;
 
-    public ChessController(CommandDao commandDao) {
-        this.commandDao = commandDao;
+    public ChessController(StateService stateService) {
+        this.stateService = stateService;
     }
 
     @GetMapping("/")
@@ -31,27 +30,28 @@ public class ChessController {
 
     @PostMapping("/start")
     public String startChessGame(RedirectAttributes redirectAttributes) {
-        commandDao.clear();
+        stateService.initCommandRecord();
         redirectAttributes.addAttribute("message", "게임을 시작합니다.");
         return "redirect:game";
     }
 
     @GetMapping(path = "/game")
     public ModelAndView printCurrentBoard(@RequestParam("message") String message) {
-        State state = currentState();
+        State state = stateService.currentState();
         StateDto stateDto = StateDto.of(state);
         ModelAndView modelAndView = new ModelAndView(getViewName(state));
         modelAndView.addObject("squares", stateDto.getSquares());
         modelAndView.addObject("player", stateDto.getPlayer());
-        modelAndView.addObject("commands", commandDao.findAll());
+        modelAndView.addObject("commands", stateService.getAllCommand());
         modelAndView.addObject("message", message);
         return modelAndView;
     }
 
     @PostMapping(path = "/game")
     public String movePiece(RedirectAttributes redirectAttributes, @RequestParam("command") String command) {
-        currentState().proceed(command);
-        commandDao.insert(command);
+        stateService.currentState()
+                .proceed(command);
+        stateService.insertCommand(command);
         redirectAttributes.addAttribute("message", "실행한 명령어: " + command);
         return "redirect:game";
     }
@@ -59,19 +59,11 @@ public class ChessController {
     @GetMapping(path = "/result")
     public ModelAndView printResult() {
         ModelAndView modelAndView = new ModelAndView("status");
-        StatusDto statusDto = StatusDto.of(currentState().proceed("status"));
+        StatusDto statusDto = StatusDto.of(stateService.currentState().proceed("status"));
         modelAndView.addObject("squares", statusDto.getSquares());
         modelAndView.addObject("whiteScore", statusDto.getScore(Player.WHITE));
         modelAndView.addObject("blackScore", statusDto.getScore(Player.BLACK));
         return modelAndView;
-    }
-
-    private State currentState() {
-        State state = Start.of();
-        for (String command : commandDao.findAll()) {
-            state = state.proceed(command);
-        }
-        return state;
     }
 
     private String getViewName(State state) {

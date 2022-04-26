193249cb736cc593e476f5ed9831a99f51ca855f
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-26 16:13:18 +0900

    refactor: console, spark관련 클래스 삭제

--- src/main/java/chess/exception/ExceptionAdvice.java
+++ src/main/java/chess/exception/ExceptionAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler()
+    @ExceptionHandler
     public ResponseEntity<String> getException(final Exception exception) {
         return ResponseEntity.internalServerError().body(exception.getMessage());
     }

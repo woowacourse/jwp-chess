bd68a2714b795c2589df1d0b28c71b8ea54222d4
chawani <60432062+chawani@users.noreply.github.com>
2022-04-26 19:19:48 +0900

    feat: 체스 방 관련 view 구현
    - 필요없는 파일 삭제

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -2,13 +2,29 @@ package chess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/game")
+@RequestMapping("/")
 public class ChessController {
 
-    @GetMapping
+    @GetMapping()
+    public String chess() {
+        return "chess";
+    }
+
+    @PostMapping("/write")
+    public String write() {
+        return "write";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
+
+    @GetMapping("/game")
     public String board() {
         return "board";
     }

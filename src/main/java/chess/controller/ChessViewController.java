b52acb15e740881834dbea3f3154d51474ddf498
sure-why-not <pkse1213@gmail.com>
2022-04-26 20:09:39 +0900

    refactor: 필요하지 않은 코드 삭제

--- src/main/java/chess/controller/ChessViewController.java
+++ src/main/java/chess/controller/ChessViewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/chess-game")
 public class ChessViewController {
 
-    @GetMapping()
+    @GetMapping
     public String init() {
         return "index";
     }

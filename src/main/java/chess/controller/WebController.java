c923b0965653021a9d0b89588fb94213454833c8
Ohzzi <clover7even@naver.com>
2022-04-27 00:51:47 +0900

    test: WebController 테스트 시 Present Layer만 스캔하도록 변경

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -2,8 +2,6 @@ package chess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebController {
@@ -11,11 +9,6 @@ public class WebController {
     @GetMapping("/")
     public String index() {
         return "index.html";
-    }
-
-    @GetMapping("/game")
-    public RedirectView redirectGame(@RequestParam("gameId") String gameId) {
-        return new RedirectView("/game/" + gameId);
     }
 
     @GetMapping("/game/{gameId}")
--- src/test/java/chess/controller/WebControllerTest.java
+++ src/test/java/chess/controller/WebControllerTest.java
@@ -1,56 +1,33 @@
 package chess.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(WebController.class)
 class WebControllerTest {
 
-    @LocalServerPort
-    int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
     @DisplayName("초기 화면 테스트")
     @Test
-    void index() {
-        RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .contentType(MediaType.TEXT_HTML_VALUE)
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @DisplayName("게임 redirect 테스트")
-    @Test
-    void redirect() {
-        assertThat(RestAssured.given().redirects().follow(false).log().all()
-                .when().get("/game/?gameId=1")
-                .then().log().all()
-                .statusCode(302)
-                .extract()
-                .header("Location")
-                .contains("/game/1")).isTrue();
+    void index() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+                        .accept(MediaType.TEXT_HTML_VALUE))
+                .andExpect(MockMvcResultMatchers.view().name("index.html"));
     }
 
     @DisplayName("게임 방 입장 테스트")
     @Test
-    void enterGame() {
-        RestAssured.given().log().all()
-                .when().get("/game/1")
-                .then().log().all()
-                .contentType(MediaType.TEXT_HTML_VALUE)
-                .statusCode(HttpStatus.OK.value());
+    void enterGame() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/game/1")
+                        .accept(MediaType.TEXT_HTML_VALUE))
+                .andExpect(MockMvcResultMatchers.view().name("/game.html"));
     }
 }

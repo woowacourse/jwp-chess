4477efffcae9161352e6cdfc572454c47e5c5a52
beomWhale <cndqjacndqja@gmail.com>
2022-04-26 18:54:38 +0900

    refactor: handle -> Exception과 IllegalArgumentException으로 수정

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,9 +1,16 @@
 package chess.controller;
 
-import chess.dto.*;
+import chess.dto.BoardDto;
+import chess.dto.GameStateDto;
+import chess.dto.MoveDto;
+import chess.dto.ScoreDto;
 import chess.service.ChessService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChessController {
@@ -39,8 +46,13 @@ public class ChessController {
         return ResponseEntity.ok().body(chessService.end(ROOM_ID));
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handle(RuntimeException exception) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> IllegalArgumentExceptionHandle(RuntimeException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> ExceptionHandle() {
+        return ResponseEntity.badRequest().body("실행 중 예상치 못한 오류가 발생했습니다.");
     }
 }

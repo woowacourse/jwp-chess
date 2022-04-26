9d25d84037bddc42c099510eb4c78a9b4e77d388
bomi <bonita9931@gmail.com>
2022-04-26 17:11:41 +0900

    fix: db 스키마 수정

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,16 +1,14 @@
-CREATE TABLE games
+CREATE TABLE turns
 (
-    game_id  int        not null AUTO_INCREMENT,
+    turn_id  int        not null AUTO_INCREMENT,
     turn     varchar(5) not null,
-    primary key (game_id)
+    primary key (turn_id)
 );
 
 CREATE TABLE pieces
 (
     piece_id int         not null AUTO_INCREMENT,
-    game_id int not null,
     position varchar(4)  not null,
     name    varchar(10) not null,
-    primary key (piece_id),
-    foreign key(game_id) references GAMES(game_id)
+    primary key (piece_id)
 );

bd513dfb8232b4e1e705fba9c46cebb6537c333f
injoon2019 <ison@sfu.ca>
2022-04-26 16:56:52 +0900

    refactor: 테이블 설계 변경

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,12 +1,26 @@
-create table board
+create table room
 (
-    position varchar(2) not null,
-    piece varchar(10) not null,
-    primary key (position)
+    id int(10) NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    color varchar(5) NOT NULL,
+    primary key (id)
 );
 
-create table player
+create table board
 (
-    color varchar(5) not null,
-    primary key (color)
+    board_id int(10) NOT NULL AUTO_INCREMENT,
+    position varchar(2) NOT NULL,
+    piece varchar(10) NOT NULL,
+    primary key (board_id),
+    foreign key (board_id) references room (id)
 );
+
+
+--create table board
+--(
+--    board_id int(10) NOT NULL AUTO_INCREMENT,
+--    position varchar(2) NOT NULL,
+--    piece varchar(10) NOT NULL,
+--    primary key (board_id)
+--    foreign key 'board_id' references 'room' ('id')
+--);

189787f169b0e79ea4313324747e83446a122648
skullKim <kyk990328@gmail.com>
2022-04-26 15:27:54 +0900

    refactor(init.sql): 테이블 컬럼 변경
    게임 방에 비밀번호 적용을 위해 테이블 컬럼 변경
    
    BREAKING CHANGE: game 테이블에 name과 password 컬럼 변경
    as-is:
    CREATE TABLE game
    (
        game_id int         NOT NULL PRIMARY KEY,
        state   varchar(20) NOT NULL
    );
    
    CREATE TABLE piece
    (
        piece_id   int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
        game_id    INT         NOT NULL,
        position   VARCHAR(10) NOT NULL,
        piece_type VARCHAR(10) NOT NULL,
        color      VARCHAR(10) NOT NULL,
        FOREIGN KEY (game_id)
            REFERENCES game (game_id) ON DELETE CASCADE
    );
    
    to-be:
    CREATE TABLE game (
        id INT AUTO_INCREMENT NOT NULL,
        name VARCHAR(20) NOT NULL,
        password VARCHAR(20) NOT NULL,
        state VARCHAR(20) NOT NULL,
        PRIMARY KEY (id)
    );
    
    CREATE TABLE piece (
        id INT AUTO_INCREMENT NOT NULL,
        position  VARCHAR(10) NOT NULL,
        piece_type VARCHAR(10) NOT NULL,
        color VARCHAR(10) NOT NULL,
        game_id INT NOT NULL,
        PRIMARY KEY (id),
        FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
    );

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,16 +1,17 @@
-CREATE TABLE game
-(
-    game_id int         NOT NULL PRIMARY KEY,
-    state   varchar(20) NOT NULL
+CREATE TABLE game (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE piece
-(
-    piece_id   int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    game_id    INT         NOT NULL,
-    position   VARCHAR(10) NOT NULL,
+CREATE TABLE piece (
+    id INT AUTO_INCREMENT NOT NULL,
+    position  VARCHAR(10) NOT NULL,
     piece_type VARCHAR(10) NOT NULL,
-    color      VARCHAR(10) NOT NULL,
-    FOREIGN KEY (game_id)
-        REFERENCES game (game_id) ON DELETE CASCADE
+    color VARCHAR(10) NOT NULL,
+    game_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
 );

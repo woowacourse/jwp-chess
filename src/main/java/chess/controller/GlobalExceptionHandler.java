b3d819457469347ddf668a4c22f840dcbb86a4c8
nailseong <nnis9857@gmail.com>
2022-04-26 23:14:27 +0900

    feat: 예기치 못한 예외 처리 로직 추가

--- src/main/java/chess/controller/GlobalExceptionHandler.java
+++ src/main/java/chess/controller/GlobalExceptionHandler.java
@@ -2,6 +2,10 @@ package chess.controller;
 
 import chess.dto.response.ErrorResponseDto;
 import chess.exception.NotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import org.joda.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler({IllegalArgumentException.class, DataAccessException.class,
             InvalidResultSetAccessException.class})
@@ -22,5 +28,22 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleNotFound(final NotFoundException e) {
         final ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
         return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ErrorResponseDto> handleUnexpectedException(final Exception e,
+                                                                      final HttpServletRequest request) {
+        logger.error(createLogMessage(request), e);
+        return ResponseEntity.internalServerError().body(new ErrorResponseDto("예기치 못한 문제가 발생했습니다."));
+    }
+
+    private String createLogMessage(final HttpServletRequest request) {
+        final String message = "[" + new LocalDateTime() + "] "
+                + request.getMethod() + " "
+                + request.getRequestURI();
+        if (request.getQueryString() == null) {
+            return message;
+        }
+        return message + "?" + request.getQueryString();
     }
 }

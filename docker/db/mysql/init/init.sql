ededdeaad8122abd07385e6df7a1f2c96b27e3df
brorae <jhr3239@gmail.com>
2022-04-26 20:03:27 +0900

    refactor: init.sql 순서 변경

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,3 +1,10 @@
+create table game
+(
+    id    int auto_increment
+        primary key,
+    state varchar(20) not null
+);
+
 create table board
 (
     position varchar(5)  not null,
@@ -8,11 +15,4 @@ create table board
     constraint board_game_id_fk
         foreign key (game_id) references game (id)
             on update cascade on delete cascade
-);
-
-create table game
-(
-    id    int auto_increment
-        primary key,
-    state varchar(20) not null
 );

ad1add2c616b45cddcd2628fb0ceadd16690cbf1
DOES <ldk980130@gmail.com>
2022-04-26 21:14:30 +0900

    fix: 방을 삭제하면 방 목록을 접도록 수정

--- src/main/java/chess/web/HomeController.java
+++ src/main/java/chess/web/HomeController.java
@@ -8,7 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String login() {
-        System.out.println("###############HOME################");
         return "login.html";
     }
 }

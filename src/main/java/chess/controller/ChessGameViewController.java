d80f75ca80ac38b31cee296dfc967512fa9ea622
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-26 17:25:11 +0900

    feat: 프론트 현재 게임 목록 반환 및 게임 삭제 기능 추가

--- src/main/java/chess/controller/ChessGameViewController.java
+++ src/main/java/chess/controller/ChessGameViewController.java
@@ -1,13 +1,30 @@
 package chess.controller;
 
+import chess.controller.dto.response.ChessGameRoomTitleResponse;
+import chess.service.ChessGameRoomService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ChessGameViewController {
 
+    private final ChessGameRoomService chessGameRoomService;
+
+    public ChessGameViewController(final ChessGameRoomService chessGameRoomService) {
+        this.chessGameRoomService = chessGameRoomService;
+    }
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("chess_game_list");
+        List<ChessGameRoomTitleResponse> chessGameRoomTitleResponses = chessGameRoomService.findAllChessGameRooms()
+                .stream()
+                .map(ChessGameRoomTitleResponse::from)
+                .collect(Collectors.toList());
+        modelAndView.addObject("chessGames", chessGameRoomTitleResponses);
+        return modelAndView;
     }
 }

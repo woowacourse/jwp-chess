f3b448570e7f71708c8971d0c339d815eb862c9c
ulimy <ulim130@gmail.com>
2022-04-28 00:35:28 +0900

    refactor : 불필요한 어노테이션 제거

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -2,10 +2,8 @@ package chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

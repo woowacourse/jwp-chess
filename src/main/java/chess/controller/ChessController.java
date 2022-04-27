80965e7444e30bdbccb9eb9f0b6ce2652ffc7797
sure-why-not <pkse1213@gmail.com>
2022-04-27 16:05:58 +0900

    refactor: @Autowired 삭제

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -5,7 +5,6 @@ import chess.dto.ChessResponseDto;
 import chess.dto.MoveCommandDto;
 import chess.dto.ScoresDto;
 import chess.serviece.ChessService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;

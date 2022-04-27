ce4367b28300987015659128ca92085041207d3e
summerlunaa <summerlunaa9@gmail.com>
2022-04-27 20:57:00 +0900

    feat: 새로운 요구사항에 맞춰 controller, service 수정

--- src/main/java/chess/web/controller/ChessController.java
+++ src/main/java/chess/web/controller/ChessController.java
@@ -1,14 +1,20 @@
 package chess.web.controller;
 
+import chess.domain.state.StateType;
+import chess.web.dto.DeleteGameRequestDto;
 import chess.web.dto.MovePositionsDto;
 import chess.web.dto.MoveResultDto;
 import chess.web.service.ChessService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChessController {
@@ -19,24 +25,54 @@ public class ChessController {
         this.chessService = chessService;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String showIndex(final Model model) {
-        model.addAttribute("chessStatus", chessService.getChessStatus());
+        model.addAttribute("games", chessService.getAllGame());
+        return "index2";
+    }
+
+    @PostMapping("/")
+    public String createGame(@RequestParam String title, @RequestParam String password) {
+        int gameId = chessService.newGame(title, password);
+        return "redirect:/game/" + gameId;
+    }
+
+    @PostMapping("/delete")
+    public String deleteGame(@RequestBody DeleteGameRequestDto deleteGameRequestDto) {
+        chessService.deleteGame(deleteGameRequestDto);
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handle(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @GetMapping("/game/{gameId}")
+    public String showGame(@PathVariable int gameId, final Model model) {
+        if (chessService.getStateType(gameId) == StateType.END) {
+            return "redirect:/game/" + gameId + "/result";
+        }
+        model.addAttribute("chessStatus", chessService.getChessStatus(gameId));
 
         return "index";
     }
 
-    @ResponseBody
-    @PostMapping("/move")
-    public MoveResultDto movePiece(@RequestBody MovePositionsDto movePositionsDto) {
-        return chessService.getMoveResult(movePositionsDto);
+    @PatchMapping("/game/{gameId}/move")
+    public ResponseEntity<MoveResultDto> move(@PathVariable int gameId,
+                                              @RequestBody MovePositionsDto movePositionsDto) {
+        return ResponseEntity.ok(chessService.getMoveResult(gameId, movePositionsDto));
     }
 
-    @GetMapping("/result")
-    public String showResult(final Model model) {
-        model.addAttribute("result", chessService.getChessResult());
-        chessService.restart();
-
+    @GetMapping("/game/{gameId}/result")
+    public String showResult(@PathVariable int gameId, final Model model) {
+        model.addAttribute("result", chessService.getChessResult(gameId));
         return "result";
+    }
+
+    @GetMapping("/game/{gameId}/restart")
+    public String restartGame(@PathVariable int gameId) {
+        chessService.start(gameId);
+        return "redirect:/game/" + gameId;
     }
 }
--- src/main/java/chess/web/service/ChessService.java
+++ src/main/java/chess/web/service/ChessService.java
@@ -8,10 +8,12 @@ import chess.domain.generator.InitBoardGenerator;
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 import chess.domain.state.StateType;
-import chess.web.dao.BoardStateDao;
+import chess.web.dao.GameDao;
 import chess.web.dao.PieceDao;
 import chess.web.dto.ChessResultDto;
 import chess.web.dto.ChessStatusDto;
+import chess.web.dto.DeleteGameRequestDto;
+import chess.web.dto.GameResponseDto;
 import chess.web.dto.MovePositionsDto;
 import chess.web.dto.MoveResultDto;
 import chess.web.dto.PieceDto;
@@ -25,57 +27,91 @@ public class ChessService {
     private static final int BOARD_START_INDEX = 0;
     private static final int BOARD_END_INDEX = 7;
 
-    private final BoardStateDao boardStateDao;
+    private final GameDao gameDao;
     private final PieceDao pieceDao;
 
-    public ChessService(BoardStateDao boardStateDao, PieceDao pieceDao) {
-        this.boardStateDao = boardStateDao;
+    public ChessService(GameDao gameDao, PieceDao pieceDao) {
+        this.gameDao = gameDao;
         this.pieceDao = pieceDao;
     }
 
-    private void initChessBoard(Board board) {
+    public int newGame(String title, String password) {
+        int gameId = gameDao.save(title, password, StateType.READY);
+        start(gameId);
+        return gameId;
+    }
+
+    public void start(int gameId) {
+        gameDao.updateStateById(gameId, StateType.WHITE_TURN);
+        ChessBoard chessBoard = new ChessBoard(new InitBoardGenerator());
+        initChessBoard(chessBoard.getBoard(), gameId);
+    }
+
+    private void initChessBoard(Board board, int gameId) {
         for (int rankIndex = BOARD_START_INDEX; rankIndex <= BOARD_END_INDEX; rankIndex++) {
-            initOneRank(board, rankIndex);
+            initOneRank(board, rankIndex, gameId);
         }
     }
 
-    private void initOneRank(Board board, int rankIndex) {
+    private void initOneRank(Board board, int rankIndex, int gameId) {
         for (int fileIndex = BOARD_START_INDEX; fileIndex <= BOARD_END_INDEX; fileIndex++) {
             Position position = new Position(fileIndex, rankIndex);
             Piece piece = board.findPiece(position);
-            pieceDao.save(new PieceDto(piece, position));
+            pieceDao.save(gameId, new PieceDto(piece, position));
         }
     }
 
-    public void restart() {
-        end();
-        start();
+    public List<GameResponseDto> getAllGame() {
+        return gameDao.findAll();
     }
 
-    private void end() {
-        boardStateDao.deleteAll();
-        pieceDao.deleteAll();
+    public void deleteGame(DeleteGameRequestDto deleteGameRequestDto) {
+        int gameId = deleteGameRequestDto.getGameId();
+        String password = deleteGameRequestDto.getPassword();
+        String realPassword = gameDao.findPasswordById(gameId);
+
+        if (!realPassword.equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 잘못되었습니다.");
+        }
+
+        if (getChessGame(gameId).isRunning()) {
+            throw new IllegalArgumentException("게임이 아직 진행중입니다.");
+        }
+
+        gameDao.deleteGameById(gameId);
     }
 
-    private void start() {
-        boardStateDao.save(StateType.WHITE_TURN);
-
-        ChessBoard chessBoard = new ChessBoard(new InitBoardGenerator());
-        initChessBoard(chessBoard.getBoard());
+    private ChessGame getChessGame(int gameId) {
+        PiecesDto piecesDto = new PiecesDto(getPieces(gameId));
+        ChessBoard chessBoard = new ChessBoard(piecesDto.toBoard());
+        return new ChessGame(getStateType(gameId).newState(chessBoard));
     }
 
-    public ChessStatusDto getChessStatus() {
-        return new ChessStatusDto(new PiecesDto(getPieces()), getScore(Color.BLACK), getScore(Color.WHITE));
+    private List<PieceDto> getPieces(int gameId) {
+        return pieceDao.findAllByGameId(gameId);
     }
 
-    public MoveResultDto getMoveResult(MovePositionsDto movePositionsDto) {
-        ChessGame chessGame = getChessGame();
+    public StateType getStateType(int gameId) {
+        return gameDao.findStateById(gameId);
+    }
+
+    public ChessStatusDto getChessStatus(int gameId) {
+        return new ChessStatusDto(gameId, new PiecesDto(getPieces(gameId)), getScore(gameId, Color.BLACK),
+                getScore(gameId, Color.WHITE));
+    }
+
+    private double getScore(int gameId, Color color) {
+        return getChessGame(gameId).score(color);
+    }
+
+    public MoveResultDto getMoveResult(int gameId, MovePositionsDto movePositionsDto) {
+        ChessGame chessGame = getChessGame(gameId);
 
         try {
             chessGame.move(movePositionsDto.getSource(), movePositionsDto.getTarget());
             Position sourcePosition = new Position(movePositionsDto.getSource());
             Position targetPosition = new Position(movePositionsDto.getTarget());
-            move(chessGame, sourcePosition, targetPosition);
+            move(gameId, chessGame, sourcePosition, targetPosition);
         } catch (Exception ex) {
             return new MoveResultDto(400, ex.getMessage(), chessGame.isFinished());
         }
@@ -83,46 +119,28 @@ public class ChessService {
         return new MoveResultDto(200, "", chessGame.isFinished());
     }
 
-    private void move(ChessGame chessGame, Position target, Position source) {
-        boardStateDao.update(chessGame.getStateType());
-        pieceDao.update(new PieceDto(chessGame.board().findPiece(target), target));
-        pieceDao.update(new PieceDto(chessGame.board().findPiece(source), source));
+    private void move(int gameId, ChessGame chessGame, Position target, Position source) {
+        gameDao.updateStateById(gameId, chessGame.getStateType());
+        pieceDao.updateByGameId(gameId, new PieceDto(chessGame.board().findPiece(target), target));
+        pieceDao.updateByGameId(gameId, new PieceDto(chessGame.board().findPiece(source), source));
     }
 
-    public ChessResultDto getChessResult() {
-        ChessGame chessGame = getChessGame();
+    public ChessResultDto getChessResult(int gameId) {
+        ChessGame chessGame = getChessGame(gameId);
         endGame(chessGame);
-        return new ChessResultDto(getScore(Color.BLACK), getScore(Color.WHITE), chessGame.result());
+
+        double blackScore = getScore(gameId, Color.BLACK);
+        double whiteScore = getScore(gameId, Color.WHITE);
+
+        pieceDao.deleteAllByGameId(gameId);
+        gameDao.updateStateById(gameId, StateType.END);
+
+        return new ChessResultDto(gameId, blackScore, whiteScore, chessGame.result());
     }
 
     private void endGame(ChessGame chessGame) {
         if (!chessGame.isFinished()) {
             chessGame.end();
         }
-    }
-
-    public ChessGame getChessGame() {
-        PiecesDto piecesDto = new PiecesDto(getPieces());
-        ChessBoard chessBoard = new ChessBoard(piecesDto.toBoard());
-        return new ChessGame(getStateType().newState(chessBoard));
-    }
-
-    private List<PieceDto> getPieces() {
-        return pieceDao.findAll();
-    }
-
-    private StateType getStateType() {
-        return boardStateDao.findState();
-    }
-
-    private double getScore(Color color) {
-        return getChessGame().score(color);
-    }
-
-    public boolean isNotRunning() {
-        if (getStateType() == null) {
-            return true;
-        }
-        return !getChessGame().isRunning();
     }
 }

4272e298debf9d3acfb162d716a5b9f6e1e9e1d3
pup-paw <wldusdl0310@gmail.com>
2022-04-27 20:17:56 +0900

    feat: 저장된 게임을 삭제하는 기능

--- src/main/java/chess/controller/ReadyController.java
+++ src/main/java/chess/controller/ReadyController.java
@@ -3,6 +3,8 @@ package chess.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import chess.service.ChessService;
 
@@ -18,5 +20,11 @@ public class ReadyController {
     public String index(Model model) {
         model.addAttribute("games", chessService.getGameIDs());
         return "ready";
+    }
+
+    @PostMapping("/")
+    public String delete(@RequestParam String gameID, Model model) {
+        chessService.deleteGameByGameID(gameID);
+        return index(model);
     }
 }

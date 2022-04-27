3467f735744f07016ac63773e846c6a410fc5fbf
hyeonic <dev.hyeonic@gmail.com>
2022-04-27 18:04:33 +0900

    docs: init.sql 수정

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,23 +1,20 @@
-USE
-chess;
+USE chess;
 
 CREATE TABLE room
 (
-    id           INT AUTO_INCREMENT,
+    id           INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name         VARCHAR(10) NOT NULL,
     password     VARCHAR(10) NOT NULL,
     game_status  VARCHAR(10) NOT NULL,
-    current_turn VARCHAR(10) NOT NULL,
-    PRIMARY KEY (name)
+    current_turn VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE chess_piece
 (
-    id          INT AUTO_INCREMENT,
+    id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     room_id     INT         NOT NULL,
     position    VARCHAR(10) NOT NULL,
     chess_piece VARCHAR(10) NOT NULL,
     color       VARCHAR(10) NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES Room (id) ON DELETE CASCADE
 );

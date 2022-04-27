4b12d4f20d3e8215e51468f76536173a16aaddc6
jayjaehunchoi <wogns0108@naver.com>
2022-04-28 07:50:27 +0900

    test : test 스프링 컨텍스트 최적화

--- src/test/java/chess/util/PasswordEncoderTest.java
+++ src/test/java/chess/util/PasswordEncoderTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PasswordEncoderTest {
 

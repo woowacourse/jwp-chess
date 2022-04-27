7af5797a07f7571d4d20223aa865686e4ee03bb4
ulimy <ulim130@gmail.com>
2022-04-28 02:18:59 +0900

    refactor : 부적절한 Runnable 제거

--- src/main/java/chess/controller/WebChessController.java
+++ src/main/java/chess/controller/WebChessController.java
@@ -5,6 +5,7 @@ import chess.dto.ScoreDto;
 import chess.service.ChessGameService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class WebChessController {
 
+    private static final String CHESS_GAME_URL = "chessGame";
+    private static final String START_URL = "start";
     private final ChessGameService service;
 
     public WebChessController(ChessGameService service) {
@@ -20,55 +23,62 @@ public class WebChessController {
 
     @GetMapping("/")
     public String main() {
-        return "start";
+        return START_URL;
     }
 
     @GetMapping("/game")
     public String game(Model model) {
-        return executeCommand(service::init, model);
+        service.init();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
     @GetMapping("/start")
     public String start(Model model) {
-        return executeCommand(service::start, model);
+        service.start();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
     @GetMapping("/end")
     public String end(Model model) {
-        return executeCommand(service::end, model);
+        service.end();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
     @GetMapping("/restart")
     public String restart(Model model) {
-        return executeCommand(service::restart, model);
+        service.restart();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
     @GetMapping("/save")
     public String save(Model model) {
-        return executeCommand(service::save, model);
+        service.save();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
     @GetMapping("status")
     public String status(Model model) {
-        return executeCommand(() -> {
-            ScoreDto score = service.status();
-            model.addAttribute("score", score);
-        }, model);
+        ScoreDto score = service.status();
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        model.addAttribute("score", score);
+        return CHESS_GAME_URL;
     }
 
     @PostMapping("/move")
     public String move(@ModelAttribute MoveRequestDto moveRequest, Model model) {
-        return executeCommand(() -> service.move(moveRequest.getFrom(), moveRequest.getTo()), model);
+        service.move(moveRequest.getFrom(), moveRequest.getTo());
+        model.addAttribute("pieces", service.getPiecesByUnicode());
+        return CHESS_GAME_URL;
     }
 
-    private String executeCommand(Runnable command, Model model) {
-        try {
-            command.run();
-            model.addAttribute("pieces", service.getPiecesByUnicode());
-        } catch (RuntimeException e) {
-            model.addAttribute("error", e.getMessage());
-        }
+    @ExceptionHandler(RuntimeException.class)
+    private String handelException(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
         return "chessGame";
     }
-
 }

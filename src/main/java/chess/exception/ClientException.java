bb3ef355f7b2ea0ad569a11bed65187cb355eb43
progress0407 <progress0407@gmail.com>
2022-04-28 22:10:53 +0900

    feat(ClientException): 사용하지 않는 생성자 소거

--- src/main/java/chess/exception/ClientException.java
+++ src/main/java/chess/exception/ClientException.java
@@ -2,23 +2,7 @@ package chess.exception;
 
 public class ClientException extends RuntimeException {
 
-    public ClientException() {
-        super();
-    }
-
     public ClientException(String message) {
         super(message);
-    }
-
-    public ClientException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ClientException(Throwable cause) {
-        super(cause);
-    }
-
-    protected ClientException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }

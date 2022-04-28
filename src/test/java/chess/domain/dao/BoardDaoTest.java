67a7c770c424edbdf4749548da6a24d3a8faf5a1
Juhyung990122 <harukawa99@naver.com>
2022-04-28 09:15:51 +0900

    refactor: 불필요한 import문 삭제

--- src/test/java/chess/domain/dao/BoardDaoTest.java
+++ src/test/java/chess/domain/dao/BoardDaoTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestConstructor;
 
 import java.util.List;
--- src/test/java/chess/domain/dao/GameDaoTest.java
+++ src/test/java/chess/domain/dao/GameDaoTest.java
@@ -7,7 +7,6 @@ import chess.domain.game.status.Playing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
 

0c9511e496782a04745f27162e5545cd7ebe7e39
RIANAEH <ririhan217@gmail.com>
2022-04-28 16:47:27 +0900

    refactor: 불필요한 import 제거

--- src/main/java/chess/controller/ChessWebController.java
+++ src/main/java/chess/controller/ChessWebController.java
@@ -2,7 +2,6 @@ package chess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ChessWebController {

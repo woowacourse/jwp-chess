2744b2cf0ecbba18beb5d6a16b0e55463b6d0e18
injoon2019 <ison@sfu.ca>
2022-04-28 23:38:36 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/chess/web/controller/ChessGameController.java
+++ src/main/java/chess/web/controller/ChessGameController.java
@@ -1,22 +1,9 @@
 package chess.web.controller;
 
-import chess.domain.piece.Piece;
-import chess.web.dto.CreateRoomRequestDto;
-import chess.web.dto.CreateRoomResultDto;
-import chess.web.dto.MoveDto;
-import chess.web.dto.MoveResultDto;
-import chess.web.dto.PlayResultDto;
-import chess.web.dto.ReadRoomResultDto;
-import chess.web.dto.StartResultDto;
 import chess.web.service.ChessGameService;
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ChessGameController {

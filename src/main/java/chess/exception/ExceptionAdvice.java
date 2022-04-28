71a9fedc64522461b1a557fa8f64ec9197e21871
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-28 16:50:03 +0900

    refactor: exception bad Request로 변경

--- src/main/java/chess/exception/ExceptionAdvice.java
+++ src/main/java/chess/exception/ExceptionAdvice.java
@@ -9,6 +9,6 @@ public class ExceptionAdvice {
 
     @ExceptionHandler
     public ResponseEntity<String> getException(final Exception exception) {
-        return ResponseEntity.internalServerError().body(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage() + "\n뒤로 가시면 됩니당.");
     }
 }

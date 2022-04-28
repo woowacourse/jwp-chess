3625706bc24cbf70abe86f3bb89eb1cde4f70426
jaejae-yoo <wotj102@gmail.com>
2022-04-29 03:02:08 +0900

    refactor: exception 상태 코드 수정

--- src/main/java/chess/controller/ChessGameControllerAdvice.java
+++ src/main/java/chess/controller/ChessGameControllerAdvice.java
@@ -1,6 +1,5 @@
 package chess.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,17 +9,12 @@ import java.sql.SQLException;
 @RestControllerAdvice
 public class ChessGameControllerAdvice {
 
-    @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<String> runTimeExceptionHandle(RuntimeException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-    }
-
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<String> IllegalArgumentExceptionHandle(IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
-    @ExceptionHandler({SQLException.class})
+    @ExceptionHandler({SQLException.class, NullPointerException.class})
     public ResponseEntity<String> DBExceptionHandle(SQLException exception) {
         return ResponseEntity.internalServerError().body(exception.getMessage());
     }

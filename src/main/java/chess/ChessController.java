8b88982bf23f9747e2241cea0a0988fd45399ab9
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-04-30 00:51:17 +0900

    refactor: static resource 반환 컨트롤러 분리

--- src/main/java/chess/ChessController.java
+++ src/main/java/chess/ChessController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import chess.domain.Status;
 import chess.dto.BoardDto;
@@ -22,7 +21,7 @@ import chess.dto.ExceptionResponseDto;
 import chess.dto.MoveDto;
 import chess.dto.RoomDto;
 
-@Controller
+@RestController
 public class ChessController {
 
     private final ChessService chessService;
@@ -31,13 +30,12 @@ public class ChessController {
         this.chessService = chessService;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "room.html";
+    @GetMapping("/rooms")
+    public List<RoomDto> rooms() {
+        return chessService.loadRooms();
     }
 
     @PostMapping("/rooms")
-    @ResponseBody
     public ResponseEntity<Long> create(@RequestParam String name, @RequestParam String password) {
         long id = chessService.createRoom(name, password).getId();
         return ResponseEntity.created(URI.create("/rooms/" + id))
@@ -45,40 +43,29 @@ public class ChessController {
     }
 
     @PostMapping("/rooms/{roomId}")
-    @ResponseBody
     public BoardDto start(@PathVariable Long roomId) {
         return chessService.startNewGame(roomId);
     }
 
     @GetMapping("/rooms/{roomId}")
-    @ResponseBody
     public BoardDto load(@PathVariable Long roomId) {
         return chessService.load(roomId);
     }
 
     @DeleteMapping("/rooms/{roomId}")
-    @ResponseBody
     public boolean delete(@PathVariable Long roomId, @RequestParam String password) {
         return chessService.delete(roomId, password);
     }
 
     @PatchMapping("/rooms/{roomId}/move")
-    @ResponseBody
     public BoardDto move(@PathVariable Long roomId,
         @RequestBody MoveDto moveDto) {
         return chessService.move(roomId, moveDto);
     }
 
     @GetMapping("/rooms/{roomId}/status")
-    @ResponseBody
     public Status status(@PathVariable Long roomId) {
         return chessService.status(roomId);
-    }
-
-    @GetMapping("/rooms")
-    @ResponseBody
-    public List<RoomDto> rooms() {
-        return chessService.loadRooms();
     }
 
     @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, NoSuchElementException.class})

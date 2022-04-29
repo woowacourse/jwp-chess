2d9d4cb04ba9754a424d80a388b98838818d73dc
sun <syoun602@gmail.com>
2022-04-29 16:45:38 +0900

    refactor: 포괄적인 exception에 대해 다루도록 변경

--- src/main/java/chess/controller/ExceptionAdvice.java
+++ src/main/java/chess/controller/ExceptionAdvice.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
-    public ResponseEntity<String> exception(Exception e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(final Exception e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }

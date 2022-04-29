6d690c35416a417e8ecfdf9d7f40309bc8680491
Wishoon <heonga2@gmail.com>
2022-04-29 21:27:48 +0900

    refactor : 화면 렌더링, API 요청 로직 변경

---
+++ src/main/resources/static/src/board.js
@@ -0,0 +1,65 @@
+const score = document.getElementById("score");
+
+function selectBoard(roomId) {
+    window.location.href = `/room/${roomId}`;
+}
+
+async function insertBoard() {
+    const roomId = document.getElementById("roomId").value;
+    const res = await fetch(`/api/room/${roomId}/board`, {
+        method: "post"
+    });
+
+    selectBoard(roomId);
+}
+
+async function updateBoard() {
+    const roomId = document.getElementById("roomId").value;
+    const res = await fetch(`/api/room/${roomId}/square`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            from: document.getElementById("from").value,
+            to: document.getElementById("to").value
+        }),
+    });
+
+    if (!res.ok) {
+        const data = await res.json();
+        alert(data.message);
+        return;
+    }
+
+    selectBoard(roomId);
+}
+
+async function updateStateEnd() {
+    const roomId = document.getElementById("roomId").value;
+    const res = await fetch(`/api/room/${roomId}/stateEnd`, {
+        method: "put"
+    });
+
+    if (!res.ok) {
+        const data = await res.json();
+        alert(data.message);
+        return;
+    }
+
+    selectBoard(roomId);
+}
+
+async function selectStatus() {
+    const roomId = document.getElementById("roomId").value;
+    const res = await fetch(`/api/room/${roomId}/status`, {
+        method: "get",
+    });
+
+    if (!res.ok) {
+        alert(res.json().message);
+        return;
+    }
+    const data = await res.json();
+    score.innerText = `백: ${data.whiteScore}점 흑: ${data.blackScore}점`;
+}

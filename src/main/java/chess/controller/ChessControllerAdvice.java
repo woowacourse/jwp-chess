d00042660336c72edbd8ce4b1173e263b4403f5c
Chris <bjuuuu98@gmail.com>
2022-04-29 15:01:53 +0900

    refactor: @ControllerAdvice를 @RestControllerAdvice로 수정

--- src/main/java/chess/controller/ChessControllerAdvice.java
+++ src/main/java/chess/controller/ChessControllerAdvice.java
@@ -1,10 +1,10 @@
 package chess.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ChessControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)

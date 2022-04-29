9faf16e8aa4167f053af05c4b611ed7b0962faf5
progress0407 <progress0407@gmail.com>
2022-04-29 16:31:33 +0900

    refactor: 유휴 세션 제거를 SessionToChessRepository 가 아닌 yml 파일의 설정에서 관리하도록 변경

--- src/main/java/chess/SpringWebChessApplication.java
+++ src/main/java/chess/SpringWebChessApplication.java
@@ -2,9 +2,7 @@ package chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class SpringWebChessApplication {
     public static void main(String[] args) {
--- src/main/java/chess/repository/SessionToChessRepository.java
+++ src/main/java/chess/repository/SessionToChessRepository.java
@@ -1,11 +1,9 @@
 package chess.repository;
 
 import chess.domain.board.ChessBoard;
-import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.http.HttpSession;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -23,13 +21,14 @@ public class SessionToChessRepository {
         return sessionToChessBoard.get(session);
     }
 
-    @Scheduled(cron = "*/10 * * * * *")
-    public void checkAndRemoveSessions() {
-        for (HttpSession session : sessionToChessBoard.keySet()) {
-            checkAndRemoveSession(session);
-        }
-    }
+//    @Scheduled(cron = "*/10 * * * * *")
+//    public void checkAndRemoveSessions() {
+//        for (HttpSession session : sessionToChessBoard.keySet()) {
+//            checkAndRemoveSession(session);
+//        }
+//    }
 
+/*
     private void checkAndRemoveSession(HttpSession session) {
         long currentTimeMillis = System.currentTimeMillis();
         long lastAccessedTime = session.getLastAccessedTime();
@@ -41,4 +40,5 @@ public class SessionToChessRepository {
             System.out.println("remove it " + session);
         }
     }
+*/
 }

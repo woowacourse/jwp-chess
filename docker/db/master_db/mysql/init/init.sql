b968520ca8c227920f93073db937883c353494ad
sinb57 <sinb57@naver.com>
2022-04-30 02:17:29 +0900

    chore: docker db init.sql 수정

--- docker/db/master_db/mysql/init/init.sql
+++ docker/db/master_db/mysql/init/init.sql
@@ -1,15 +1,23 @@
 create table Player (
-                        id integer not null auto_increment,
-                        color varchar(5) not null,
-                        pieces varchar(255),
-                        primary key (id)
+    id integer not null auto_increment,
+    color varchar(5) not null,
+    pieces varchar(255),
+    primary key (id)
 );
 
 create table Game (
-                      id integer not null auto_increment,
-                      player_id1 integer not null,
-                      player_id2 integer not null,
-                      finished boolean not null,
-                      turn_color varchar(5) not null,
-                      primary key (id)
+    id integer not null auto_increment,
+    player_id1 integer not null,
+    player_id2 integer not null,
+    finished boolean not null,
+    turn_color varchar(5) not null,
+    primary key (id)
+);
+
+create table Room (
+    id integer not null auto_increment,
+    game_id integer not null,
+    title varchar(30) not null,
+    password varchar(30) not null,
+    primary key (id)
 );

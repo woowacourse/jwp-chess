1842bcbc2252692a86b99e64885a9415aa186fa2
beomWhale <cndqjacndqja@gmail.com>
2022-04-29 23:10:15 +0900

    refactor: error log 제거

--- src/main/java/chess/controller/ControllerAdvice.java
+++ src/main/java/chess/controller/ControllerAdvice.java
@@ -3,13 +3,9 @@ package chess.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestControllerAdvice
 public class ControllerAdvice {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> IllegalArgumentExceptionHandle(RuntimeException exception) {
@@ -18,7 +14,6 @@ public class ControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> ExceptionHandle(Exception exception) {
-        logger.error(exception.getMessage());
         return ResponseEntity.badRequest().body("서버에 문제가 발생했습니다.");
     }
 }
--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -53,6 +53,9 @@ public class ChessService {
 
     public void deleteBy(Long roomId, String password) {
         Room room = roomDao.findById(roomId);
+        if (room.getStatus()) {
+            throw new IllegalArgumentException("진행 중인 게임은 삭제할 수 없습니다.");
+        }
         if (room.getPassword().equals(password)) {
             roomDao.deleteBy(roomId, password);
             return;

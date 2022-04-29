b9c68310efb98b9f198730162bab3972ced02ea7
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-29 15:06:21 +0900

    feat: Exception.class 핸들러 기능 추가

--- src/main/java/chess/controller/ChessGameControllerAdvice.java
+++ src/main/java/chess/controller/ChessGameControllerAdvice.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ChessGameControllerAdvice {
 
+    private static final String INTERNAL_EXCEPTION_MESSAGE = "서버 내부 에러입니다.";
+
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, NullPointerException.class,
             NoSuchElementException.class})
     public ResponseEntity<ChessGameErrorResponse> handleBusinessException(RuntimeException runtimeException) {
@@ -27,6 +29,8 @@ public class ChessGameControllerAdvice {
         return ResponseEntity.internalServerError().build();
     }
 
-
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ChessGameErrorResponse> handleException() {
+        return ResponseEntity.internalServerError().body(ChessGameErrorResponse.create(INTERNAL_EXCEPTION_MESSAGE));
+    }
 }

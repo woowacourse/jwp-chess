0b348bbc273f47ecba5bee0c3c9c9d63efd49356
Juhyung990122 <harukawa99@naver.com>
2022-04-29 15:40:32 +0900

    feat: 컨트롤러 예외처리 설정 변경

--- src/main/java/chess/controller/WebChessExceptionHandler.java
+++ src/main/java/chess/controller/WebChessExceptionHandler.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
 public class WebChessExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {IllegalArgumentException.class})

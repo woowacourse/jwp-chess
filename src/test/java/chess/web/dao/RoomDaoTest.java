c7370b9e2ef4279efbf78af588e0534430b85f46
giron <ggyucheol0@gmail.com>
2022-04-29 20:20:03 +0900

    feat: RoomDao에 대한 테스트 추가 - findById() - deleteById() - findAll() - existByTitle()

--- src/test/java/chess/web/dao/RoomDaoTest.java
+++ src/test/java/chess/web/dao/RoomDaoTest.java
@@ -2,6 +2,7 @@ package chess.web.dao;
 
 import chess.domain.board.Team;
 import chess.domain.board.Turn;
+import chess.domain.entity.Room;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @Sql("/data.sql")
@@ -17,13 +21,8 @@ class RoomDaoTest {
 
     @Autowired
     private RoomDao roomDao;
-    private Long boardId;
-
-    @BeforeEach
-    void setUp() {
-
-        boardId = roomDao.save("첫번째 방제목", "123");
-    }
+    private final Long boardId = 1L;
+    private static final String TITLE = "제목";
 
     @Test
     @DisplayName("처음 저장된 board가 초기화된 Turn과 동일한지 테스트")
@@ -48,11 +47,42 @@ class RoomDaoTest {
     @DisplayName("새로운 보드판이 만들어지면 pk값이 1 증가한다.")
     void save() {
         //when
-        Long saveId = roomDao.save("방제목","123");
+        Long saveId = roomDao.save("방제목", "123");
         //then
         assertThat(boardId + 1).isEqualTo(saveId);
         roomDao.deleteById(saveId);
+    }
 
+    @Test
+    @DisplayName("해당 식별자의 방 정보를 가져온다.")
+    void findById() {
+        Room room = roomDao.findById(boardId).get();
+        assertAll(
+                () -> assertThat(room.getId()).isEqualTo(boardId),
+                () -> assertThat(room.getTitle()).isEqualTo(TITLE),
+                () -> assertThat(room.getPassword()).isEqualTo("패스워드")
+        );
+    }
+
+    @Test
+    @DisplayName("식별자를 통해 방을 제거하면 남은 방 개수는 이전보다 한 개 적어야한다.")
+    void deleteById() {
+        int before = roomDao.findAll().size();
+        roomDao.deleteById(boardId);
+        int after = roomDao.findAll().size();
+        assertThat(after).isEqualTo(before - 1);
+    }
+
+    @Test
+    @DisplayName("전체 방 개수인 한 개가 나온다.")
+    void findAll() {
+        assertThat(roomDao.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 제목이 있다면 true를 반환한다.")
+    void existByTitle(){
+        assertThat(roomDao.existByTitle(TITLE)).isTrue();
     }
 
 }

ec52959a598cf4d512c62c926760d554c903cf1d
Seongwon97 <osw970325@naver.com>
2022-04-30 16:54:18 +0900

    refactor: sql 변수명을 SQL로 변경
    - 가독성 향상을 위해 변경

--- src/main/java/chess/dao/PieceDao.java
+++ src/main/java/chess/dao/PieceDao.java
@@ -19,10 +19,10 @@ public class PieceDao {
     }
 
     public void saveAllPieces(final Player player, final long roomId) {
-        final String sql = "insert into piece (position, name, team, room_id) values (?, ?, ?, ?)";
+        final String SQL = "insert into piece (position, name, team, room_id) values (?, ?, ?, ?)";
         final List<Piece> pieces = player.findAll();
         for (Piece piece : pieces) {
-            jdbcTemplate.update(sql, toPositionString(piece.getPosition()), String.valueOf(piece.getName()),
+            jdbcTemplate.update(SQL, toPositionString(piece.getPosition()), String.valueOf(piece.getName()),
                     player.getTeamName(), roomId);
         }
     }
@@ -34,8 +34,8 @@ public class PieceDao {
     }
 
     public List<PieceDto> findAllPieceByIdAndTeam(final long roomId, final String team) {
-        final String sql = "select * from piece where room_id = (?) and team = (?)";
-        return jdbcTemplate.query(sql,
+        final String SQL = "select * from piece where room_id = (?) and team = (?)";
+        return jdbcTemplate.query(SQL,
                 (resultSet, count) -> new PieceDto(
                         resultSet.getString("position"),
                         resultSet.getString("name")
@@ -43,18 +43,18 @@ public class PieceDao {
     }
 
     public void deleteAllPiecesByRoomId(final long roomId) {
-        final String sql = "delete from piece where room_id = (?)";
-        jdbcTemplate.update(sql, roomId);
+        final String SQL = "delete from piece where room_id = (?)";
+        jdbcTemplate.update(SQL, roomId);
     }
 
     public void deletePieceByRoomIdAndPositionAndTeam(final long roomId, final String position, final String team) {
-        final String sql = "delete from piece where room_id = (?) and position = (?) and team = (?)";
-        jdbcTemplate.update(sql, roomId, position, team);
+        final String SQL = "delete from piece where room_id = (?) and position = (?) and team = (?)";
+        jdbcTemplate.update(SQL, roomId, position, team);
     }
 
     public void updatePiecePositionByRoomIdAndTeam(final long roomId, final String current,
                                                    final String destination, final String team) {
-        final String sql = "update piece set position = (?) where room_id = (?) and position = (?) and team = (?)";
-        jdbcTemplate.update(sql, destination, roomId, current, team);
+        final String SQL = "update piece set position = (?) where room_id = (?) and position = (?) and team = (?)";
+        jdbcTemplate.update(SQL, destination, roomId, current, team);
     }
 }
--- src/main/java/chess/dao/RoomDao.java
+++ src/main/java/chess/dao/RoomDao.java
@@ -20,27 +20,27 @@ public class RoomDao {
     }
 
     public void save(final String roomName, final String password, final Team turn) {
-        final String sql = "insert into room (name, password, turn) values (?, ?, ?)";
-        jdbcTemplate.update(sql, roomName, password, turn.getName());
+        final String SQL = "insert into room (name, password, turn) values (?, ?, ?)";
+        jdbcTemplate.update(SQL, roomName, password, turn.getName());
     }
 
     public Optional<Integer> findRoomIdByName(final String roomName) {
-        final String sql = "select id from room where name = (?)";
+        final String SQL = "select id from room where name = (?)";
         try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, Integer.class, roomName));
+            return Optional.ofNullable(jdbcTemplate.queryForObject(SQL, Integer.class, roomName));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
     }
 
     public String findTurn(final long roomId) {
-        final String sql = "select turn from room where id = (?)";
-        return jdbcTemplate.queryForObject(sql, String.class, roomId);
+        final String SQL = "select turn from room where id = (?)";
+        return jdbcTemplate.queryForObject(SQL, String.class, roomId);
     }
 
     public GameNameAndTurnDto findNameAndTurnById(final long roomId) {
-        final String sql = "select name, turn from room where id =(?)";
-        return jdbcTemplate.queryForObject(sql,
+        final String SQL = "select name, turn from room where id =(?)";
+        return jdbcTemplate.queryForObject(SQL,
                 (resultSet, count) -> new GameNameAndTurnDto(
                         resultSet.getString("name"),
                         resultSet.getString("turn")
@@ -48,13 +48,13 @@ public class RoomDao {
     }
 
     public String findRoomPasswordById(final long roomId) {
-        final String sql = "select password from room where id = (?)";
-        return jdbcTemplate.queryForObject(sql, String.class, roomId);
+        final String SQL = "select password from room where id = (?)";
+        return jdbcTemplate.queryForObject(SQL, String.class, roomId);
     }
 
     public List<RoomInfoDto> findAll() {
-        final String sql = "select id, name from room";
-        return jdbcTemplate.query(sql,
+        final String SQL = "select id, name from room";
+        return jdbcTemplate.query(SQL,
                 (resultSet, count) -> new RoomInfoDto(
                         resultSet.getLong("id"),
                         resultSet.getString("name")
@@ -62,12 +62,12 @@ public class RoomDao {
     }
 
     public void updateTurn(final long roomId, final Team turn) {
-        final String sql = "update room set turn = (?) where id = (?)";
-        jdbcTemplate.update(sql, turn.getName(), roomId);
+        final String SQL = "update room set turn = (?) where id = (?)";
+        jdbcTemplate.update(SQL, turn.getName(), roomId);
     }
 
     public void delete(final long roomId) {
-        final String sql = "delete from room where id = (?)";
-        jdbcTemplate.update(sql, roomId);
+        final String SQL = "delete from room where id = (?)";
+        jdbcTemplate.update(SQL, roomId);
     }
 }

0d631f35c84052b796eef6dc76b785680204ef25
skullKim <kyk990328@gmail.com>
2022-04-30 12:32:29 +0900

    chore(/test): 잘못 해결한 conflict 수정
    테스트 코드에서 제대로 수정하지 않은 conflict 해결

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE game (
       id INT AUTO_INCREMENT NOT NULL,
       name VARCHAR(20) NOT NULL,
@@ -8,28 +7,11 @@ CREATE TABLE game (
 );
 
 CREATE TABLE piece (
-       id INT AUTO_INCREMENT NOT NULL,
-       position  VARCHAR(10) NOT NULL,
-       piece_type VARCHAR(10) NOT NULL,
-       color VARCHAR(10) NOT NULL,
-       game_id INT NOT NULL,
-       PRIMARY KEY (id),
-       FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
-=======
-CREATE TABLE game
-(
-    game_id int         NOT NULL PRIMARY KEY,
-    state   varchar(20) NOT NULL
-);
-
-CREATE TABLE piece
-(
-    piece_id   int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    game_id    INT         NOT NULL,
+    id         INT AUTO_INCREMENT NOT NULL,
     position   VARCHAR(10) NOT NULL,
     piece_type VARCHAR(10) NOT NULL,
     color      VARCHAR(10) NOT NULL,
-    FOREIGN KEY (game_id)
-        REFERENCES game (game_id) ON DELETE CASCADE
->>>>>>> refactor: GameDao가 JdbcTemplate을 사용하도록 변경
+    game_id    INT         NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
 );

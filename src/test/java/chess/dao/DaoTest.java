6d2b73f46fa3396e47a138ac00d0db5c081f5717
kth990303 <kth990303@naver.com>
2022-04-30 16:17:18 +0900

    refactor: 불필요한 import 및 메서드 제거

--- src/test/java/chess/dao/DaoTest.java
+++ src/test/java/chess/dao/DaoTest.java
@@ -1,6 +1,5 @@
 package chess.dao;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;

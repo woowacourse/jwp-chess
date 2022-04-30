122eb3b15312fa4be342c58ffaa4a6e5ca0cf0f5
Donggyu <a29661498@gmail.com>
2022-04-30 12:54:56 +0900

    feat: 누락된 init.sql 업데이트

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,23 +1,22 @@
-create table PIECE
-(
-    id     bigint auto_increment primary key,
-    type   enum ('r', 'n', 'b', 'q', 'k', 'p') not null,
-    team   enum ('white', 'black')             not null,
-    `rank` int                                 not null,
-    file   varchar(1)                          not null
-);
-
 create table CHESSGAME
 (
-    id bigint auto_increment primary key,
+    id      bigint auto_increment
+        primary key,
     game_name varchar(20) not null,
-    turn      varchar(5)  not null
+    turn      varchar(20) not null,
+    password  varchar(20) not null
 );
 
-alter table PIECE
-    add chessgame_id bigint null;
-
-alter table PIECE
-    add constraint PIECE_CHESSBOARD__fk
+create table PIECE
+(
+    id      bigint auto_increment
+        primary key,
+    type        enum ('r', 'n', 'b', 'q', 'k', 'p') not null,
+    team        enum ('white', 'black')             not null,
+    `rank`      int                                 not null,
+    file        varchar(1)                          not null,
+    chessgame_id bigint                             null,
+    constraint PIECE_CHESSBOARD__fk
         foreign key (chessgame_id) references CHESSGAME (id)
-            on update cascade on delete cascade;
+            on update cascade on delete cascade
+);

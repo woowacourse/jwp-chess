e11b5bcea6abca3488768a29aee157adc21872cd
skullKim <kyk990328@gmail.com>
2022-04-30 12:25:32 +0900

    chore(chess/): conflict 해결시에 잘못 해결한 부분 수정

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -95,39 +95,5 @@ public class ChessController {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping("/games/{gameId}")
-    public ChessGameResponse startOrRestartGame(@PathVariable long gameId) {
-        return chessService.startOrRestartGame(gameId);
-    }
-
-    @GetMapping("/restart/{gameId}")
-    public ChessGameResponse restartGame(@PathVariable long gameId) {
-        return chessService.restartGame(gameId);
-    }
-
-    @PostMapping("/move/{gameId}")
-    public ChessGameResponse movePiece(@PathVariable long gameId, @RequestBody MoveRequest moveRequest) {
-        return chessService.move(gameId, moveRequest);
-    }
-
-    @GetMapping("/{gameId}/status")
-    public StatusResponse status(@PathVariable long gameId) {
-        return chessService.status(gameId);
-    }
-
-    @GetMapping("/{gameId}/end")
-    public ChessGameResponse endGame(@PathVariable long gameId) {
-        return chessService.end(gameId);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequest(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Void> handleDataNotFound() {
-        return ResponseEntity.notFound().build();
-    }
 }
 
--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -1,13 +1,35 @@
 package chess.controller;
 
+import chess.service.ChessService;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebController {
 
+    private final ChessService chessService;
+
+    public WebController(ChessService chessService) {
+        this.chessService = chessService;
+    }
+
     @GetMapping("/")
     public String index() {
         return "index.html";
+    }
+
+
+    @GetMapping("/game")
+    public RedirectView redirectGame(@RequestParam String gameName, @RequestParam String password, HttpSession session) {
+        final int gameId = chessService.getGameId(gameName, password);
+        return new RedirectView("/game/" + gameId);
+    }
+
+    @GetMapping("/game/{gameId}")
+    public String joinGame() {
+        return "/game.html";
     }
 }

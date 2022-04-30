5e21666f73551e08c4022d52bd89d243a33f39f8
yeon-06 <rnjstldus2@gmail.com>
2022-04-30 11:24:59 +0900

    refactor: printStackTrace() 대신 Logger 사용

--- src/main/java/chess/controller/ExceptionResolver.java
+++ src/main/java/chess/controller/ExceptionResolver.java
@@ -1,5 +1,5 @@
-package chess.controller;
-
+package chess.controller;import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,16 +9,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionResolver {
 
     private static final String DEFAULT_SERVER_EXCEPTION_MESSAGE = "예상치 못한 문제가 발생하였습니다.";
+    private static final String EMPTY_STRING = "";
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionResolver.class);
 
     @ExceptionHandler
     public ResponseEntity<String> handleException(Exception e) {
-        e.printStackTrace();
+        logger.error(EMPTY_STRING, e);
         return new ResponseEntity<>(DEFAULT_SERVER_EXCEPTION_MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleException(IllegalArgumentException e) {
-        e.printStackTrace();
+        logger.error(EMPTY_STRING, e);
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }

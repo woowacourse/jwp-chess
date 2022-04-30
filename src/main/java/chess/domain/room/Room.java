2541893e2b48c204bf9d15fd0c9a46a8b6ae246c
nailseong <nnis9857@gmail.com>
2022-04-30 17:21:18 +0900

    feat: RoomName 도메인 추가

--- src/main/java/chess/domain/room/Room.java
+++ src/main/java/chess/domain/room/Room.java
@@ -1,37 +1,26 @@
-package chess.domain;
+package chess.domain.room;
 
+import chess.domain.ChessGame;
 import chess.domain.chessboard.ChessBoardFactory;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class Room {
 
-    private static final int NAME_MAX_LENGTH = 10;
-    
-    private final String name;
+    private final RoomName name;
     private final String password;
     private final ChessGame chessGame;
 
     public Room(final String name, final String password, final ChessGame chessGame) {
-        validateName(name);
-        this.name = name;
+        this.name = new RoomName(name);
         validatePassword(password);
         this.password = password;
         this.chessGame = chessGame;
     }
 
     public Room(final String name, final String password) {
-        this.name = name;
+        this.name = new RoomName(name);
         this.password = BCrypt.hashpw(password, BCrypt.gensalt());
         this.chessGame = new ChessGame(ChessBoardFactory.createChessBoard());
-    }
-
-    private void validateName(final String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("방 이름에 공백은 허용되지 않습니다.");
-        }
-        if (name.length() > NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException("방 이름은 최대 " + NAME_MAX_LENGTH + "자 까지 허용됩니다.");
-        }
     }
 
     private void validatePassword(final String password) {
---
+++ src/main/java/chess/domain/room/RoomName.java
@@ -0,0 +1,22 @@
+package chess.domain.room;
+
+public class RoomName {
+
+    private static final int NAME_MAX_LENGTH = 10;
+
+    private final String value;
+
+    public RoomName(final String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("방 이름에 공백은 허용되지 않습니다.");
+        }
+        if (value.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("방 이름은 최대 " + NAME_MAX_LENGTH + "자 까지 허용됩니다.");
+        }
+    }
+}

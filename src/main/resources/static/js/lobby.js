ae308580fcce478a49e3b4e225b51da411dffe30
asebn1 <asebn1@khu.ac.kr>
2022-05-01 22:43:02 +0900

    refactor: 코드 컨밴션 적용

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -1,4 +1,3 @@
-
 function func(id) {
     let password = prompt("비밀번호 입력", "");
     $.ajax({
@@ -12,9 +11,11 @@ function func(id) {
         error: alertError
     })
 }
+
 function redirectIndex() {
     window.location.href = "/";
 }
+
 function alertError(response) {
     alert(response.responseText);
 }

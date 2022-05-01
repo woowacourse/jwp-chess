f3827cd36f291a104e6c706bde34d1d6f494725f
hyunrrr <whgusrms96@gmail.com>
2022-05-01 16:19:31 +0900

    style: public메서드 위로 올림

--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -36,71 +36,6 @@ public final class ChessService {
         this.pieceDao = pieceDao;
     }
 
-    private void saveAllPieceToStorage(int gameId, Map<Position, Piece> boardElements) {
-        List<BoardElementDto> boardElementDtos = toBoardDtos(boardElements);
-        for (BoardElementDto boardElementDto : boardElementDtos) {
-            pieceDao.savePiece(gameId, boardElementDto);
-        }
-    }
-
-    private List<BoardElementDto> toBoardDtos(Map<Position, Piece> board) {
-        return board.entrySet()
-                .stream()
-                .map(it -> toBoardDto(it.getKey(), it.getValue()))
-                .collect(Collectors.toList());
-    }
-
-    private BoardElementDto toBoardDto(Position position, Piece piece) {
-        return new BoardElementDto(position, piece);
-    }
-
-    private Board convertToBoard(final List<BoardElementDto> boardDatas) {
-        Map<Position, Piece> board = boardDatas.stream()
-                .collect(Collectors.toMap(it -> Position.from(it.getPosition()),
-                        it -> PieceFactory.of(it.getPieceName(), it.getPieceColor())));
-        return new Board(() -> board);
-    }
-
-    public ChessGameDto move(int gameId, final String from, final String to) {
-        ChessGame chessGame = new ChessGame(StateFactory.of(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
-        chessGame.move(Position.from(from), Position.from(to));
-        final var nextColor = getColorFromStorage(gameId).next();
-        updateBoard(from, to, gameId, nextColor.name());
-        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
-    }
-
-    private void updateBoard(String from, String to, int gameId, String turn) {
-        pieceDao.deletePieceByIdAndPosition(gameId, to);
-        pieceDao.updatePiecePosition(gameId, from, to);
-        gameDao.updateTurn(gameId, turn);
-    }
-
-    public ChessGameDto newGame(int gameId) {
-        initGame(gameId);
-        ChessGame chessGame = new ChessGame(new Running(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
-        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
-    }
-
-    private void initGame(int gameId) {
-        pieceDao.deleteAllPieceById(gameId);
-        saveAllPieceToStorage(gameId, new BoardInitializer().init());
-        gameDao.updateTurn(gameId, "WHITE");
-        roomDao.updateStatus(gameId, "PLAY");
-    }
-
-    public ChessGameDto loadGame(int gameId) {
-        ChessGame chessGame = new ChessGame(StateFactory.of(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
-        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
-    }
-
-    private Color getColorFromStorage(int gameId) {
-        return Color.from(gameDao.findTurnById(gameId));
-    }
-
-    private Board getBoardFromStorage(int gameId) {
-        return convertToBoard(pieceDao.findAllPieceById(gameId));
-    }
-
     public void createRoom(String roomName, String password) {
         int gameId = gameDao.insertWithKeyHolder("WHITE");
         RoomDto roomDto = new RoomDto(gameId, roomName, password, "STOP");
@@ -109,10 +44,26 @@ public final class ChessService {
     }
 
     public List<RoomDto> loadRooms() {
-        for (RoomDto roomDto : roomDao.findAll()) {
-            System.out.println(roomDto.getId());
-        }
         return roomDao.findAll();
+    }
+
+    public ChessGameDto newGame(int gameId) {
+        initGame(gameId);
+        ChessGame chessGame = new ChessGame(new Running(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
+        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
+    }
+
+    public ChessGameDto loadGame(int gameId) {
+        ChessGame chessGame = new ChessGame(StateFactory.of(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
+        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
+    }
+
+    public ChessGameDto move(int gameId, final String from, final String to) {
+        ChessGame chessGame = new ChessGame(StateFactory.of(getColorFromStorage(gameId), getBoardFromStorage(gameId)));
+        chessGame.move(Position.from(from), Position.from(to));
+        final var nextColor = getColorFromStorage(gameId).next();
+        updateBoard(from, to, gameId, nextColor.name());
+        return new ChessGameDto(pieceDao.findAllPieceById(gameId), chessGame.status());
     }
 
     public void endGame(int gameId) {
@@ -128,5 +79,51 @@ public final class ChessService {
             return DeleteResponseDto.success();
         }
         return DeleteResponseDto.fail();
+    }
+
+    private List<BoardElementDto> toBoardDtos(Map<Position, Piece> board) {
+        return board.entrySet()
+                .stream()
+                .map(it -> toBoardDto(it.getKey(), it.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    private void saveAllPieceToStorage(int gameId, Map<Position, Piece> boardElements) {
+        List<BoardElementDto> boardElementDtos = toBoardDtos(boardElements);
+        for (BoardElementDto boardElementDto : boardElementDtos) {
+            pieceDao.savePiece(gameId, boardElementDto);
+        }
+    }
+
+    private BoardElementDto toBoardDto(Position position, Piece piece) {
+        return new BoardElementDto(position, piece);
+    }
+
+    private Board convertToBoard(final List<BoardElementDto> boardDatas) {
+        Map<Position, Piece> board = boardDatas.stream()
+                .collect(Collectors.toMap(it -> Position.from(it.getPosition()),
+                        it -> PieceFactory.of(it.getPieceName(), it.getPieceColor())));
+        return new Board(() -> board);
+    }
+
+    private void updateBoard(String from, String to, int gameId, String turn) {
+        pieceDao.deletePieceByIdAndPosition(gameId, to);
+        pieceDao.updatePiecePosition(gameId, from, to);
+        gameDao.updateTurn(gameId, turn);
+    }
+
+    private void initGame(int gameId) {
+        pieceDao.deleteAllPieceById(gameId);
+        saveAllPieceToStorage(gameId, new BoardInitializer().init());
+        gameDao.updateTurn(gameId, "WHITE");
+        roomDao.updateStatus(gameId, "PLAY");
+    }
+
+    private Color getColorFromStorage(int gameId) {
+        return Color.from(gameDao.findTurnById(gameId));
+    }
+
+    private Board getBoardFromStorage(int gameId) {
+        return convertToBoard(pieceDao.findAllPieceById(gameId));
     }
 }

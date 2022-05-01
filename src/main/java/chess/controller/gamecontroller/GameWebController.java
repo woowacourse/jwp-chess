9149d79f5d9da4be2a120958cadeed04aaa85ac8
Chris <bjuuuu98@gmail.com>
2022-05-01 23:37:12 +0900

    refactor: @PutMapping -> @PatchMapping으로 수정

--- src/main/java/chess/controller/gamecontroller/GameWebController.java
+++ src/main/java/chess/controller/gamecontroller/GameWebController.java
@@ -8,9 +8,9 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,7 +36,7 @@ public class GameWebController {
         return render(roomId);
     }
 
-    @PutMapping
+    @PatchMapping
     public ModelAndView move(@PathVariable int roomId, @RequestBody MoveRequestDto moveRequest) {
         final String sourcePosition = moveRequest.getSource();
         final String targetPosition = moveRequest.getTarget();

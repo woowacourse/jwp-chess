aabcbd2a5a8d213cb29fdbac4688d04e7c6bd878
Hyunta <mohai2618@gmail.com>
2022-05-01 11:40:18 +0900

    refactor: @ModelAttribute 생략

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,7 +36,7 @@ public class ChessController {
     }
 
     @PostMapping("/room")
-    public String room(@ModelAttribute RoomRequestDto roomDto) {
+    public String room(RoomRequestDto roomDto) {
         Room room = chessService.init(
                 roomDto.getRoomName(),
                 roomDto.getPassword(),

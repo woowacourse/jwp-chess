323129f793be8fae50ec880a05585bdc7b0e6f31
summerlunaa <summerlunaa9@gmail.com>
2022-05-02 01:29:44 +0900

    refactor: 에러 세분화 및 에러 페이지 구현

--- src/main/java/chess/web/controller/ChessControllerAdvice.java
+++ src/main/java/chess/web/controller/ChessControllerAdvice.java
@@ -1,14 +1,42 @@
 package chess.web.controller;
 
+import java.sql.SQLException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ChessControllerAdvice {
 
+    @ExceptionHandler({IllegalArgumentException.class, UnsupportedOperationException.class})
+    public ResponseEntity<String> handleGameException(Exception exception) {
+        return new ResponseEntity<>(
+                exception.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({SQLException.class, DataAccessException.class})
+    public String handleSQLException(Exception exception, Model model) {
+        model.addAttribute("exception", exception);
+        return "/error";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoHandlerFoundException(NoHandlerFoundException exception, Model model){
+        model.addAttribute("exception", exception);
+        return "/error";
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handle(Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public String handleCommonException(Exception exception, Model model) {
+        model.addAttribute("exception", exception);
+        return "/error";
     }
 }

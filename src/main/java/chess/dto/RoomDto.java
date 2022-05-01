f1478438d90afa2a4abc50f94576927703afeb27
brorae <jhr3239@gmail.com>
2022-05-02 00:45:59 +0900

    refactor: DB Column의 길이에 맞춰 검증로직 추가

--- src/main/java/chess/dto/RoomDto.java
+++ src/main/java/chess/dto/RoomDto.java
@@ -2,18 +2,31 @@ package chess.dto;
 
 public class RoomDto {
 
+    private static final int MAX_TITLE_LENGTH = 20;
+    private static final int MAX_PASSWORD_LENGTH = 20;
+
     private String title;
     private String password;
 
     public RoomDto(String title, String password) {
         validateTitle(title);
+        validatePassword(password);
         this.title = title;
         this.password = password;
     }
 
     private void validateTitle(String title) {
         if (title.isBlank()) {
-            throw new IllegalArgumentException("방이름을 입력해주세요.");
+            throw new IllegalArgumentException("방 이름을 입력해주세요.");
+        }
+        if (title.length() > MAX_TITLE_LENGTH) {
+            throw new IllegalArgumentException("방 이름의 최대 길이는 20입니다.");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password.length() > MAX_PASSWORD_LENGTH) {
+            throw new IllegalArgumentException("비밀번호의 최대 길이는 20입니다.");
         }
     }
 

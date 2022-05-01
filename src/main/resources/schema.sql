b06de78efad4861aaa5c66586863be7a6a33ed33
progress0407 <progress0407@gmail.com>
2022-05-01 14:50:04 +0900

    chore(application.yml): production table 생성 설정 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,4 @@
-drop table if exists board_pieces cascade;
-drop table if exists games cascade;
-
-
-create table games (
+create table if not exists games  (
 	game_id 	    char(50)   not null     primary key,
 	last_team 		char(20),
 	create_at 		datetime
@@ -10,7 +6,7 @@ create table games (
 ENGINE=InnoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
 
 
-create table board_pieces (
+create table if not exists board_pieces  (
 	board_piece_id 	char(50)	not null 	primary key,
 	game_id 	    char(50),
 	`position` 	    char(5),

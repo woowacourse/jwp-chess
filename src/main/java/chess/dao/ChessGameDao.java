e0a02f1987b7a712f7b26da43b228d5118f51037
이한울 <dkfghg3@naver.com>
2022-05-01 16:04:22 +0900

    refactor: dao 예외 처리 부분 삭제

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -1,11 +1,8 @@
 package chess.dao;
 
-import chess.domain.ChessGame;
 import chess.domain.player.Team;
 import chess.dto.response.ChessGameInfoDto;
-import chess.exception.ExecuteQueryException;
 import java.util.List;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -21,11 +18,7 @@ public class ChessGameDao {
     public void saveChessGame(final String gameName, final String password, final String turn) {
         validateDuplicate(gameName);
         final String sql = "insert into chess_game (name, password, turn, running) values (?, ?, ?, ?)";
-        try {
-            jdbcTemplate.update(sql, gameName, password, turn, true);
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("게임을 저장할 수 없습니다.");
-        }
+        jdbcTemplate.update(sql, gameName, password, turn, true);
     }
 
     private void validateDuplicate(final String gameName) {
@@ -39,52 +32,32 @@ public class ChessGameDao {
 
     public int findChessGameIdByName(final String gameName) {
         final String sql = "select id from chess_game where name = (?)";
-        try {
-            return jdbcTemplate.queryForObject(sql, Integer.class, gameName);
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("체스 게임 정보 불러오는데 실패했습니다.");
-        }
+        return jdbcTemplate.queryForObject(sql, Integer.class, gameName);
     }
 
     public String findCurrentTurn(final int chessGameId) {
         final String sql = "select turn from chess_game where id = (?)";
-        try {
-            return jdbcTemplate.queryForObject(sql, String.class, chessGameId);
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("현재 턴 정보 불러오기가 실패했습니다.");
-        }
+        return jdbcTemplate.queryForObject(sql, String.class, chessGameId);
     }
 
     public void deleteChessGame(final int chessGameId) {
         final String sql = "delete from chess_game where id = (?)";
-        try {
-            jdbcTemplate.update(sql, chessGameId);
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("게임을 삭제할 수 없습니다.");
-        }
+        jdbcTemplate.update(sql, chessGameId);
     }
 
     public void updateGameTurn(final int gameId, final Team nextTurn) {
         final String sql = "update chess_game set turn = (?) where id = (?)";
-        try {
-            jdbcTemplate.update(sql, nextTurn.getName(), gameId);
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("턴 정보 업데이트에 실패했습니다.");
-        }
+        jdbcTemplate.update(sql, nextTurn.getName(), gameId);
     }
 
     public List<ChessGameInfoDto> findAllChessGame() {
         final String sql = "select id, name, turn, running from chess_game";
-        try {
-            return jdbcTemplate.query(sql, (rs, rowNum) -> new ChessGameInfoDto(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getString("turn"),
-                    rs.getBoolean("running")
-            ));
-        } catch (DataAccessException e) {
-            throw new ExecuteQueryException("체스 게임 정보 불러오기를 실패했습니다.");
-        }
+        return jdbcTemplate.query(sql, (rs, rowNum) -> new ChessGameInfoDto(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("turn"),
+                rs.getBoolean("running")
+        ));
     }
 
     public int findChessGameByIdAndPassword(final int gameId, final String password) {

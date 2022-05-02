f58ee0ef1a79f0e05eb2bc87b0b271fdec435e1f
sinb57 <sinb57@naver.com>
2022-05-02 17:21:51 +0900

    style: 사용하지 않는 import문 제거

--- src/main/java/chess/controller/IndexController.java
+++ src/main/java/chess/controller/IndexController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import chess.controller.dto.ControllerDtoAssembler;

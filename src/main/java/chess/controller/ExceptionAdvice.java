c50baa853b3b3261bc6fce1f0db8c03bd17d7ddf
yukong <kyb062666@gmail.com>
2022-05-02 15:19:18 +0900

    feat(ExceptionAdvice): 전역 에러 처리 추가

--- src/main/java/chess/controller/ExceptionAdvice.java
+++ src/main/java/chess/controller/ExceptionAdvice.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice {
 
+    private static final String DEFAULT_ERROR_MESSAGE = "체스 게임을 실행할 수 없습니다. 관리자에게 문의해주세요.";
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(DEFAULT_ERROR_MESSAGE);
+        return ResponseEntity.badRequest().body(errorResponseDto);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(IllegalArgumentException exception) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(exception.getMessage());

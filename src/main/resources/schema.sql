eda973aaeb17e42c489bdab84df66712b740aa79
Hyunta <mohai2618@gmail.com>
2022-05-02 14:40:06 +0900

    refactor: sql문 통일

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,45 +1,45 @@
 create table board
 (
-    id     int(10)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id     int(10)     not null auto_increment primary key,
     status varchar(10) not null,
     team   varchar(10) not null
 );
 
 create table square
 (
-    id          int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    square_file int(10) NOT NULL,
-    square_rank int(10) NOT NULL,
-    board_id    int(10) NOT NULL,
+    id          int(10) not null auto_increment primary key,
+    square_file int(10) not null,
+    square_rank int(10) not null,
+    board_id    int(10) not null,
     foreign key (board_id) references board (id)
         on delete cascade
 );
 
 create table piece
 (
-    id        int(10)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type      varchar(10) NOT NULL,
-    team      varchar(10) NOT NULL,
-    square_id int(10)     NOT NULL,
+    id        int(10)     not null auto_increment primary key,
+    type      varchar(10) not null,
+    team      varchar(10) not null,
+    square_id int(10)     not null,
     foreign key (square_id) references square (id)
         on delete cascade
 );
 
 create table room
 (
-    id       int(10)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title    varchar(20) NOT NULL,
+    id       int(10)     not null auto_increment primary key,
+    title    varchar(20) not null,
     password varchar(20) not null,
-    board_id int(10)     NOT NULL,
+    board_id int(10)     not null,
     foreign key (board_id) references board (id)
         on delete cascade
 );
 
 create table member
 (
-    id      int(10)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name    varchar(20) NOT NULL,
-    room_id int(10)     NOT NULL,
+    id      int(10)     not null auto_increment primary key,
+    name    varchar(20) not null,
+    room_id int(10)     not null,
     foreign key (room_id) references room (id)
         on delete cascade
 );

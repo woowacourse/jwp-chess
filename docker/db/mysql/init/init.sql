ead4325312c1642c2c6f3978d745265f588093be
beomWhale <cndqjacndqja@gmail.com>
2022-05-02 16:37:27 +0900

    refactor: init.sql 수정

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,20 +1,20 @@
 CREATE TABLE room
 (
-    id       bigint      NOT NULL AUTO_INCREMENT,
-    team     varchar(50) NOT NULL,
-    title    varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
-    status   boolean,
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    team     VARCHAR(50) NOT NULL,
+    title    VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    status   BOOLEAN,
     UNIQUE (title),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE board
 (
-    id       bigint      NOT NULL AUTO_INCREMENT,
-    position varchar(50) NOT NULL,
-    symbol   varchar(50) NOT NULL,
-    room_id  bigint      NOT NULL,
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    position VARCHAR(50) NOT NULL,
+    symbol   VARCHAR(50) NOT NULL,
+    room_id  BIGINT      NOT NULL,
     PRIMARY KEY (id),
     KEY room_id (room_id),
     CONSTRAINT id FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE ON UPDATE CASCADE

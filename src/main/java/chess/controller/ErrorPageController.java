9d3c7fc933287b62a981d6e1d24d1d94c18c6576
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-03 07:53:59 +0900

    feat(ErrorPageController): http error page 컨트롤러 구현

---
+++ src/main/java/chess/controller/ErrorPageController.java
@@ -0,0 +1,30 @@
+package chess.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            }
+            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            }
+        }
+        return "400";
+    }
+
+}

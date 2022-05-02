ebe8fbe98ad0bd5aa5d0fbf6a885571e9b096d85
sure-why-not <pkse1213@gmail.com>
2022-05-02 18:21:21 +0900

    refactor: Controller 메서드 네이밍 변경

--- src/main/java/chess/controller/GameController.java
+++ src/main/java/chess/controller/GameController.java
@@ -31,26 +31,26 @@ public class GameController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ChessGameResponse> load(@PathVariable Long id) {
+    public ResponseEntity<ChessGameResponse> getGame(@PathVariable Long id) {
         GameDto game = chessGameService.getGame(id);
         List<PieceDto> pieces = chessGameService.getPiecesOfGame(id);
         return ResponseEntity.ok().body(ChessGameResponse.from(game, pieces));
     }
 
     @GetMapping("/{id}/score")
-    public ResponseEntity<ScoresResponse> score(@PathVariable Long id) {
+    public ResponseEntity<ScoresResponse> getScore(@PathVariable Long id) {
         Map<PieceColor, Score> scoreByColor = chessGameService.getScore(id);
         return ResponseEntity.ok().body(ScoresResponse.of(scoreByColor));
     }
 
     @PutMapping(value = "/{id}/pieces")
-    public ResponseEntity<Void> move(@PathVariable Long id, @RequestBody MoveRequest moveRequest) {
+    public ResponseEntity<Void> movePiece(@PathVariable Long id, @RequestBody MoveRequest moveRequest) {
         chessGameService.movePiece(id, moveRequest.toEntity());
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ScoresResponse> end(@PathVariable Long id) {
+    public ResponseEntity<ScoresResponse> updateStatusToFinish(@PathVariable Long id) {
         Map<PieceColor, Score> scoresByColor = chessGameService.finishGame(id);
         return ResponseEntity.ok().body(ScoresResponse.of(scoresByColor));
     }

90cc9fd67700a1a4fc1fdb0f991c1000bce2f514
sure-why-not <pkse1213@gmail.com>
2022-05-02 17:13:29 +0900

    refactor: Exception 발생시 Error 상태코드 변경

--- src/main/java/chess/controller/ExceptionAdvice.java
+++ src/main/java/chess/controller/ExceptionAdvice.java
@@ -17,6 +17,6 @@ public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException() {
         ErrorResponse errorResponse = new ErrorResponse("서버에 문제가 발생했습니다. 다시 시도해주세요.");
-        return ResponseEntity.badRequest().body(errorResponse);
+        return ResponseEntity.internalServerError().body(errorResponse);
     }
 }

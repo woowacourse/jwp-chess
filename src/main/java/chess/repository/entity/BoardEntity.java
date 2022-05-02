4e0d241192b6d6c69c622a96dd605a75c8254c15
hj-rich <ztzy1907@gmail.com>
2022-05-03 08:20:55 +0900

    feat(board-entity): 게임아이디, 위치, 기물 정보를 기준으로 equals&hashcode 재정의

--- src/main/java/chess/repository/entity/BoardEntity.java
+++ src/main/java/chess/repository/entity/BoardEntity.java
@@ -2,6 +2,7 @@ package chess.repository.entity;
 
 import chess.domain.board.Position;
 import chess.domain.piece.Piece;
+import java.util.Objects;
 
 public class BoardEntity {
 
@@ -29,5 +30,23 @@ public class BoardEntity {
 
     public Piece getPiece() {
         return piece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BoardEntity that = (BoardEntity) o;
+        return gameId == that.gameId && Objects.equals(position, that.position) && Objects.equals(piece,
+                that.piece);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameId, position, piece);
     }
 }

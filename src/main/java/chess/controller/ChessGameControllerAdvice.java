b67bde3039c243faa742ef46c7ff804d8f436ffe
jaejae-yoo <wotj102@gmail.com>
2022-05-03 00:33:56 +0900

    refactor: exception http 상태 코드 추가

--- src/main/java/chess/controller/ChessGameControllerAdvice.java
+++ src/main/java/chess/controller/ChessGameControllerAdvice.java
@@ -1,6 +1,7 @@
 package chess.controller;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +14,11 @@ public class ChessGameControllerAdvice {
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<String> IllegalArgumentExceptionHandle(IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> RuntimeExceptionHandle(RuntimeException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler({SQLException.class, NullPointerException.class, EmptyResultDataAccessException.class})

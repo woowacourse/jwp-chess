2f0203174aa2bcccf5486402f41e0029b94a8470
beomWhale <cndqjacndqja@gmail.com>
2022-05-02 16:45:26 +0900

    refactor: ViewResolverController -> ChessViewController로 클래스명 변경

--- src/main/java/chess/controller/ChesViewController.java
+++ src/main/java/chess/controller/ChesViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewResolverController {
+public class ChesViewController {
 
     @GetMapping("/")
     public String main() {

7868e606ada305a0ed11e6fe0e34b70dd6ac05b3
EastHShin <xldk78@gmail.com>
2022-05-02 17:33:36 +0900

    refactor(ExceptionController): 익셉션의 순서 정렬과 예외 메시지의 상수화

--- src/main/java/chess/controller/ExceptionController.java
+++ src/main/java/chess/controller/ExceptionController.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<String> sqlException(SQLException exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예상치 못한 에러가 발생했습니다! 잠시후 다시 시도해주세요.");
-    }
+    private static final String SERVER_ERROR_MESSAGE = "예상치 못한 에러가 발생했습니다! 잠시후 다시 시도해주세요.";
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> stateException(IllegalStateException exception) {
@@ -31,8 +28,13 @@ public class ExceptionController {
                 .body(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<String> sqlException(SQLException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(SERVER_ERROR_MESSAGE);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> unCheckedException(Exception exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예상치 못한 에러가 발생했습니다! 잠시후 다시 시도해주세요.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(SERVER_ERROR_MESSAGE);
     }
 }

903a13caf656a2f1e9b3b09847252384cdad09c1
summerlunaa <summerlunaa9@gmail.com>
2022-05-02 17:36:22 +0900

    refactor: 커스텀 예외 적용 및 에러 메시지 설정

--- src/main/java/chess/web/controller/ChessControllerAdvice.java
+++ src/main/java/chess/web/controller/ChessControllerAdvice.java
@@ -1,10 +1,10 @@
 package chess.web.controller;
 
+import chess.exception.ChessGameException;
 import java.sql.SQLException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,30 +13,24 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class ChessControllerAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class, UnsupportedOperationException.class})
+    @ExceptionHandler(ChessGameException.class)
     public ResponseEntity<String> handleGameException(Exception exception) {
-        return new ResponseEntity<>(
-                exception.getMessage(),
-                HttpStatus.BAD_REQUEST
-        );
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({SQLException.class, DataAccessException.class})
-    public String handleSQLException(Exception exception, Model model) {
-        model.addAttribute("exception", exception);
-        return "/error";
+    public ResponseEntity<String> handleSQLException() {
+        return new ResponseEntity<>("잘못된 DB 접근입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNoHandlerFoundException(NoHandlerFoundException exception, Model model){
-        model.addAttribute("exception", exception);
-        return "/error";
+    public ResponseEntity<String> handleNoHandlerFoundException() {
+        return new ResponseEntity<>("잘못된 주소입니다.", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleCommonException(Exception exception, Model model) {
-        model.addAttribute("exception", exception);
-        return "/error";
+    public ResponseEntity<String> handleCommonException() {
+        return new ResponseEntity<>("잘못된 접근입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

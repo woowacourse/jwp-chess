037fc1db6aa0e19fdecb6777183ebd33af32fd7d
sun <syoun602@gmail.com>
2022-05-02 15:14:12 +0900

    refactor: 예외에 대한 처리 방식 변경

--- src/main/java/chess/controller/ExceptionAdvice.java
+++ src/main/java/chess/controller/ExceptionAdvice.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    public ResponseEntity<String> gameException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exception(final Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.internalServerError().body(e.getMessage());
     }
 }

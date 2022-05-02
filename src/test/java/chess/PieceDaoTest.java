958dced49368af46dee8fd46c42466a8b5730c41
bomi <bonita9931@gmail.com>
2022-05-02 15:02:04 +0900

    refactor: @sql을 사용해 스키마 파일 분리

--- src/test/java/chess/PieceDaoTest.java
+++ src/test/java/chess/PieceDaoTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql("classpath:schema.sql")
 @JdbcTest
 class PieceDaoTest {
     private PieceDao pieceDao;

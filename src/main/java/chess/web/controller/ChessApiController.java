1f5b2ccf75b61c82f042fa8b39fcc0c8dfa44d83
youngwooyoo <fbduddn97@naver.com>
2022-05-03 10:59:43 +0900

    refactor: @ResponseStatus 사용해 HttpStatus 프론트에 응답하도록 수정

--- src/main/java/chess/web/controller/ChessApiController.java
+++ src/main/java/chess/web/controller/ChessApiController.java
@@ -4,11 +4,13 @@ import chess.web.dto.board.GameOverDto;
 import chess.web.dto.board.MovePositionsDto;
 import chess.web.dto.game.PasswordDto;
 import chess.web.service.ChessService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,7 @@ public class ChessApiController {
     }
 
     @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteGame(@RequestBody PasswordDto passwordDto) {
         chessService.deleteGame(passwordDto);
     }

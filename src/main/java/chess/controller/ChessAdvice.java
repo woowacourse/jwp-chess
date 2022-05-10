05824b7489471930b95b4b85407af523e01d8ed3
yoon <duddbs774@gmail.com>
2022-05-10 14:28:58 +0900

    refactor: 예상하지 못한 예외에 대해서 최소한의 정보만 반환하도록 리펙토링

--- src/main/java/chess/controller/ChessAdvice.java
+++ src/main/java/chess/controller/ChessAdvice.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ChessAdvice {
 
-    @ExceptionHandler({Exception.class})
-    private ResponseEntity<String> handleException(final Exception exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
-    }
-
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     private ResponseEntity<String> handleException(final RuntimeException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler({Exception.class})
+    private ResponseEntity<String> handleException(final Exception exception) {
+        return ResponseEntity.badRequest().build();
     }
 }

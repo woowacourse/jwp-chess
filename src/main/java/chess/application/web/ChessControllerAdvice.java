df3a284659e1f7be497f474b16e83f3867b7f807
SuyeonChoi <tndus970120@gmail.com>
2022-05-11 05:04:45 +0900

    feat(ChessControllerAdvice): IllegalArgumentException 예외를 처리할 수 있도록 변경 - RuntimeException 처리 추가 - 예외 반환 시, String이 아닌 ErrorResponse 객체를 응답하도록 변경

--- src/main/java/chess/application/web/ChessControllerAdvice.java
+++ src/main/java/chess/application/web/ChessControllerAdvice.java
@@ -1,5 +1,6 @@
 package chess.application.web;
 
+import chess.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ChessControllerAdvice {
+
     @ExceptionHandler
-    public ResponseEntity<String> handle(Exception exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[ERROR] " + exception.getMessage());
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("서버 내부 오류입니다."));
     }
 }
